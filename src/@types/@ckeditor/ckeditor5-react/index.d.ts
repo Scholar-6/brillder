3d297dbcdbbfb2f3de11df6314473ef925e07f6a
ivanromakh <ivanrouman@gmail.com>
2020-02-06 14:07:23 +0200

    CKEditor5 for react installed

--- src/@types/@ckeditor/ckeditor5-react/index.d.ts
+++ src/@types/@ckeditor/ckeditor5-react/index.d.ts
@@ -1,30 +1,4 @@
 declare module '@ckeditor/ckeditor5-react' {
-    import { Stats } from "fs";
-
-    export interface readOptions {
-      filter?: RegExp | Filter,
-      dirTransform?: DirTransform,
-      fileTransform?: FileTransform
-    }
-  
-    export type Filter = (file: File) => boolean;
-    export type DirTransform = (file: File, value: any) => any;
-    export type FileTransform = (file: File) => any;
-  
-    export function readDirectory(dir: string, options?: readOptions): object;
-  
-    export class File {
-      key: string;
-      readonly path: string;
-      readonly fullpath: string;
-      readonly ext: string;
-      readonly name: string;
-      readonly basename: string;
-  
-      constructor(dir: string, file: string);
-  
-      readonly attributes: Stats;
-      readonly isDirectory: boolean;
-      readonly isRequirable: boolean;
+    export default class Editor extends React.Component<any, any> {
     }
 }
