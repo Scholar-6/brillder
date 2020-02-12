15edf1c1896fd91dc9545cb519a90fcfba1ddb82
ivanromakh <ivanrouman@gmail.com>
2020-02-12 18:14:47 +0200

    login with google

--- src/components/preLoginPage/preLoginPage.tsx
+++ src/components/preLoginPage/preLoginPage.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import update from 'immutability-helper';
 import { Button, Grid } from "@material-ui/core";
-import './preLoginPage.scss'
 
+import './preLoginPage.scss'
+import host from '../../hostname';
+
+
+enum LoginType {
+  Student = 1,
+  Teacher = 2,
+  Builder = 3
+}
 
 function PreLoginPage(props: any) {
   const [userType, setUserType] = React.useState(0)
-  const selectLoginType = (number: number) => {
-    setUserType(update(userType, { $set: number }));
+  const selectLoginType = (type: LoginType) => {
+    setUserType(update(userType, { $set: type }));
   }
 
   const moveToLogin = () => {
-    props.history.push('/login')
+    props.history.push('/login?userType=' + userType)
   }
 
   if (userType == 0) {
@@ -23,7 +31,7 @@ function PreLoginPage(props: any) {
           </div>
           <Grid container direction="row">
             <Grid container item xs={12} justify="center">
-              <Button onClick={() => selectLoginType(1)} className="user-type-btn">
+              <Button onClick={() => selectLoginType(LoginType.Student)} className="user-type-btn">
                 <img src="/images/lflogo.png" className="user-type-img" />
                 <span className="user-type-name">Student</span>
               </Button>
@@ -31,7 +39,7 @@ function PreLoginPage(props: any) {
           </Grid>
           <Grid container direction="row">
             <Grid container item xs={12} justify="center">
-              <Button onClick={() => selectLoginType(2)} className="user-type-btn">
+              <Button onClick={() => selectLoginType(LoginType.Teacher)} className="user-type-btn">
                 <img src="/images/lflogo.png" className="user-type-img rotate-180" />
                 <span className="user-type-name">Teacher</span>
               </Button>
@@ -39,7 +47,7 @@ function PreLoginPage(props: any) {
           </Grid>
           <Grid container direction="row">
             <Grid container item xs={12} justify="center">
-              <Button onClick={() => selectLoginType(3)} className="user-type-btn">
+              <Button onClick={() => selectLoginType(LoginType.Builder)} className="user-type-btn">
                 <img src="/images/lflogo.png" className="user-type-img rotate-90" />
                 <span className="user-type-name">Builder</span>
               </Button>
@@ -71,7 +79,7 @@ function PreLoginPage(props: any) {
           </Grid>
           <Grid container direction="row">
             <Grid container item xs={12} justify="center">
-              <Button className="google-button">
+              <Button className="google-button" href={host.BACKEND_HOST + '/auth/google'}>
                 <img className="google-icon" alt="" src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"/>
                 <span className="google-button-text">Sign in with Google</span>
               </Button>
