e3330c80e208fc6c9cf39f2ac77e096ce94ade4c
ivanromakh <ivanrouman@gmail.com>
2020-02-12 13:33:12 +0200

    Story book with component

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,10 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import Button from "./Button";
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button>😀 😎 👍 💯</Button>
+  ));
