35adac4a337b081c15014f5de233f7cbee6ae1bc
ivanromakh <ivanrouman@gmail.com>
2020-02-18 15:43:55 +0200

    #65 Logo Preview

--- src/components/logoPage/logoPage.tsx
+++ src/components/logoPage/logoPage.tsx
@@ -5,7 +5,7 @@ function LogoPage() {
   return (
     <Grid container direction="row" style={{ height: '100%', background: '#06215C' }} alignItems="center">
       <Grid container justify="center" item xs={12} style={{ height: '100%' }} alignItems="center">
-        <img src="/images/BrixLogo.png" style={{ height: '80%', width: '100%' }} alt="brix-logo" />
+        <img src="/images/BrixLogo.png" style={{ width: '70%' }} alt="brix-logo" />
       </Grid>
     </Grid>
   );
