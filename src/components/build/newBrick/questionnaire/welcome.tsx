ac2c74f7e9d2951105a11aeef2ccef62836e8f30
ivanromakh <ivanrouman@gmail.com>
2020-02-18 16:07:05 +0200

    welcome page design updated

--- src/components/build/newBrick/questionnaire/welcome.tsx
+++ src/components/build/newBrick/questionnaire/welcome.tsx
@@ -14,16 +14,21 @@ function Welcome() {
       <Grid container direction="row" style={{ height: '100%' }} alignItems="center">
         <Grid container justify="center" item xs={12} md={7} lg={8}>
           <div className="left-card">
-            <Grid container direction="row" justify="center" alignItems="center" style={{ height: "300px" }}>
+            <Grid className="tutorial-logo-container" container direction="row" justify="center" alignItems="center" style={{ height: "300px" }}>
               <Grid container direction="row" alignItems="center" style={{ height: "100%" }}>
                 <Grid container justify="center" item xs={12} style={{ height: "100%" }}>
                   <img src="/images/BrixLogo.png" style={{ height: '100%' }} alt="brix-logo" />
                 </Grid>
               </Grid>
             </Grid>
-            <h2>Welcome to Brix.</h2>
-            <h1>Start Building</h1>
-            <NextButton step={NewBrickStep.Welcome} canSubmit={true} />
+            <Grid item xs={12} style={{position: "relative"}}>
+              <h2 className="tutorial-logo-text">A &nbsp; S C H O L A R &nbsp; 6 &nbsp; T E C H &nbsp; P R O D U C T </h2>
+              <h2 className="welcome-upper-text">Welcome to Brix.</h2>
+              <h1 className="welcome-bottom-text">Start Building </h1>
+              <div style={{position: "absolute", top: '10px', right: 0}}>
+                <NextButton step={NewBrickStep.Welcome} canSubmit={true} />
+              </div>
+            </Grid>
           </div>
         </Grid>
         <Hidden only={['xs', 'sm']}>
