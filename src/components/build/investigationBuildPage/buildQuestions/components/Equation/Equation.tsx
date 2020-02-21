0fffa7162e7d7a60a0742a50c771ebcd1ff42976
ivanromakh <ivanrouman@gmail.com>
2020-02-21 12:09:40 +0200

    Remove CKEditor from Equation for now

--- src/components/build/investigationBuildPage/buildQuestions/components/Equation/Equation.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Equation/Equation.tsx
@@ -1,11 +1,4 @@
 import React from 'react'
-// @ts-ignore 
-import CKEditor from '@ckeditor/ckeditor5-react';
-// @ts-ignore 
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-// @ts-ignore
-import MathType from '@wiris/mathtype-ckeditor5';
-
 import './Equation.scss'
 
 export interface EquationComponentProps {
@@ -15,27 +8,10 @@ export interface EquationComponentProps {
   updateComponent(component: any, index: number): void
 }
 
-const editorConfiguration = {
-  plugins: [ MathType ],
-  toolbar: ['MathType', 'ChemType']
-};
-
-const EquationComponent: React.FC<EquationComponentProps> = ({index, data, cleanComponent, updateComponent}) => {
-  if (!data.value) {
-    data.value = "";
-  }
+const EquationComponent: React.FC<any> = () => {
 
   return (
     <div className="question-build-text-editor">
-      <CKEditor
-        editor={ClassicEditor}
-        data={data.value}
-        config={editorConfiguration}
-        onChange={(event: any, editor: any) => {
-          data.value = editor.getData();
-          updateComponent(data, index);
-        }}
-      />
     </div>
   );
 }
