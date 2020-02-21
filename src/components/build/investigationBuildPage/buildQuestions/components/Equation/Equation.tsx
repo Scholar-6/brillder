ab34c61b0cef97a6419ba4b5c0db881b089b3621
Lindsay MacVean <lindsaymacvean@gmail.com>
2020-02-21 09:48:12 +0000

    added equation component

--- src/components/build/investigationBuildPage/buildQuestions/components/Equation/Equation.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Equation/Equation.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+// @ts-ignore 
+import CKEditor from '@ckeditor/ckeditor5-react';
+// @ts-ignore 
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// @ts-ignore
+import MathType from '@wiris/mathtype-ckeditor5';
+
 import './Equation.scss'
 
-const EquationComponent: React.FC<any> = () => {
-  return <p>Equation Component</p>
+export interface EquationComponentProps {
+  index: number,
+  data: any,
+  cleanComponent(): void
+  updateComponent(component: any, index: number): void
+}
+
+const editorConfiguration = {
+  plugins: [ MathType ],
+  toolbar: ['MathType', 'ChemType']
+};
+
+const EquationComponent: React.FC<EquationComponentProps> = ({index, data, cleanComponent, updateComponent}) => {
+  if (!data.value) {
+    data.value = "";
+  }
+
+  return (
+    <div className="question-build-text-editor">
+      <CKEditor
+        editor={ClassicEditor}
+        data={data.value}
+        config={editorConfiguration}
+        onChange={(event: any, editor: any) => {
+          data.value = editor.getData();
+          updateComponent(data, index);
+        }}
+      />
+    </div>
+  );
 }
 
 export default EquationComponent
