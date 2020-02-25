9848259b8c9a4f6b6dcddc530504d5f6bf3e61b4
ivanromakh <ivanrouman@gmail.com>
2020-02-25 17:19:34 +0200

    Word highlighting and fix short answer button

--- src/components/build/investigationBuildPage/buildQuestions/questionTypes/wordHighlighting/wordHighlighting.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/questionTypes/wordHighlighting/wordHighlighting.tsx
@@ -1,14 +1,59 @@
 import React from 'react'
+import EditIcon from '@material-ui/icons/Edit';
 
+import './wordHighlighting.scss'
 
-export interface WordHighlightingProps {
+export interface Word {
+
 }
 
-const WordHighlightingComponent: React.FC<WordHighlightingProps> = () => {
+export interface WordHighlightingData {
+  text: string;
+  word: Word;
+  mode: boolean;
+}
+
+export interface WordHighlightingProps {
+  data: WordHighlightingData
+  updateComponent(component: any): void
+}
+
+const WordHighlightingComponent: React.FC<WordHighlightingProps> = ({ data, updateComponent }) => {
+
+  const switchMode = () => {
+    data.mode = !data.mode;
+    updateComponent(data);
+  }
+
+  const updateText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    data.text = e.target.value;
+    updateComponent(data);
+  }
+
+  const renderBox = () => {
+    if (data.mode) {
+      let words = data.text.split(' ');
+      return <div className="hightlight-area">{words.map((word, i) => <div>{word}</div>)}</div>;
+    }
+    return (
+      <textarea className="words-input" rows={5} value={data.text} onChange={updateText} placeholder="Enter words here..." />
+    );
+  }
+
   return (
-    <div className="input-box ">
-      <div>
-        WordHighlightingComponent
+    <div className="word-highlight-build">
+      <div className="component-title">
+        <div>Enter/Paste Text Below.</div>
+        <div>Use Highlighter Icon to click correct word(s).</div>
+      </div>
+      <div className="pencil-icon-container">
+        <EditIcon className={data.mode ? "active" : ""} onClick={switchMode} />
+      </div>
+      <div className="input-container">
+        {renderBox()}
+      </div>
+      <div className="button-box">
+        <button className="add-answer-button" onClick={() => {}}>Convert to Click and Correct?</button>
       </div>
     </div>
   )
