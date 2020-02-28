d8c18b4cdbcc13367e9e14dadba9863f3adafb97
ivanromakh <ivanrouman@gmail.com>
2020-02-28 15:51:22 +0200

    Qoute as text editor

--- src/components/build/investigationBuildPage/buildQuestions/components/Quote/Quote.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Quote/Quote.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+// @ts-ignore 
+import CKEditor from '@ckeditor/ckeditor5-react';
+// @ts-ignore 
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 import './Quote.scss'
+
 
 export interface QuoteComponentProps {
   locked: boolean
@@ -9,19 +14,27 @@ export interface QuoteComponentProps {
   updateComponent(component: any, index: number): void
 }
 
+const editorConfiguration = {
+  toolbar: ['bold']
+};
+
 const QuoteComponent: React.FC<QuoteComponentProps> = ({locked, index, data, updateComponent}) => {
   if (!data.value) {
     data.value = "";
   }
 
-  const onChange = (e: any) => {
-    data.value = e.target.value;
-    updateComponent(data, index);
-  }
-
   return (
     <div className="question-build-text-editor">
-      <input value={data.value} disabled={locked} onChange={onChange} placeholder="Enter Quote Here..."/>
+      <CKEditor
+        editor={ClassicEditor}
+        data={data.value}
+        disabled={locked}
+        config={editorConfiguration}
+        onChange={(e: any, editor: any) => {
+          data.value = editor.getData();
+          updateComponent(data, index);
+        }}
+      />
     </div>
   );
 }
