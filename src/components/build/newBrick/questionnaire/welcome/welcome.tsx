34df9eb0d07b6da52fcefaf803e4f24fe043fb2a
ivanromakh <ivanrouman@gmail.com>
2020-03-04 16:57:56 +0200

    Fix welcome page

--- src/components/build/newBrick/questionnaire/welcome/welcome.tsx
+++ src/components/build/newBrick/questionnaire/welcome/welcome.tsx
@@ -20,31 +20,33 @@ function Welcome() {
   return (
     <div className="tutorial-page welcome-page">
       <Grid container direction="row" style={{ height: '100%' }} alignItems="center">
-        <div className="left-card">
-          <div style={{position: 'absolute', top: 0, right: 0, width: '100%', height: '100%'}}>
-            <Grid className="tutorial-logo-container" container direction="row" justify="center" alignItems="center">
-              <Grid container direction="row" alignItems="center" justify="center" style={{ height: "100%" }}>
-                <Grid container justify="center" item xs={12} style={{ height: "100%" }}>
-                  <img src="/images/BrixLogo.png" alt="brix-logo" />
+        <Grid container justify="center" item xs={12} md={7} lg={8}>
+          <Grid justify="center" container item xs={12} sm={9} md={10} lg={7}>
+            <div className="left-card">
+              <Grid className="tutorial-logo-container" container direction="row" justify="center" alignItems="center">
+                <Grid container direction="row" alignItems="center" justify="center" style={{ height: "100%" }}>
+                  <Grid container justify="center" item xs={12} style={{ height: "100%" }}>
+                    <img src="/images/BrixLogo.png" alt="brix-logo" />
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-            <Grid container direction="row" style={{height: '40%', paddingTop: '13%'}}>
-              <Grid item xs={12} style={{ position: "relative", height: '100%' }}>
-                <h2 className="tutorial-logo-text">
-                  A &nbsp; S C H O L A R &nbsp; 6 &nbsp; T E C H &nbsp; P R O D U C T
-                </h2>
-                <h2 className="welcome-upper-text">Welcome to Brix.</h2>
-                <h1 className="welcome-bottom-text">Start Building </h1>
-                <div className="welcome-next-container">
-                  <IconButton className="welcome-next-button" onClick={next} aria-label="next">
-                    <ArrowForwardIosIcon className="welcome-next-icon" />
-                  </IconButton>
-                </div>
+              <Grid container direction="row" className="welcome-fotter">
+                <Grid item xs={12} style={{ position: "relative", height: '100%' }}>
+                  <h2 className="tutorial-logo-text">
+                    A &nbsp; S C H O L A R &nbsp; 6 &nbsp; T E C H &nbsp; P R O D U C T
+                  </h2>
+                  <h2 className="welcome-upper-text">Welcome to Brix.</h2>
+                  <h1 className="welcome-bottom-text">Start Building </h1>
+                  <div className="welcome-next-container">
+                    <IconButton className="welcome-next-button" onClick={next} aria-label="next">
+                      <ArrowForwardIosIcon className="welcome-next-icon" />
+                    </IconButton>
+                  </div>
+                </Grid>
               </Grid>
-            </Grid>
-          </div>
-        </div>
+            </div>
+          </Grid>
+        </Grid>
         <Hidden only={['xs', 'sm']}>
           <PhonePreview link={window.location.origin + '/logo-page'} />
         </Hidden>
