4aae7ac628e7ac992b010d6b5932b0f0e773c723
ivanromakh <ivanrouman@gmail.com>
2020-03-05 11:26:51 +0200

    Phone preview

--- src/components/build/investigationBuildPage/buildQuestions/components/Sound/Sound.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Sound/Sound.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {useDropzone} from 'react-dropzone';
+// @ts-ignore
+import Recorder from 'react-mp3-recorder'
 
-import './Sound.scss'
+import './Sound.scss';
 import { Grid } from '@material-ui/core';
 
 interface SoundProps {
@@ -20,15 +22,31 @@ const SoundComponent: React.FC<SoundProps> = ({locked}) => {
     </li>
   ));
 
+  const onStop = (blob:any) => {
+    console.log(blob);
+  }
+
+  const onRecordingComplete = (blob:any) => {
+    console.log('recording', blob)
+  }
+ 
+  const onRecordingError = (err:any) => {
+    console.log('recording error', err)
+  }
+
   return (
     <div>
       <div {...getRootProps({className: 'dropzone ' + ((locked) ? 'disabled' : '')})}>
         <input {...getInputProps()} />
-        <Grid container justify="center" alignContent="center" style={{height:'100%'}}>
+        <Grid container justify="center" alignContent="center" style={{height:'80%'}}>
           <p>Drag Sound File Here | Click to Select Sound File</p>
         </Grid>
+        
       </div>
       {files[0]}
+      <Grid container justify="center" alignContent="center" style={{height:'20%'}}>
+          <Recorder onRecordingComplete={onRecordingComplete} onRecordingError={onRecordingError} />
+        </Grid>
     </div>
   );
 }
