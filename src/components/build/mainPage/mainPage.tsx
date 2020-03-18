191d490a97a7aed5b883dff5f0a39da76d2b35cf
ivan <=>
2020-03-18 21:06:25 +0200

    Main Page Styling

--- src/components/build/mainPage/mainPage.tsx
+++ src/components/build/mainPage/mainPage.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 // @ts-ignore
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
 
 import './mainPage.scss';
-import MainMenu from '../baseComponents/mainMenu/MainMenu';
+import actions from 'redux/actions/auth';
 
 
 const mapState = (state: any) => {
@@ -13,7 +12,9 @@ const mapState = (state: any) => {
 }
 
 const mapDispatch = (dispatch: any) => {
-  return { }
+  return {
+    logout: () => dispatch(actions.logout())
+  }
 }
 
 const connector = connect(
@@ -21,25 +22,46 @@ const connector = connect(
   mapDispatch
 )
 
-class MainPage extends Component {
+class MainPage extends Component<any, any> {
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      viewHover: false,
+      createHober: false,
+      backHober: false,
+    } as any;
+  }
+
+  viewHoverToggle(viewHover: boolean) {
+    console.log(viewHover)
+    this.setState({viewHover});
+  }
+
   render() {
+    const {history} = this.props;
     return (
       <Grid container direction="row" className="mainPage">
         <div className="first-col">
         <div style={{background: "#B44438 0% 0% no-repeat padding-box"}}></div>
           <div className="first-item">
-            <div className="view-item">
-              <img alt="Logo" src="/images/main-page/glasses.png" className="item-image" />
-              <div className="item-description">View All Bricks</div>
-            </div>
-            <div className="create-item">
-              <img alt="Logo" src="/images/main-page/create.png" className="item-image" />
-              <div className="item-description">Start Building</div>
-            </div>
-            <div className="back-item">
-              <img alt="Logo" src="/images/main-page/backToWork.png" className="item-image" />
-              <div className="item-description">Back To Work</div>
-            </div>
+            <Grid container justify="center" style={{width: "100%"}}>
+              <div className="zoom-item view-item" onClick={() => history.push('/build/bricks-list')}>
+                <img alt="Logo" src="/images/main-page/glasses.png" className="item-image" />
+                <div className="item-description">View All Bricks</div>
+              </div>
+            </Grid>
+            <Grid container justify="center" style={{width: "100%"}}>
+              <div className="zoom-item create-item" onClick={() => history.push('/build/new-brick/welcome')}>
+                <img alt="Logo" src="/images/main-page/create.png" className="item-image" />
+                <div className="item-description">Start Building</div>
+              </div>
+            </Grid>
+            <Grid container justify="center" style={{width: "100%"}}>
+              <div className="zoom-item back-item" onClick={() => history.push('/build')}>
+                <img alt="Logo" src="/images/main-page/backToWork.png" className="item-image" />
+                <div className="item-description">Back To Work</div>
+              </div>
+            </Grid>
           </div>
           <div className="second-item"></div>
         </div>
@@ -47,9 +69,17 @@ class MainPage extends Component {
           <div className="first-item"></div>
           <div className="second-item"></div>
         </div>
-        <div className="logout-button">
+        <div className="logout-button" onClick={this.props.logout}>
           <Grid container alignContent="center">
-            <p>Q</p>
+            <div style={{position: 'relative'}}>
+              <img className="logout-image image-bottom" alt="logout" src="/images/main-page/logout.png" />
+              <img className="logout-image image-top" alt="logout" src="/images/main-page/logout-hover.png" />
+            </div>
+            <div>
+              <Grid container alignContent="center" style={{height: '100%'}}>
+                <span>LOGOUT</span>
+              </Grid>
+            </div>
           </Grid>
         </div>
       </Grid>
