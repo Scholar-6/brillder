09a83c79bcc416d75af30975b9300b9ab27acce9
ivan <=>
2020-03-18 17:39:05 +0200

    choose user mobile

--- src/components/authPages/chooseUserPage/ChooseUserPage.tsx
+++ src/components/authPages/chooseUserPage/ChooseUserPage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import update from 'immutability-helper';
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, Hidden } from "@material-ui/core";
 import Snackbar from '@material-ui/core/Snackbar';
 import { History } from 'history';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import './ChooseUserPage.scss';
 import { UserLoginType } from 'model/auth';
@@ -70,24 +71,69 @@ const ChooseUserPage:React.FC<ChooseUserProps> = (props) => {
 
   return (
     <Grid className="choose-user-page" style={{ height: '100%' }}>
-      <Grid container direction="row" className="first-row">
-        <div className="first-col"></div>
-        <div className="second-col"></div>
-        <div className="third-col"></div>
-      </Grid>
-      <Grid container direction="row" className="second-row">
-        <div className="first-col">
-          {renderLogo()}
+      <Hidden only={['xs']}>
+        <Grid container direction="row" className="first-row">
+          <div className="first-col"></div>
+          <div className="second-col"></div>
+          <div className="third-col"></div>
+        </Grid>
+        <Grid container direction="row" className="second-row">
+          <div className="first-col">
+            {renderLogo()}
+          </div>
+          <div className="second-col">
+            {renderButtons()}
+          </div>
+        </Grid>
+        <Grid container direction="row" className="third-row">
+          <div className="first-col"></div>
+          <div className="second-col"></div>
+          <div className="third-col"></div>
+        </Grid>
+      </Hidden>
+      <Hidden only={['md', 'sm', 'lg', 'xl']} >
+        <div className="mobile-choose-user">
+          <Grid container direction="row" className="first-mobile-row">
+            <div className="first-col"></div>
+            <div className="second-col"></div>
+          </Grid>
+          <Grid container direction="row" className="second-mobile-row">
+            <div className="first-col"></div>
+            <div className="second-col"></div>
+          </Grid>
+          <Grid container direction="row" className="third-mobile-row">
+            <div className="first-col"></div>
+            <div className="second-col"></div>
+          </Grid>
         </div>
-        <div className="second-col">
-          {renderButtons()}
-        </div>
-      </Grid>
-      <Grid container direction="row" className="third-row">
-        <div className="first-col"></div>
-        <div className="second-col"></div>
-        <div className="third-col"></div>
-      </Grid>
+        <div style={{position: 'fixed', top: 0, height: '100%', width: '100%'}}>
+          <Grid justify="center">
+            <img alt="Logo" src="/images/BrixLogo.png" className="logo-mobile-image" />
+          </Grid>
+          <ExpandMoreIcon className="mobile-arrow" />
+          <Grid container direction="row">
+            <Grid container item xs={12} justify="center">
+              <Button onClick={() => { selectLoginType(UserLoginType.Student); }} className="user-type-mobile-btn">
+                <span className="user-type-name">L e a r n</span>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row">
+            <Grid container item xs={12} justify="center">
+              <Button onClick={() => { selectLoginType(UserLoginType.Teacher); openMessage(); }} className="user-type-mobile-btn">
+                <span className="user-type-name">T e a c h</span>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row">
+            <Grid container item xs={12} justify="center">
+              <Button onClick={() => selectLoginType(UserLoginType.Builder)} className="user-type-mobile-btn">
+                <span className="user-type-name">B u i l d</span>
+              </Button>
+            </Grid>
+          </Grid>
+          </div>
+      </Hidden>
       <Snackbar
         anchorOrigin={{
           vertical: 'bottom', horizontal: 'center',
