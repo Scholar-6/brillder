9925e729f44d272d5f18074f3f9c1cefcef2c1a7
ivan <=>
2020-03-31 16:48:12 +0300

    Styling synthesis page

--- src/components/build/investigationBuildPage/synthesisPage/SynthesisPage.tsx
+++ src/components/build/investigationBuildPage/synthesisPage/SynthesisPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import './SynthesisPage.scss';
+import { Grid, Button } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
 
 export interface SynthesisProps {
@@ -12,12 +14,35 @@ const SynthesisPage: React.FC<SynthesisProps> = ({ onSynthesisChange, goBack }) 
   document.title = "Synthesis";
 
   return (
-    <div className="question-type">
+    <div className="question-type synthesis-page">
       <div className="inner-question-type">
-        <textarea
-          placeholder="Synthesis"
-          style={{width: '100%', height: '30vh', fontSize: '1.5vw'}}
-          onChange={(e) => onSynthesisChange(e.target.value)}></textarea>
+        <Grid container direction="row">
+          <Grid item md={9}>
+            <textarea
+              placeholder="Synthesis"
+              onChange={(e) => onSynthesisChange(e.target.value)}></textarea>
+          </Grid>
+          <Grid container item md={3}>
+            <div style={{width: '100%'}}>
+              <div className="finish-text">Finished?</div>
+              <div style={{textAlign: 'center'}}>
+                <Button className="edit-proposal-button" onClick={() => {}}>
+                  <EditIcon className="edit-icon"/>
+                  Edit Proposal
+                </Button>
+              </div>
+              <div style={{textAlign: 'center'}}>
+                <Button className="submit-button" onClick={() => {}}>
+                  <div>
+                <div>Review</div>
+                <div>&</div>
+                <div>Submit</div>
+                </div>
+                </Button>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
