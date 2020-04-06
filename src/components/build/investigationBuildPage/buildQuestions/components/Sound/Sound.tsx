6fb96c994237bb8da0c60fcbb52e25e6e9ab4444
ivan <=>
2020-04-06 11:41:39 +0300

    Audio recorder

--- src/components/build/investigationBuildPage/buildQuestions/components/Sound/Sound.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Sound/Sound.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import {useDropzone} from 'react-dropzone';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import axios from 'axios';
+// @ts-ignore
+import ReactRecord from 'react-record';
 
 import './Sound.scss';
-import { Grid } from '@material-ui/core';
+import { Grid, Button } from '@material-ui/core';
 
 interface SoundProps {
   locked: boolean
 }
 
+enum AudioStatus {
+  Start,
+  Recording,
+  Recorded,
+  Play,
+  Stop
+}
+
 const SoundComponent: React.FC<SoundProps> = ({locked}) => {
+  const [status, setStatus] = React.useState(AudioStatus.Start);
+  const [blobUrl, setBlobUrl] = React.useState("");
+  const [audio, setAudio] = React.useState(new Audio());
   const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
     accept: 'audio/*',
     disabled: locked
@@ -20,8 +35,84 @@ const SoundComponent: React.FC<SoundProps> = ({locked}) => {
     </li>
   ));
 
+  const recordFile = (blob: Blob) => {
+    console.log('Record: ', blob);
+  }
+
+  const onSave = (blob: any) => {
+    saveAudio(blobUrl);
+  }
+
+  const onStop = (blob: any) => {
+    setBlobUrl(blob.blobURL);
+    setAudio(new Audio(blob.blobURL));
+  }
+
+  const startRecording = () => {
+    return status === AudioStatus.Start ? setStatus(AudioStatus.Recording) : null;
+  }
+
+  const stopRecording = () => {
+    return status === AudioStatus.Recording ? setStatus(AudioStatus.Recorded) : null;
+  }
+
+  const playRecord = () => {
+    audio.play();
+    setStatus(AudioStatus.Play);
+    audio.onpause = function () {
+      //console.log(pause);
+    }
+  }
+
+  function dataURItoBlob(dataURI: string) {
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+      byteString = atob(dataURI.split(',')[1]);
+    } else {
+      byteString = unescape(dataURI.split(',')[1]);
+    }
+
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+  }
+
+  const saveAudio = (result: string) => {
+    if (result) {
+      let file = dataURItoBlob(result as any);
+      var formData = new FormData();
+      formData.append('file', file);
+      return axios.post(
+        process.env.REACT_APP_BACKEND_HOST + '/fileUpload',
+        formData,
+        {
+          headers: { 'Content-Type': 'multipart/form-data' },
+          withCredentials: true
+        }
+      ).then(res => {
+        console.log(res.data);
+        //let comp = Object.assign({}, props.data);
+        //comp.value = res.data.fileName;
+        //props.updateComponent(comp, props.index);
+      })
+      .catch(error => {
+        alert('Can`t save image');
+      });
+    }
+  }
+
+  const deleteAudio = () => {
+    setStatus(AudioStatus.Start);
+    setBlobUrl("");
+  }
+
   return (
-    <div>
+    <div className="react-recording">
       <div {...getRootProps({className: 'dropzone ' + ((locked) ? 'disabled' : '')})}>
         <input {...getInputProps()} />
         <Grid container justify="center" alignContent="center" style={{height:'80%'}}>
@@ -29,6 +120,43 @@ const SoundComponent: React.FC<SoundProps> = ({locked}) => {
         </Grid>
       </div>
       {files[0]}
+      <Grid container item xs={12} justify="center">
+      <ReactRecord
+        record={status === AudioStatus.Recording}
+        onStop={onStop}
+        onSave={onSave}
+        onData={recordFile}
+      >
+        {
+          (status === AudioStatus.Start && !blobUrl) ?
+            <Button className="start-record" onClick={startRecording} type="button">
+              <div className="round-circle"></div> Record
+            </Button>
+          : <div></div>
+        }
+        {
+          (status === AudioStatus.Recording) ?
+            <Button className="stop-record" onClick={stopRecording} type="button">
+             <div className="round-circle"></div>  Recording
+            </Button>
+          : <div></div>
+        }
+        {
+          blobUrl ?
+            <Button className="play-record" onClick={playRecord} type="button">
+              <PlayArrowIcon className="play-arrow" /> Play
+            </Button>
+         : <div></div>
+        }
+        <Button
+          className={"delete-record " + (!blobUrl ? 'disabled' : "")}
+          onClick={() => deleteAudio()}
+          disabled={!blobUrl}
+        >
+          Delete
+        </Button>
+      </ReactRecord>
+      </Grid>
     </div>
   );
 }
