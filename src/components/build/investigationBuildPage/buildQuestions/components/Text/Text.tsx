73863715a43d0d1fd79e83bcfc9e4f085465f606
ivan <=>
2020-04-21 15:21:16 +0300

    ckeditor with colors

--- src/components/build/investigationBuildPage/buildQuestions/components/Text/Text.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Text/Text.tsx
@@ -1,3 +1,59 @@
+import React, { useEffect } from 'react'
+
+import './Text.scss'
+import { Grid } from '@material-ui/core';
+
+
+declare var CKEDITOR: any;
+
+export interface TextComponentProps {
+  locked: boolean
+  index: number
+  data: any
+  updateComponent(component: any, index: number): void
+}
+
+const TextComponent: React.FC<TextComponentProps> = (props) => {
+  const id = "editor-" + new Date().getTime();
+  const [created, setCreated] = React.useState(false);
+
+  useEffect(() => {
+    if (created === false) {
+      var editor = CKEDITOR.replace(id, {
+        toolbar: [
+          { name: 'colors', items: [ 'TextColor' ] },
+          { name: 'basicstyles', items: [ 'Bold', 'Italic' ] }
+        ],
+        height: 100
+      });
+      console.log(props.data.value);
+      editor.setData(props.data.value);
+      editor.on('change', (e:any) => {
+        let comp = Object.assign({}, props.data);
+        comp.value = e.editor.getData();
+        props.updateComponent(comp, props.index);
+      });
+      setCreated(true);
+    }
+  }, [id, props, created]);
+
+  return (
+    <div className="question-build-text-editor">
+      <div className="text-label-container">
+        <Grid className="text-label" container justify="center" alignContent="center">
+          Text
+        </Grid>
+      </div>
+      <div style={{minHeight: 170}} className="question-build-text-editor">
+        <div id={id} className="ckeditor-4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default TextComponent
+
+/*
 import React from 'react'
 // @ts-ignore 
 import CKEditor from '@ckeditor/ckeditor5-react';
@@ -53,3 +109,4 @@ const TextComponent: React.FC<TextComponentProps> = ({locked, index, data, updat
 }
 
 export default TextComponent
+*/
