884472672583ab5c5cb467b614b0f4223a31248d
ivan <=>
2020-05-05 14:01:34 +0300

    Navigation update

--- src/components/build/proposal/components/navigation/Navigation.tsx
+++ src/components/build/proposal/components/navigation/Navigation.tsx
@@ -35,31 +35,56 @@ const NextButton:React.FC<NextButtonProps> = ({ step }) => {
   return (
     <div className="navigation-container">
       <Grid container item justify="center">
-        <img
-          onClick={moveToTitles}
-          className="navigation-button first" alt=""
-          src="/images/new-brick/titles.png"
-        />
-        <img
-          onClick={moveToOpenQuestion}
-          className="navigation-button" alt=""
-          src={step >= ProposalStep.OpenQuestion ? "/images/new-brick/head.png" : "/images/new-brick/head-grey.png"}
-        />
-        <img
-          onClick={moveToBrief}
-          className="navigation-button" alt=""
-          src={step >= ProposalStep.Brief ? "/images/new-brick/brief-circles.png" : "/images/new-brick/brief-circles-grey.png"}
-        />
-        <img
-          onClick={moveToPrep}
-          className="navigation-button" alt=""
-          src={step >= ProposalStep.Prep ? "/images/new-brick/prep.png" : "/images/new-brick/prep-grey.png"}
-        />
-        <img
-          onClick={moveToLength}
-          className="navigation-button last" alt=""
-          src={step >= ProposalStep.BrickLength ? "/feathericons/clock.png" : "/feathericons/clock-grey.png"}
-        />
+        <div className="step-container">
+          {
+            step === ProposalStep.BrickTitle ? <div className="step-label">Title</div> : ""
+          }
+          <img
+            onClick={moveToTitles}
+            className="navigation-button first" alt=""
+            src="/images/new-brick/titles.png"
+          />
+          </div>
+        <div className="step-container">
+          {
+            step === ProposalStep.OpenQuestion ? <div className="step-label">Open Question</div> : ""
+          }
+          <img
+            onClick={moveToOpenQuestion}
+            className="navigation-button" alt=""
+            src={step >= ProposalStep.OpenQuestion ? "/images/new-brick/head.png" : "/images/new-brick/head-grey.png"}
+          />
+        </div>
+        <div className="step-container">
+          {
+            step === ProposalStep.Brief ? <div className="step-label">Brief</div> : ""
+          }
+          <img
+            onClick={moveToBrief}
+            className="navigation-button" alt=""
+            src={step >= ProposalStep.Brief ? "/images/new-brick/brief-circles.png" : "/images/new-brick/brief-circles-grey.png"}
+          />
+        </div>
+        <div className="step-container">
+          {
+            step === ProposalStep.Prep ? <div className="step-label">Prep</div> : ""
+          }
+          <img
+            onClick={moveToPrep}
+            className="navigation-button" alt=""
+            src={step >= ProposalStep.Prep ? "/images/new-brick/prep.png" : "/images/new-brick/prep-grey.png"}
+          />
+        </div>
+        <div className="step-container">
+          {
+            step === ProposalStep.BrickLength ? <div className="step-label">Length</div> : ""
+          }
+          <img
+            onClick={moveToLength}
+            className="navigation-button last" alt=""
+            src={step >= ProposalStep.BrickLength ? "/feathericons/clock.png" : "/feathericons/clock-grey.png"}
+          />
+        </div>
       </Grid>
     </div>
   );
