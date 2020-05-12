319f712667643ce8b46eaf7559966a45d47c955d
ivan <=>
2020-05-12 10:38:11 +0300

    Fix interface

--- src/components/baseComponents/homeButton/HomeButton.tsx
+++ src/components/baseComponents/homeButton/HomeButton.tsx
@@ -19,7 +19,7 @@ interface HomeButtonState {
   style: any,
 }
 
-class HomeButtonComponent extends React.Component<HomeButtonProps, HomeButtonState> {
+class HomeButtonComponent extends React.Component<any, HomeButtonState> {
   constructor(props: HomeButtonProps) {
     super(props);
     this.state = {
