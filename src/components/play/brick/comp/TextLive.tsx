5d6d9c9a675a3c21436781714c87e16d6143acd4
ivan <=>
2020-05-13 13:20:31 +0300

    MathJaxPreview

--- src/components/play/brick/comp/TextLive.tsx
+++ src/components/play/brick/comp/TextLive.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// @ts-ignore
+import MathJax from 'react-mathjax-preview'
 import './TextLive.scss';
 
 
@@ -8,7 +10,33 @@ interface TextProps {
 }
 
 const TextLive: React.FC<TextProps> = ({ component }) => {
-  return <div className="text-play" dangerouslySetInnerHTML={{ __html: component.value}} />;
+  const parseData = () => {
+    const res = component.value.replace(/<\/p>/gi, (s: string) => {
+      return s + '\n';
+    });
+    return res.match(/<(.+)>.*?<\/(.+)>/g);
+  }
+
+  var arr = parseData();
+
+  const renderMath = (data: string, i: number) => {
+    return <MathJax math={data} key={i} />;
+  }
+
+  return (
+    <div className="text-play">
+      {
+        arr ? arr.map((el:any, i:number) => {
+          const res = el.indexOf('<math xmlns="http://www.w3.org/1998/Math/MathML">');
+          if (res >= 0) {
+            return renderMath(el, i);
+          } else {
+            return <div key={i} dangerouslySetInnerHTML={{ __html: el}} />
+          }
+        }) : ""
+      }
+    </div>
+  );
 }
 
 export default TextLive;
