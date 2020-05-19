24e5bc3ef9d58143e693d1b1e7d4e9884708a501
ivan <=>
2020-05-19 14:56:32 +0300

    Validate text images and sounds

--- src/components/build/investigationBuildPage/questionService/QuestionService.ts
+++ src/components/build/investigationBuildPage/questionService/QuestionService.ts
@@ -1,14 +1,11 @@
 import {
   Question, QuestionTypeEnum, QuestionComponentTypeEnum, Hint, HintStatus
 } from 'model/question';
-import {QuestionValueType} from '../buildQuestions/questionTypes/types';
-import { Answer } from '../buildQuestions/questionTypes/pairMatchBuild/types';
-import { MissingChoice } from '../buildQuestions/questionTypes/missingWordBuild/MissingWordBuild';
+import uniqueValidator from './UniqueValidator';
 
 const getUniqueComponent = (components: any[]) => {
   return components.find(c => c.type === QuestionComponentTypeEnum.Component);
 }
-
 
 export function getNonEmptyComponent(components: any[]) {
   return !components.find(c =>
@@ -17,6 +14,21 @@ export function getNonEmptyComponent(components: any[]) {
     c.type === QuestionComponentTypeEnum.Quote ||
     c.type === QuestionComponentTypeEnum.Sound
   );
+}
+
+const validateComponentValues = (components: any[]) => {
+  const comps = components.filter(c => {
+    return c.type === QuestionComponentTypeEnum.Text
+    || c.type === QuestionComponentTypeEnum.Quote
+    || c.type === QuestionComponentTypeEnum.Image
+    || c.type === QuestionComponentTypeEnum.Sound
+  });
+
+  let invalid = comps.find(c => !c.value);
+  if (invalid) {
+    return false;
+  }
+  return true;
 }
 
 const validateHint = (hint: Hint) => {
@@ -28,158 +40,16 @@ const validateHint = (hint: Hint) => {
   }
 }
 
-const validateNotEmptyAnswer = (comp: any) => {
-  if (comp.list && comp.list.length >= 1) {
-    let invalid = comp.list.find((a:any) => !a.value);
-    if (invalid) {
-      return false;
-    }
-    return true;
-  }
-  return false;
-}
-
-const getChecked = (list: any[]) => {
-  return list.find((a:any) => a.checked === true);
-}
-
-const validateCheckedAnswer = (comp: any) => {
-  if (comp.list && comp.list.length > 1) {
-    let invalid = comp.list.find((a:any) => !a.value);
-    if (invalid) {
-      return false;
-    }
-    let checked = getChecked(comp.list);
-    if (checked) {
-      return true;
-    }
-  }
-  return false;
-}
-
-const validateTwoCheckedAnswers = (comp: any) => {
-  if (comp.list && comp.list.length > 1) {
-    let invalid = comp.list.find((a:any) => !a.value);
-    if (invalid) {
-      return false;
-    }
-
-    let checkedCount = 0;
-    for (let choice of comp.list) {
-      if (choice.checked) {
-        checkedCount++;
-      }
-    }
-    if (checkedCount >= 2) {
-      return true;
-    }
-  }
-  return false;
-}
-
-
-const validatePairMatch = (comp: any) => {
-  const validateChoice = (a: Answer) => {
-    if (a.answerType === QuestionValueType.Image && !a.valueFile) {
-      return false;
-    } else if (a.answerType !== QuestionValueType.Image && !a.value) {
-      return false;
-    }
-    return true;
-  }
-
-  const validateOption = (a: Answer) => {
-    if (a.optionType === QuestionValueType.Image && !a.optionFile) {
-      return false;
-    } else if (a.optionType !== QuestionValueType.Image && !a.option) {
-      return false;
-    }
-    return true;
-  }
-
-  const getInvalid = (a:Answer) => {
-    if (!validateChoice(a)) {
-      return true;
-    }
-
-    if (!validateOption(a)) {
-      return true;
-    }
-
-    return false;
-  }
-
-  if (comp.list && comp.list.length > 1) {
-    let invalid = comp.list.find(getInvalid);
-    if (invalid) {
-      return false;
-    }
-    return true;
-  }
-  return false;
-}
-
-const validateSort = (comp: any) => {
-  if (comp.categories && comp.categories.length > 1) {
-    const invalid = comp.categories.find((c:any) => {
-      if (!c.name) {
-        return true;
-      }
-      const invalid = c.answers.find((a:any) => !a.value);
-      if (invalid) {
-        return true;
-      }
-      return false;
-    });
-    if (invalid) {
-      return false;
-    }
-    return true;
-  }
-  return false;
-}
-
-const validateWordHighlighting = (comp: any) => {
-  if (comp.words && comp.words.length > 1) {
-    const valid = comp.words.find((w:any) => w.checked);
-    if (valid) {
-      return true;
-    }
-  }
-  return false;
-}
-
-const validateLineHighlighting = (comp: any) => {
-  if (comp.lines && comp.lines.length > 1) {
-    const valid = comp.lines.find((l:any) => l.checked);
-    if (valid) {
-      return true;
-    }
-  }
-  return false;
-}
-
-const validateMissingWord = (comp: any) => {
-  for (let choice of comp.choices as MissingChoice[]) {
-    let invalid = choice.answers.find((a:any) => !a.value);
-
-    if (invalid) {
-      return false;
-    }
-   
-    let checked = getChecked(choice.answers);
-    if (!checked) {
-      return false;
-    }
-  }
-  return true;
-}
-
 export function validateQuestion(question: Question) {
   const {type, hint, components} = question;
 
   let noComponent = getNonEmptyComponent(components);
   if (noComponent) {
+    return false;
+  }
+
+  let isValid = validateComponentValues(components);
+  if (!isValid) {
     return false;
   }
 
@@ -192,21 +62,21 @@ export function validateQuestion(question: Question) {
   if (type === QuestionTypeEnum.ShortAnswer || type === QuestionTypeEnum.VerticalShuffle
     || type === QuestionTypeEnum.HorizontalShuffle)
   {
-    return validateNotEmptyAnswer(comp);
+    return uniqueValidator.validateShortAnswer(comp);
   } else if (type === QuestionTypeEnum.ChooseOne) {
-    return validateCheckedAnswer(comp);
+    return uniqueValidator.validateChooseOne(comp);
   } else if (type === QuestionTypeEnum.ChooseSeveral) {
-    return validateTwoCheckedAnswers(comp);
+    return uniqueValidator.validateChooseSeveral(comp);
   } else if (type === QuestionTypeEnum.PairMatch) {
-    return validatePairMatch(comp);
+    return uniqueValidator.validatePairMatch(comp);
   } else if (type === QuestionTypeEnum.Sort) {
-    return validateSort(comp);
+    return uniqueValidator.validateSort(comp);
   } else if (type === QuestionTypeEnum.WordHighlighting) {
-    return validateWordHighlighting(comp);
+    return uniqueValidator.validateWordHighlighting(comp);
   } else if (type === QuestionTypeEnum.LineHighlighting) {
-    return validateLineHighlighting(comp);
+    return uniqueValidator.validateLineHighlighting(comp);
   } else if (type === QuestionTypeEnum.MissingWord) {
-    return validateMissingWord(comp);
+    return uniqueValidator.validateMissingWord(comp);
   }
   return false;
 };
---
+++ src/components/build/investigationBuildPage/questionService/UniqueValidator.ts
@@ -0,0 +1,163 @@
+import {QuestionValueType} from '../buildQuestions/questionTypes/types';
+import { Answer } from '../buildQuestions/questionTypes/pairMatchBuild/types';
+import { MissingChoice } from '../buildQuestions/questionTypes/missingWordBuild/MissingWordBuild';
+
+
+const getChecked = (list: any[]) => {
+  return list.find((a:any) => a.checked === true);
+}
+
+const validateShortAnswer = (comp: any) => {
+  if (comp.list && comp.list.length >= 1) {
+    let invalid = comp.list.find((a:any) => !a.value);
+    if (invalid) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+const validateChooseOne = (comp: any) => {
+  if (comp.list && comp.list.length > 1) {
+    let invalid = comp.list.find((a:any) => !a.value);
+    if (invalid) {
+      return false;
+    }
+    let checked = getChecked(comp.list);
+    if (checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const validateChooseSeveral = (comp: any) => {
+  if (comp.list && comp.list.length > 1) {
+    let invalid = comp.list.find((a:any) => !a.value);
+    if (invalid) {
+      return false;
+    }
+
+    let checkedCount = 0;
+    for (let choice of comp.list) {
+      if (choice.checked) {
+        checkedCount++;
+      }
+    }
+    if (checkedCount >= 2) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+const validatePairMatch = (comp: any) => {
+  const validateChoice = (a: Answer) => {
+    if (a.answerType === QuestionValueType.Image && !a.valueFile) {
+      return false;
+    } else if (a.answerType !== QuestionValueType.Image && !a.value) {
+      return false;
+    }
+    return true;
+  }
+
+  const validateOption = (a: Answer) => {
+    if (a.optionType === QuestionValueType.Image && !a.optionFile) {
+      return false;
+    } else if (a.optionType !== QuestionValueType.Image && !a.option) {
+      return false;
+    }
+    return true;
+  }
+
+  const getInvalid = (a:Answer) => {
+    if (!validateChoice(a)) {
+      return true;
+    }
+
+    if (!validateOption(a)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  if (comp.list && comp.list.length > 1) {
+    let invalid = comp.list.find(getInvalid);
+    if (invalid) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+const validateSort = (comp: any) => {
+  if (comp.categories && comp.categories.length > 1) {
+    const invalid = comp.categories.find((c:any) => {
+      if (!c.name) {
+        return true;
+      }
+      const invalid = c.answers.find((a:any) => !a.value);
+      if (invalid) {
+        return true;
+      }
+      return false;
+    });
+    if (invalid) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+const validateWordHighlighting = (comp: any) => {
+  if (comp.words && comp.words.length > 1) {
+    const valid = comp.words.find((w:any) => w.checked);
+    if (valid) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const validateLineHighlighting = (comp: any) => {
+  if (comp.lines && comp.lines.length > 1) {
+    const valid = comp.lines.find((l:any) => l.checked);
+    if (valid) {
+      return true;
+    }
+  }
+  return false;
+}
+  
+const validateMissingWord = (comp: any) => {
+  for (let choice of comp.choices as MissingChoice[]) {
+    let invalid = choice.answers.find((a:any) => !a.value);
+  
+    if (invalid) {
+      return false;
+    }
+     
+    let checked = getChecked(choice.answers);
+    if (!checked) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+export default {
+  validateShortAnswer,
+  validateChooseOne,
+  validateChooseSeveral,
+  validatePairMatch,
+  validateSort,
+  validateMissingWord,
+  validateWordHighlighting,
+  validateLineHighlighting,
+}
