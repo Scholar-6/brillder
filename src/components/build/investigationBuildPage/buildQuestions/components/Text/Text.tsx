2643dffc35bb84d21a1752f44209d50a490ae800
ivan <=>
2020-05-25 19:15:33 +0300

    #662 Text (between wiris C and bullets)

--- src/components/build/investigationBuildPage/buildQuestions/components/Text/Text.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/components/Text/Text.tsx
@@ -24,6 +24,11 @@ const TextComponent: React.FC<TextComponentProps> = ({locked, index, data, ...pr
 
   return (
     <div className="question-build-text-editor">
+      <div className="text-label-container">
+        <Grid className="text-label" container justify="center" alignContent="center">
+          Text
+        </Grid>
+      </div>
       <DocumentCKEditor
         data={data.value}
         placeholder=""
