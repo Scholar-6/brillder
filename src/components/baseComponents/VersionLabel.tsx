f7c65d2ef0bc6b168ab569d05278b7b5b2609062
ivan <=>
2020-06-01 19:40:18 +0300

    BETA. number under text

--- src/components/baseComponents/VersionLabel.tsx
+++ src/components/baseComponents/VersionLabel.tsx
@@ -4,7 +4,8 @@ import React from "react";
 const VersionLabel:React.FC = () => {
   return (
     <div className="beta-text">
-      BETA {process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : ""}
+      <div className="version-number">{process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : ""}</div>
+      <div>BETA</div>
     </div>
   );
 }
