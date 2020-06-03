ad53d0c0af7f66e2f5c830bda301ca2d11f82e33
ivan <=>
2020-06-03 11:30:22 +0300

    Beta text styling #777

--- src/components/baseComponents/VersionLabel.tsx
+++ src/components/baseComponents/VersionLabel.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const VersionLabel:React.FC = () => {
   return (
     <div className="beta-text">
-      <div className="version-number">{process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : ""}</div>
       <div>BETA</div>
+      <div className="version-number">{process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : ""}</div>
     </div>
   );
 }
