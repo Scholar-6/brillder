fef2b6c280c535dd227db9bcd1685f7df4b2cee5
ivanromakh <ivanrouman@gmail.com>
2020-06-09 17:03:17 +0300

    #842 Check brickId before changing data

--- src/components/build/investigationBuildPage/investigationBuildPage.tsx
+++ src/components/build/investigationBuildPage/investigationBuildPage.tsx
@@ -87,11 +87,15 @@ const InvestigationBuildPage: React.FC<InvestigationBuildProps> = props => {
   let initSynthesis = props.brick ? props.brick.synthesis as string : "";
   const [synthesis, setSynthesis] = React.useState(initSynthesis);
   useEffect(() => {
-    if (props.brick && props.brick.synthesis) {
-      setSynthesis(props.brick.synthesis)
-    }
-    if (props.brick && props.brick.locked) {
-      setLock(true);
+    if (props.brick) {
+      if (props.brick.id === brickId) { 
+        if (props.brick.synthesis || props.brick.synthesis === '') {
+          setSynthesis(props.brick.synthesis)
+        }
+        if (props.brick.locked) {
+          setLock(true);
+        }
+      }
     }
   }, [props.brick]);
   /* Synthesis */
