b355757289c84e69c083490494bc17322d3c056d
farmanahmed2007 <farmanahmed2007@gmail.com>
2020-06-09 19:40:29 +0500

    updated

--- src/components/baseComponents/homeButton/HomeButton.tsx
+++ src/components/baseComponents/homeButton/HomeButton.tsx
@@ -15,9 +15,6 @@ export interface HomeButtonProps {
 interface HomeButtonState {
 	focused: boolean,
 	ref: any,
-	//   enterTimeout: any,
-	//   leaveTimeout: any,
-	//   style: any,
 }
 
 class HomeButtonComponent extends React.Component<any, HomeButtonState> {
@@ -26,73 +23,8 @@ class HomeButtonComponent extends React.Component<any, HomeButtonState> {
 		this.state = {
 			focused: false,
 			ref: React.createRef(),
-
-			//   enterTimeout: null,
-			//   leaveTimeout: null,
-
-			//   style: {
-			//     baseSize: '6.3vh',
-			//     animateMarginLeft: '0.9vh',
-			//     animateMarginTop: '2.25vh',
-			//     animateHeight: '4vh',
-			//     animateWidth: '4.5vh',
-			//     transaction: 'all 0.25s ease-in-out',
-			//     delay: 200,
-			//   }
 		}
 	}
-
-	// onMouseEnterHandler() {
-	//   if (this.state.leaveTimeout) {
-	//     clearTimeout(this.state.leaveTimeout);
-	//   }
-
-	//   const {style} = this.state;
-	//   let homeIcon = this.state.ref.current;
-	//   homeIcon.style.marginTop = style.animateMarginTop;
-	//   homeIcon.style.marginLeft = style.animateMarginLeft;
-	//   homeIcon.style.height = style.animateHeight;
-	//   homeIcon.style.width = style.animateWidth;
-	//   homeIcon.style.transition = style.transaction;
-
-	//   const enterTimeout = setTimeout(() => {
-	//     homeIcon.style.transition = 'none';
-	//     homeIcon.style.marginTop = 0;
-	//     homeIcon.style.marginLeft = 0;
-	//     homeIcon.style.height = style.baseSize;
-	//     homeIcon.style.width = style.baseSize;
-	//     homeIcon.style.backgroundImage = 'url(/images/brick-list/home-hover.png)';
-	//     this.setState({...this.state, enterTimeout: null});
-	//   }, style.delay);
-	//   this.setState({...this.state, enterTimeout})
-	// }
-
-	// onMouseLeaveHandler() {
-	//   if (this.state.enterTimeout) {
-	//     clearTimeout(this.state.enterTimeout);
-	//   }
-	//   const {style} = this.state;
-	//   let homeIcon = this.state.ref.current;
-	//   homeIcon.style.marginTop = 0;
-	//   homeIcon.style.marginLeft = 0;
-	//   homeIcon.style.marginTop = style.animateMarginTop;
-	//   homeIcon.style.marginLeft = style.animateMarginLeft;
-	//   homeIcon.style.height = style.animateHeight;
-	//   homeIcon.style.width = style.animateWidth;
-	//   homeIcon.style.backgroundImage = 'url(/images/choose-login/logo.png)';
-
-	//   const leaveTimeout = setTimeout(() => {
-	//     homeIcon.style.transition = 'none';
-	//     homeIcon.style.height = style.baseSize;
-	//     homeIcon.style.width = style.baseSize;
-	//     homeIcon.style.marginTop = 0;
-	//     homeIcon.style.marginLeft = 0;
-	//     homeIcon.style.transition = style.transaction;
-	//     this.setState({...this.state, leaveTimeout: null});
-	//   }, 100);
-	//   this.setState({...this.state, leaveTimeout})
-	// }
-
 	onClick() {
 		if (this.props.onClick) {
 			this.props.onClick();
@@ -105,10 +37,7 @@ class HomeButtonComponent extends React.Component<any, HomeButtonState> {
 		return (
 			<Grid container direction="row" className="home-button-container">
 				<button type="button" className="btn btn-transparent svgOnHover home-button"
-					// onMouseEnter={() => this.onMouseEnterHandler()}
-					// onMouseLeave={() => this.onMouseLeaveHandler()}
 					onClick={() => this.onClick()}>
-					{/* <div ref={this.state.ref} /> */}\
 					<svg className="svg w100 h100 svg-default">
 						<use href={sprite + "#logo"} className="text-orange" />
 					</svg>
