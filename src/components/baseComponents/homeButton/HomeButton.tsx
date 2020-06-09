60228890cbef03d8fccb3d76dfea2e0ab64dcf60
Farman <farmanahmed2007@gmail.com>
2020-06-09 16:46:28 +0500

    fixed header icon and structure

--- src/components/baseComponents/homeButton/HomeButton.tsx
+++ src/components/baseComponents/homeButton/HomeButton.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Grid } from '@material-ui/core';
 import { withRouter } from "react-router-dom";
 
+import sprite from "../../../assets/img/icons-sprite.svg";
 import './HomeButton.scss';
 
 
@@ -14,9 +15,9 @@ export interface HomeButtonProps {
 interface HomeButtonState {
   focused: boolean,
   ref: any,
-  enterTimeout: any,
-  leaveTimeout: any,
-  style: any,
+//   enterTimeout: any,
+//   leaveTimeout: any,
+//   style: any,
 }
 
 class HomeButtonComponent extends React.Component<any, HomeButtonState> {
@@ -26,71 +27,71 @@ class HomeButtonComponent extends React.Component<any, HomeButtonState> {
       focused: false,
       ref: React.createRef(),
 
-      enterTimeout: null,
-      leaveTimeout: null,
+    //   enterTimeout: null,
+    //   leaveTimeout: null,
 
-      style: {
-        baseSize: '6.3vh',
-        animateMarginLeft: '0.9vh',
-        animateMarginTop: '2.25vh',
-        animateHeight: '4vh',
-        animateWidth: '4.5vh',
-        transaction: 'all 0.25s ease-in-out',
-        delay: 200,
-      }
+    //   style: {
+    //     baseSize: '6.3vh',
+    //     animateMarginLeft: '0.9vh',
+    //     animateMarginTop: '2.25vh',
+    //     animateHeight: '4vh',
+    //     animateWidth: '4.5vh',
+    //     transaction: 'all 0.25s ease-in-out',
+    //     delay: 200,
+    //   }
     }
   }
 
-  onMouseEnterHandler() {
-    if (this.state.leaveTimeout) {
-      clearTimeout(this.state.leaveTimeout);
-    }
+  // onMouseEnterHandler() {
+  //   if (this.state.leaveTimeout) {
+  //     clearTimeout(this.state.leaveTimeout);
+  //   }
 
-    const {style} = this.state;
-    let homeIcon = this.state.ref.current;
-    homeIcon.style.marginTop = style.animateMarginTop;
-    homeIcon.style.marginLeft = style.animateMarginLeft;
-    homeIcon.style.height = style.animateHeight;
-    homeIcon.style.width = style.animateWidth;
-    homeIcon.style.transition = style.transaction;
+  //   const {style} = this.state;
+  //   let homeIcon = this.state.ref.current;
+  //   homeIcon.style.marginTop = style.animateMarginTop;
+  //   homeIcon.style.marginLeft = style.animateMarginLeft;
+  //   homeIcon.style.height = style.animateHeight;
+  //   homeIcon.style.width = style.animateWidth;
+  //   homeIcon.style.transition = style.transaction;
 
-    const enterTimeout = setTimeout(() => {
-      homeIcon.style.transition = 'none';
-      homeIcon.style.marginTop = 0;
-      homeIcon.style.marginLeft = 0;
-      homeIcon.style.height = style.baseSize;
-      homeIcon.style.width = style.baseSize;
-      homeIcon.style.backgroundImage = 'url(/images/brick-list/home-hover.png)';
-      this.setState({...this.state, enterTimeout: null});
-    }, style.delay);
-    this.setState({...this.state, enterTimeout})
-  }
+  //   const enterTimeout = setTimeout(() => {
+  //     homeIcon.style.transition = 'none';
+  //     homeIcon.style.marginTop = 0;
+  //     homeIcon.style.marginLeft = 0;
+  //     homeIcon.style.height = style.baseSize;
+  //     homeIcon.style.width = style.baseSize;
+  //     homeIcon.style.backgroundImage = 'url(/images/brick-list/home-hover.png)';
+  //     this.setState({...this.state, enterTimeout: null});
+  //   }, style.delay);
+  //   this.setState({...this.state, enterTimeout})
+  // }
 
-  onMouseLeaveHandler() {
-    if (this.state.enterTimeout) {
-      clearTimeout(this.state.enterTimeout);
-    }
-    const {style} = this.state;
-    let homeIcon = this.state.ref.current;
-    homeIcon.style.marginTop = 0;
-    homeIcon.style.marginLeft = 0;
-    homeIcon.style.marginTop = style.animateMarginTop;
-    homeIcon.style.marginLeft = style.animateMarginLeft;
-    homeIcon.style.height = style.animateHeight;
-    homeIcon.style.width = style.animateWidth;
-    homeIcon.style.backgroundImage = 'url(/images/choose-login/logo.png)';
+  // onMouseLeaveHandler() {
+  //   if (this.state.enterTimeout) {
+  //     clearTimeout(this.state.enterTimeout);
+  //   }
+  //   const {style} = this.state;
+  //   let homeIcon = this.state.ref.current;
+  //   homeIcon.style.marginTop = 0;
+  //   homeIcon.style.marginLeft = 0;
+  //   homeIcon.style.marginTop = style.animateMarginTop;
+  //   homeIcon.style.marginLeft = style.animateMarginLeft;
+  //   homeIcon.style.height = style.animateHeight;
+  //   homeIcon.style.width = style.animateWidth;
+  //   homeIcon.style.backgroundImage = 'url(/images/choose-login/logo.png)';
 
-    const leaveTimeout = setTimeout(() => {
-      homeIcon.style.transition = 'none';
-      homeIcon.style.height = style.baseSize;
-      homeIcon.style.width = style.baseSize;
-      homeIcon.style.marginTop = 0;
-      homeIcon.style.marginLeft = 0;
-      homeIcon.style.transition = style.transaction;
-      this.setState({...this.state, leaveTimeout: null});
-    }, 100);
-    this.setState({...this.state, leaveTimeout})
-  }
+  //   const leaveTimeout = setTimeout(() => {
+  //     homeIcon.style.transition = 'none';
+  //     homeIcon.style.height = style.baseSize;
+  //     homeIcon.style.width = style.baseSize;
+  //     homeIcon.style.marginTop = 0;
+  //     homeIcon.style.marginLeft = 0;
+  //     homeIcon.style.transition = style.transaction;
+  //     this.setState({...this.state, leaveTimeout: null});
+  //   }, 100);
+  //   this.setState({...this.state, leaveTimeout})
+  // }
 
   onClick () {
     if (this.props.onClick) {
@@ -102,20 +103,20 @@ class HomeButtonComponent extends React.Component<any, HomeButtonState> {
 
   render() {
     return (
-      <Grid item style={{width: '7.65vw'}}>
-        <Grid container direction="row">
-          <Grid item className="home-button-container">
-            <div
-              className="home-button"
-              onMouseEnter={() => this.onMouseEnterHandler()}
-              onMouseLeave={() => this.onMouseLeaveHandler()}
-              onClick={() => this.onClick()}
-            >
-              <div ref={this.state.ref} />
-            </div>
-          </Grid>
-        </Grid>
-      </Grid>
+			<Grid container direction="row" className="home-button-container">
+				<button type="button" className="btn btn-transparent svgOnHover home-button"
+					// onMouseEnter={() => this.onMouseEnterHandler()}
+					// onMouseLeave={() => this.onMouseLeaveHandler()}
+					onClick={() => this.onClick()}>
+					{/* <div ref={this.state.ref} /> */}\
+					<svg className="svg svg-default">
+						<use href={sprite + "#logo"}/>
+					</svg>
+					<svg className="svg colored">
+						<use href={sprite + "#logo-hover"}/>
+					</svg>
+				</button>
+			</Grid>
     );
   }
 }
