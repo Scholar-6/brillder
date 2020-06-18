0256fb0d46055e1fda7d4ad0cc67cccb5841f86f
ivan <=>
2020-06-18 14:03:28 +0300

    sidebar fotter buttons updated in live

--- src/components/play/brick/introduction/Introduction.tsx
+++ src/components/play/brick/introduction/Introduction.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { Grid } from "@material-ui/core";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import sprite from "../../../../assets/img/icons-sprite.svg";
 
 import "./Introduction.scss";
