d4151544b6e580429c41be227ae8c6daad1d701e
ivan <=>
2020-06-25 12:56:42 +0300

    Final score page updaete

--- src/components/play/brick/ending/Ending.tsx
+++ src/components/play/brick/ending/Ending.tsx
@@ -1,16 +1,13 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { CircularProgressbar } from 'react-circular-progressbar';
 
-import React from 'react';
-import { Grid, Fab, FormControlLabel } from '@material-ui/core';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import CircularProgress from '@material-ui/core/CircularProgress';
-
-import './Ending.scss';
-import { Brick } from 'model/brick';
-import { useHistory } from 'react-router-dom';
-import OtherInformation from '../baseComponents/OtherInformation';
-import { PlayStatus } from '../model/model';
-import { BrickAttempt } from '../PlayBrickRouting';
-
+import "./Ending.scss";
+import { Brick } from "model/brick";
+import { useHistory } from "react-router-dom";
+import { PlayStatus } from "../model/model";
+import { BrickAttempt } from "../PlayBrickRouting";
+import sprite from "../../../../assets/img/icons-sprite.svg";
 
 interface EndingProps {
   status: PlayStatus;
@@ -19,75 +16,83 @@ interface EndingProps {
   saveBrick(): void;
 }
 
-interface EndingState {
-  otherExpanded: boolean;
-}
-
-const EndingPage: React.FC<EndingProps> = ({ status, brick, brickAttempt, saveBrick }) => {
+const EndingPage: React.FC<EndingProps> = ({
+  status,
+  brick,
+  brickAttempt,
+  saveBrick,
+}) => {
   const history = useHistory();
   if (status === PlayStatus.Live) {
     history.push(`/play/brick/${brick.id}/intro`);
   }
 
-  const [state, setState] = React.useState({
-    otherExpanded: false,
-  } as EndingState);
-
-  const toggleOther = () => {
-    setState({ ...state, otherExpanded: !state.otherExpanded });
-  }
-
   const endBrick = () => {
     saveBrick();
-  }
+  };
 
   return (
-    <Grid container direction="row" justify="center">
-      <div className="brick-container">
-        <div className='ending-page'>
-          <div>
-            <h3>{brick.brickLength} minutes</h3>
-            <h1>{brick.title}</h1>
-          </div>
-          <Grid container justify="center" className="circle-progress-container">
-            <CircularProgress
-              variant="static"
-              className="circle-progress"
-              value={(brickAttempt.score * 100) / brickAttempt.maxScore} />
-            <div className="score-data">
-              <Grid container justify="center" alignContent="center">
-                <div>
-                  <div className="score-precentage">
-                    {Math.round((brickAttempt.score * 100) / brickAttempt.maxScore)}%
+    <div className="brick-container ending-page">
+      <Grid container direction="row">
+        <Grid item xs={8}>
+          <div className="brick-container">
+            <h1>Final Score : Agg.</h1>
+            <Grid
+              container
+              justify="center"
+              className="circle-progress-container"
+            >
+              <CircularProgressbar
+                className="circle-progress"
+                counterClockwise={true}
+                value={(brickAttempt.score * 100) / brickAttempt.maxScore}
+              />
+              <div className="score-data">
+                <Grid container justify="center" alignContent="center">
+                  <div>
+                    <div className="score-precentage">
+                      {Math.round(
+                        (brickAttempt.score * 100) / brickAttempt.maxScore
+                      )}
+                      %
+                    </div>
+                    <div className="score-number">
+                      {brickAttempt.score}/{brickAttempt.maxScore}
+                    </div>
                   </div>
-                  <div className="score-number">{brickAttempt.score}/{brickAttempt.maxScore}</div>
-                </div>
-              </Grid>
-            </div>
-          </Grid>
-          <div className="begin-row">
-            <FormControlLabel
-              className="start-brick-button"
-              labelPlacement="start"
-              control={
-                <Fab style={{ background: '#0076B4' }} color="secondary" aria-label="add" onClick={endBrick}>
-                  <PlayArrowIcon />
-                </Fab>
-              }
-              label="Return to Dashboard"
-            />
+                </Grid>
+              </div>
+            </Grid>
           </div>
-          <OtherInformation
-            creator={`${brick.author.firstName} ${brick.author.lastName}`}
-            expanded={state.otherExpanded}
-            toggle={toggleOther}
-            totalUsers={0}
-            averageScore={0}
-            highScore={0}/>
-        </div>
-      </div>
-    </Grid>
+        </Grid>
+        <Grid item xs={4}>
+          <div className="introduction-info">
+            <div className="intro-text-row"></div>
+            <div className="action-footer">
+              <div>&nbsp;</div>
+              <div className="direction-info">
+                <h2>Summary</h2>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="play-preview svgOnHover play-green"
+                  onClick={endBrick}
+                >
+                  <svg className="svg svg-default m-l-02">
+                    <use href={sprite + "#play-thin"} />
+                  </svg>
+                  <svg className="svg colored m-l-02">
+                    <use href={sprite + "#play-thick"} />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
-}
+};
 
 export default EndingPage;
