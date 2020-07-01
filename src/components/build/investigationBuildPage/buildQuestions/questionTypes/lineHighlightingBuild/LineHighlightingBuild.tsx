263237762871f5ac8e3a88a894de57ff25b8601a
William Gooch <40644139+william-gooch@users.noreply.github.com>
2020-07-01 13:10:19 +0100

    Fix some indentation

--- src/components/build/investigationBuildPage/buildQuestions/questionTypes/lineHighlightingBuild/LineHighlightingBuild.tsx
+++ src/components/build/investigationBuildPage/buildQuestions/questionTypes/lineHighlightingBuild/LineHighlightingBuild.tsx
@@ -111,10 +111,10 @@ const LineHighlightingComponent: React.FC<LineHighlightingProps> = ({
         <div>Enter/Paste Text Below.</div>
         <div>Use Highlighter Icon to click correct line(s).</div>
       </div>
-			<div className="pencil-icon-container svgOnHover" onClick={switchMode}>
-				<svg className="svg w100 h100 active">
-					<use href={sprite + "#highlighter"} className={state.mode ? "text-theme-green" : "text-theme-dark-blue"} />
-				</svg>
+      <div className="pencil-icon-container svgOnHover" onClick={switchMode}>
+        <svg className="svg w100 h100 active">
+          <use href={sprite + "#highlighter"} className={state.mode ? "text-theme-green" : "text-theme-dark-blue"} />
+        </svg>
       </div>
       <div className="input-container">
         {renderBox()}
