1788ea719028f233a409cb71ebc705175216c72d
William Gooch <william.gooch.wg@gmail.com>
2020-07-07 12:57:15 +0100

    Add colours for notification types.

--- src/model/notifications.ts
+++ src/model/notifications.ts
@@ -4,10 +4,27 @@ export interface Sender {
     lastName: string;
 }
 
+export enum NotificationType {
+    Generic,
+    BrickSubmittedForReview,
+    BrickPublished,
+    AssignedToEdit,
+    NewCommentOnBrick
+}
+
 export interface Notification {
     id: number;
     sender: Sender;
     title: string;
     text: string;
+    type: NotificationType;
     read: boolean;
 }
+
+export const notificationTypeColors = {
+    [NotificationType.Generic]: "#bdc2c8",
+    [NotificationType.BrickSubmittedForReview]: "#ff9800",
+    [NotificationType.BrickPublished]: "#00c86b",
+    [NotificationType.AssignedToEdit]: "#d42c24",
+    [NotificationType.NewCommentOnBrick]: "#006bfd",
+};
