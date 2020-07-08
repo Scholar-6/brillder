546bee08d3b66f63252cb4e154aa8368bf51e794
ivan <=>
2020-07-08 17:02:33 +0300

    working on Carousel

--- src/components/build/mainPage/mainPage.tsx
+++ src/components/build/mainPage/mainPage.tsx
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // @ts-ignore
-import { connect } from 'react-redux';
-import { Grid, Hidden } from '@material-ui/core';
+import { connect } from "react-redux";
+import { Grid, Hidden } from "@material-ui/core";
 
-import './mainPage.scss';
-import actions from 'redux/actions/auth';
-import brickActions from 'redux/actions/brickActions';
-import { User } from 'model/user';
-import { ReduxCombinedState } from 'redux/reducers';
+import "./mainPage.scss";
+import actions from "redux/actions/auth";
+import brickActions from "redux/actions/brickActions";
+import { User } from "model/user";
+import { ReduxCombinedState } from "redux/reducers";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
 
-
-const mapState = (state: ReduxCombinedState) => ({ user: state.user.user })
+const mapState = (state: ReduxCombinedState) => ({ user: state.user.user });
 
 const mapDispatch = (dispatch: any) => ({
   forgetBrick: () => dispatch(brickActions.forgetBrick()),
-  logout: () => dispatch(actions.logout())
-})
+  logout: () => dispatch(actions.logout()),
+});
 
-const connector = connect(
-  mapState,
-  mapDispatch
-);
+const connector = connect(mapState, mapDispatch);
 
 interface MainPageProps {
   history: any;
@@ -37,23 +40,27 @@ interface MainPageState {
 }
 
 class MainPage extends Component<MainPageProps, MainPageState> {
-  constructor(props:any) {
+  constructor(props: any) {
     super(props);
-
 
     this.state = {
       viewHover: false,
       createHober: false,
       backHober: false,
-      animatedName: '',
+      animatedName: "",
     } as any;
 
     let count = 0;
-    let nameToFill = props.user.firstName ? props.user.firstName as string : 'NAME';
+    let nameToFill = props.user.firstName
+      ? (props.user.firstName as string)
+      : "NAME";
     let maxCount = nameToFill.length - 1;
 
     let setNameInterval = setInterval(() => {
-      this.setState({...this.state, animatedName: this.state.animatedName + nameToFill[count]});
+      this.setState({
+        ...this.state,
+        animatedName: this.state.animatedName + nameToFill[count],
+      });
       if (count >= maxCount) {
         clearInterval(setNameInterval);
       }
@@ -62,45 +69,146 @@ class MainPage extends Component<MainPageProps, MainPageState> {
   }
 
   viewHoverToggle(viewHover: boolean) {
-    this.setState({viewHover});
+    this.setState({ viewHover });
   }
 
   creatingBrick() {
     this.props.forgetBrick();
-    this.props.history.push('/build/new-brick/subject');
+    this.props.history.push("/build/new-brick/subject");
+  }
+
+  renderViewAllButton() {
+    return (
+      <Grid container justify="center" style={{ width: "100%" }}>
+        <div
+          className="zoom-item view-item"
+          onClick={() => this.props.history.push("/play/dashboard")}
+        >
+          <img
+            alt="Logo"
+            src="/images/main-page/glasses.png"
+            className="item-image"
+          />
+          <div className="item-description">View All Bricks</div>
+        </div>
+      </Grid>
+    );
+  }
+
+  renderCreateButton() {
+    return (
+      <Grid container justify="center" style={{ width: "100%" }}>
+        <div
+          className="zoom-item create-item"
+          onClick={() =>
+            this.props.history.push("/build/new-brick/brick-title")
+          }
+        >
+          <img
+            alt="Logo"
+            src="/images/main-page/create.png"
+            className="item-image"
+          />
+          <div className="item-description">Start Building</div>
+        </div>
+      </Grid>
+    );
+  }
+
+  renderWorkButton() {
+    return (
+      <Grid container justify="center" style={{ width: "100%" }}>
+        <div
+          className="zoom-item back-item"
+          onClick={() => this.props.history.push("/back-to-work")}
+        >
+          <img
+            alt="Logo"
+            src="/images/main-page/backToWork.png"
+            className="item-image"
+          />
+          <div className="item-description">Back To Work</div>
+        </div>
+      </Grid>
+    );
+  }
+
+  renderMobilePage() {
+    return (
+      <Hidden only={["sm", "md", "lg", "xl"]}>
+        <div className="mobile-main-page">
+          <CarouselProvider
+            naturalSlideWidth={100}
+            orientation="vertical"
+            naturalSlideHeight={125}
+            infinite={true}
+            totalSlides={3}
+          >
+            <Slider>
+              <Slide index={0}>{this.renderViewAllButton()}</Slide>
+              <Slide index={1}>{this.renderCreateButton()}</Slide>
+              <Slide index={2}>{this.renderWorkButton()}</Slide>
+            </Slider>
+          </CarouselProvider>
+        </div>
+      </Hidden>
+    );
   }
 
   render() {
-    const {history} = this.props;
+    const { history } = this.props;
     return (
       <Grid container direction="row" className="mainPage">
-        <Hidden only={['xs']}>
+        <Hidden only={["xs"]}>
           <div className="welcome-col">
             <div className="welcome-box">
               <div>WELCOME</div>
               <div className="smaller">TO BRILLDER,</div>
-              <div className="welcome-name">
-                {this.state.animatedName}
-              </div>
+              <div className="welcome-name">{this.state.animatedName}</div>
             </div>
           </div>
           <div className="first-col">
             <div className="first-item">
               <Grid container justify="center" className="view-item-container">
-                <div className="zoom-item view-item" onClick={() => history.push('/play/dashboard')}>
-                  <img alt="Logo" src="/images/main-page/glasses.png" className="item-image" />
+                <div
+                  className="zoom-item view-item"
+                  onClick={() => history.push("/play/dashboard")}
+                >
+                  <img
+                    alt="Logo"
+                    src="/images/main-page/glasses.png"
+                    className="item-image"
+                  />
                   <div className="item-description">View All Bricks</div>
                 </div>
               </Grid>
-              <Grid container justify="center" className="create-item-container">
-                <div className="zoom-item create-item" onClick={() => this.creatingBrick()}>
-                  <img alt="Logo" src="/images/main-page/create.png" className="item-image" />
+              <Grid
+                container
+                justify="center"
+                className="create-item-container"
+              >
+                <div
+                  className="zoom-item create-item"
+                  onClick={() => this.creatingBrick()}
+                >
+                  <img
+                    alt="Logo"
+                    src="/images/main-page/create.png"
+                    className="item-image"
+                  />
                   <div className="item-description">Start Building</div>
                 </div>
               </Grid>
               <Grid container justify="center" className="back-item-container">
-                <div className="zoom-item back-item" onClick={() => history.push('/back-to-work')}>
-                  <img alt="Logo" src="/images/main-page/backToWork.png" className="item-image" />
+                <div
+                  className="zoom-item back-item"
+                  onClick={() => history.push("/back-to-work")}
+                >
+                  <img
+                    alt="Logo"
+                    src="/images/main-page/backToWork.png"
+                    className="item-image"
+                  />
                   <div className="item-description">Back To Work</div>
                 </div>
               </Grid>
@@ -113,68 +221,25 @@ class MainPage extends Component<MainPageProps, MainPageState> {
           </div>
           <div className="logout-button" onClick={this.props.logout}>
             <Grid container alignContent="center">
-              <div style={{position: 'relative'}}>
+              <div style={{ position: "relative" }}>
                 <div className="logout-image" />
               </div>
               <div>
-                <Grid container alignContent="center" style={{height: '100%'}}>
+                <Grid
+                  container
+                  alignContent="center"
+                  style={{ height: "100%" }}
+                >
                   <span className="logout-text">LOGOUT</span>
                 </Grid>
               </div>
             </Grid>
           </div>
         </Hidden>
-        <Hidden only={['sm', 'md', 'lg', 'xl']}>
-          <div className="mobile-main-page-background">
-            <Grid container direction="row" className="first-mobile-row">
-              <div className="first-col"></div>
-              <div className="second-col"></div>
-            </Grid>
-            <Grid container direction="row" className="second-mobile-row">
-            <div className="first-col"></div>
-              <div className="second-col"></div>
-            </Grid>
-            <Grid container direction="row" className="third-mobile-row">
-              <div className="first-col"></div>
-              <div className="second-col"></div>
-            </Grid>
-          </div>
-          <div className="mobile-main-page">
-            <div className="mobile-logout-button" onClick={this.props.logout}>
-              <Grid container alignContent="center" justify="flex-end">
-                <div>
-                  <img className="logout-image image-bottom" alt="logout" src="/images/main-page/logout.png" />
-                </div>
-                <div>
-                  <Grid container alignContent="center" style={{height: '100%'}}>
-                    <span>LOGOUT</span>
-                  </Grid>
-                </div>
-              </Grid>
-            </div>
-            <Grid container justify="center" style={{width: "100%"}}>
-              <div className="zoom-item view-item" onClick={() => history.push('/play/dashboard')}>
-                <img alt="Logo" src="/images/main-page/glasses.png" className="item-image" />
-                <div className="item-description">View All Bricks</div>
-              </div>
-            </Grid>
-            <Grid container justify="center" style={{width: "100%"}}>
-              <div className="zoom-item create-item" onClick={() => history.push('/build/new-brick/brick-title')}>
-                <img alt="Logo" src="/images/main-page/create.png" className="item-image" />
-                <div className="item-description">Start Building</div>
-              </div>
-            </Grid>
-            <Grid container justify="center" style={{width: "100%"}}>
-              <div className="zoom-item back-item" onClick={() => history.push('/back-to-work')}>
-                <img alt="Logo" src="/images/main-page/backToWork.png" className="item-image" />
-                <div className="item-description">Back To Work</div>
-              </div>
-            </Grid>
-          </div>
-        </Hidden>
+        {this.renderMobilePage()}
       </Grid>
-    )
+    );
   }
 }
 
-export default connector(MainPage)
+export default connector(MainPage);
