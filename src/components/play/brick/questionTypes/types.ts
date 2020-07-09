367b4b091a0a5e6e26402963bb0fbe134da236f5
Farman Ahmed <farmanahmed2007@gmail.com>
2020-07-09 17:08:01 +0500

    resolved #1123, #1024, #1179, #1126

--- src/components/play/brick/questionTypes/types.ts
+++ src/components/play/brick/questionTypes/types.ts
@@ -1,6 +1,8 @@
 import { Question } from "model/question";
 import {ComponentAttempt} from '../model/model';
 
+import './questionTypes.scss';
+
 
 export interface CompQuestionProps {
   question: Question;
