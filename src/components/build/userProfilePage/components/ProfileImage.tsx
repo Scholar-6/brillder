ab0564787f233843da05aacf9fb84486fa3a7704
ivan <=>
2020-07-30 18:48:14 +0300

    Uploading profile image

--- src/components/build/userProfilePage/components/ProfileImage.tsx
+++ src/components/build/userProfilePage/components/ProfileImage.tsx
@@ -1,18 +1,56 @@
 import React from "react";
-import sprite from "assets/img/icons-sprite.svg";
 
-interface ProfileImageProps {}
+import sprite from "assets/img/icons-sprite.svg";
+import { uploadFile } from "components/services/uploadFile";
+
+interface ProfileImageProps {
+  profileImage: string;
+  setImage(profileImage: string): void;
+}
 
 const ProfileImage: React.FC<ProfileImageProps> = (props) => {
+  const openUploadDialog = () => {
+    const el = document.createElement("input");
+    el.setAttribute("type", "file");
+    el.setAttribute("accept", ".jpg, .jpeg, .png");
+    el.click();
+
+    // UPLOADING IMAGE TO BACKEND
+    el.onchange = () => {
+      if (el.files && el.files.length >= 0) {
+        uploadFile(
+          el.files[0] as File,
+          (res: any) => {
+            const fileName = res.data.fileName;
+            props.setImage(fileName);
+          },
+          () => {}
+        );
+      }
+    };
+  };
+
+  const renderImage = () => {
+    if (props.profileImage) {
+      return (
+        <img
+          src={`${process.env.REACT_APP_BACKEND_HOST}/files/${props.profileImage}`}
+          className="real-profile-image"
+        />
+      );
+    }
+    return (
+      <svg className="svg active">
+        {/*eslint-disable-next-line*/}
+        <use href={sprite + "#user"} className="text-theme-dark-blue" />
+      </svg>
+    );
+  };
+
   return (
     <div className="profile-image-container">
-      <div className="profile-image svgOnHover">
-        <svg className="svg active">
-          {/*eslint-disable-next-line*/}
-          <use href={sprite + "#user"} className="text-theme-dark-blue" />
-        </svg>
-      </div>
-      <div className="add-image-button svgOnHover">
+      <div className="profile-image svgOnHover">{renderImage()}</div>
+      <div className="add-image-button svgOnHover" onClick={openUploadDialog}>
         <svg className="svg active">
           {/*eslint-disable-next-line*/}
           <use href={sprite + "#plus"} className="text-white" />
