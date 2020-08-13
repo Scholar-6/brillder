8df1f88e2e8c652025d3872275ea555291937346
ivanromakh <ivanrouman@gmail.com>
2020-08-13 13:46:31 +0300

    Cropping image in profile page

--- src/components/userProfilePage/components/ProfileImage.tsx
+++ src/components/userProfilePage/components/ProfileImage.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+// @ts-ignore
+import DropNCrop from '@synapsestudios/react-drop-n-crop';
+import Dialog from "@material-ui/core/Dialog";
+import '@synapsestudios/react-drop-n-crop/lib/react-drop-n-crop.min.css';
+
 
 import sprite from "assets/img/icons-sprite.svg";
 import { uploadFile } from "components/services/uploadFile";
@@ -10,7 +15,22 @@ interface ProfileImageProps {
 }
 
 const ProfileImage: React.FC<ProfileImageProps> = (props) => {
-  const {profileImage} = props;
+  const [state, setState] = React.useState({
+    result: null,
+    filename: null,
+    filetype: null,
+    src: null,
+    error: null,
+  })
+  const [isUploadOpen, setUploadDialog] = React.useState(false);
+  const [uploadedImage, setUploadedImage] = React.useState(null as any);
+  const [cropData, setCropData] = React.useState({
+    isOpen: false,
+    fileName: '',
+    image: {} as any,
+    crop: undefined as any
+  })
+  const { profileImage } = props;
   const handleClick = () => {
     if (profileImage) {
       props.setImage('');
@@ -20,6 +40,8 @@ const ProfileImage: React.FC<ProfileImageProps> = (props) => {
   }
 
   const openUploadDialog = () => {
+    setUploadDialog(true);
+    /*
     const el = document.createElement("input");
     el.setAttribute("type", "file");
     el.setAttribute("accept", ".jpg, .jpeg, .png");
@@ -28,17 +50,48 @@ const ProfileImage: React.FC<ProfileImageProps> = (props) => {
     // UPLOADING IMAGE TO BACKEND
     el.onchange = () => {
       if (el.files && el.files.length >= 0) {
+        setUploadedImage(el.files[0]);
+
         uploadFile(
           el.files[0] as File,
           (res: any) => {
             const fileName = res.data.fileName;
-            props.setImage(fileName);
+            //props.setImage(fileName);
+            setCropData({ ...cropData, crop: undefined, isOpen: true, fileName});
           },
-          () => {}
+          () => { }
         );
       }
     };
+    */
   };
+
+  const dataURLtoFile = (dataurl: string, filename: string) => {
+    var arr = dataurl.split(',') as any;
+    var mime = arr[0].match(/:(.*?);/)[1];
+    var bstr = atob(arr[1]);
+    var n = bstr.length;
+    var u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+  }
+
+  const uploadCropedFile = () => {
+    let file = dataURLtoFile(state.result as any, state.filename as any);
+    uploadFile(
+      file as File,
+      (res: any) => {
+        const fileName = res.data.fileName;
+        props.setImage(fileName);
+        setUploadDialog(false);
+      },
+      () => { }
+    );
+  }
 
   const renderImage = () => {
     let className = "real-profile-image"
@@ -77,9 +130,22 @@ const ProfileImage: React.FC<ProfileImageProps> = (props) => {
         </svg>
         <span>Active</span>
       </div>
+      <Dialog
+        open={isUploadOpen}
+        onClose={() => setUploadDialog(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        className="dialog-box"
+      >
+        <div className="dialog-header">
+          <DropNCrop onChange={(value: any) => setState(value)} value={state} />
+        </div>
+        <div className="dialog-footer">
+          <button onClick={uploadCropedFile}>Save</button>
+        </div>
+      </Dialog>
     </div>
   );
 };
 
 export default ProfileImage;
-
