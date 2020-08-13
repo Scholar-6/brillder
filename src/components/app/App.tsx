d0bc86dd3145cc9ac0e2d4d0d264ea98d98008f4
ivanromakh <ivanrouman@gmail.com>
2020-08-13 12:25:06 +0300

    Fix path

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -21,7 +21,7 @@ import ChooseLoginPage from '../authPages/chooseLoginPage/ChooseLoginPage';
 import SubmitBrickPage from '../build/investigationBuildPage/submit/SubmitPage';
 import PublishBrickPage from '../build/investigationBuildPage/publish/PublishPage';
 import UserProfilePage from '../userProfilePage/UserProfile';
-import ManageClassrooms from 'components/manageClassrooms/ManageClassrooms';
+import ManageClassrooms from 'components/teach/manageClassrooms/ManageClassrooms';
 
 import AuthRoute from './AuthRoute';
 import BuildRoute from './BuildRoute';
