f5fcfd2147aa08061269d511a913846afc22478e
ivanromakh <ivanrouman@gmail.com>
2020-08-21 12:25:43 +0300

    Working on teach

---
+++ src/components/build/backToWorkPage/components/AssignedBrickDescription.tsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+import { getAuthorRow } from "components/services/brickService";
+import { Brick } from "model/brick";
+import './AssignedBrickDescription.scss';
+import sprite from "assets/img/icons-sprite.svg";
+
+interface AssignedDescriptionProps {
+  brick: Brick;
+  index?: number;
+  color?: string;
+  isMobile?: boolean;
+  isExpanded?: boolean;
+  onClick?(): void;
+  move?(): void;
+}
+
+class AssignedBrickDescription extends Component<AssignedDescriptionProps> {
+  renderCircle(color: string) {
+    return (
+      <div className="left-brick-circle">
+        <div className="round-button" style={{ background: color }} />
+      </div>
+    );
+  }
+
+  renderPlayButton() {
+    return (
+      <div className="play-button-link svgOnHover" onClick={() => this.props.move ? this.props.move() : {}}>
+        <svg className="svg w100 h100 svg-default">
+          {/*eslint-disable-next-line*/}
+          <use href={sprite + "#play-thin"} className="text-gray" />
+        </svg>
+        <svg className="svg w100 h100 colored">
+          {/*eslint-disable-next-line*/}
+          <use href={sprite + "#play-thick"} className="text-gray" />
+        </svg>
+      </div>
+    )
+  }
+
+  render() {
+    let { color, brick, isMobile, isExpanded, index } = this.props;
+    let className = "assigned-brick-description";
+
+    if (isMobile && isExpanded) {
+      className += " mobile-expanded";
+    }
+    if (index !== undefined && index >= 0) {
+      className += " mobile-short-" + index;
+    }
+
+
+    if (!color) {
+      color = "#B0B0AD";
+    }
+
+    return (
+      <div className={className} onClick={() => this.props.onClick ? this.props.onClick() : {}}>
+        <div>3</div>
+        {this.renderCircle(color)}
+        <div className="short-brick-info">
+          <div className="link-description">
+            <span>{brick.title}</span>
+            </div>
+          <div className="link-info">
+            {brick.subTopic} | {brick.alternativeTopics}
+          </div>
+          <div className="link-info">{getAuthorRow(brick)}</div>
+        </div>
+        {isExpanded ? this.renderPlayButton() : ""}
+      </div>
+    );
+  }
+}
+
+export default AssignedBrickDescription;
--- src/components/build/backToWorkPage/components/ClassroomList.tsx
+++ src/components/build/backToWorkPage/components/ClassroomList.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { TeachClassroom, } from "model/classroom";
+import AssignedBrickDescription from "./AssignedBrickDescription";
 
 
 interface ClassroomListProps {
@@ -15,7 +16,13 @@ class ClassroomList extends Component<ClassroomListProps, ClassroomListState> {
   render() {
     return (
       <div className="classroom-list">
-        {this.props.classrooms.map((c, i) => <div className="classroom-title" key={i}>{c.name}</div>)}
+        {this.props.classrooms.map((c, i) => 
+          <div className="classroom-title" key={i}>
+            {c.name}
+            {}
+            {c.assignments.map(a => <AssignedBrickDescription brick={a.brick} />)}
+          </div>
+        )}
       </div>
     );
   }
