0edbf36a66f5445fce2a534b40dcac8d663e1da4
ivanromakh <ivanrouman@gmail.com>
2020-08-21 15:06:06 +0300

    teach buttons

--- src/components/build/backToWorkPage/components/teach/ClassroomList.tsx
+++ src/components/build/backToWorkPage/components/teach/ClassroomList.tsx
@@ -8,14 +8,34 @@ interface ClassroomListProps {
   classrooms: TeachClassroom[];
 }
 interface ClassroomListState {
-  activeClassroom: TeachClassroom | null;
-  filterExpanded: boolean;
+  isArchive: boolean
 }
 
 class ClassroomList extends Component<ClassroomListProps, ClassroomListState> {
+  constructor(props: ClassroomListProps) {
+    super(props);
+    this.state = {
+      isArchive: false
+    }
+  }
+
+  renderArchiveButton() {
+    let className = this.state.isArchive ? "active" : "";
+    return <div className={className} onClick={() =>this.setState({isArchive: true})}>ARCHIVE</div>;
+  }
+
+  renderLiveBricksButton() {
+    let className = this.state.isArchive ? "" : "active";
+    return <div className={className} onClick={() => this.setState({isArchive: false})}>LIVE BRICKS</div>;
+  }
+
   render() {
     return (
       <div className="classroom-list">
+        <div className="classroom-list-buttons">
+          {this.renderLiveBricksButton()}
+          {this.renderArchiveButton()}
+        </div>
         {this.props.classrooms.map((c, i) => 
           <div className="classroom-title" key={i}>
             {c.name}
