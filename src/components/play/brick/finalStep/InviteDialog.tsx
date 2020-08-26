db6f1dbcdc14bba527fddee0ea7960628feb7953
ivanromakh <ivanrouman@gmail.com>
2020-08-26 17:33:50 +0300

    copy link

--- src/components/play/brick/finalStep/InviteDialog.tsx
+++ src/components/play/brick/finalStep/InviteDialog.tsx
@@ -10,6 +10,15 @@ interface InviteProps {
 }
 
 const InviteDialog: React.FC<InviteProps> = props => {
+  const copyToClipboard = () => {
+    var linkEl = document.getElementById('invite-link') as HTMLInputElement;
+    if (linkEl) {
+      linkEl.select();
+      linkEl.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+    }
+  }
+
   return (
     <Dialog open={props.isOpen} onClose={props.close} className="dialog-box light-blue">
       <div className="close-button">
@@ -21,9 +30,9 @@ const InviteDialog: React.FC<InviteProps> = props => {
       <div className="dialog-header">
         <div>{props.link}</div>
       </div>
-      <div className="dialog-footer">
-        <button className="btn btn-md bg-theme-orange yes-button"
-          onClick={props.close}>
+      <input id="invite-link" style={{ opacity: 0, height: 0 }} value={props.link} />
+      <div className="dialog-footer" style={{justifyContent: 'center'}}>
+        <button className="btn btn-md bg-theme-orange yes-button" style={{width: 'auto'}} onClick={copyToClipboard}>
           <span>Copy link</span>
         </button>
       </div>
