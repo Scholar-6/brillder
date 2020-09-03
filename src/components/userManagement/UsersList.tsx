902f42ce1aaf0a421c780d99cdfb097c610e7aab
ivan <=>
2020-09-03 13:27:51 +0300

    remove old toggle

--- src/components/userManagement/UsersList.tsx
+++ src/components/userManagement/UsersList.tsx
@@ -1,8 +1,6 @@
 import "./UsersList.scss";
 import React, { Component } from "react";
 import { Grid, FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import Switch from "@material-ui/core/Switch";
 import axios from "axios";
 import { connect } from "react-redux";
 import grey from "@material-ui/core/colors/grey";
@@ -71,53 +69,6 @@ interface UsersListState {
   isDeleteDialogOpen: boolean;
   deleteUserId: number;
 }
-
-let anyStyles = withStyles as any;
-
-const IOSSwitch = anyStyles((theme: any) => ({
-  root: {
-    width: "4.5vh",
-    height: "2.8vh",
-    padding: 0,
-    margin: 0,
-  },
-  switchBase: {
-    padding: 1,
-    "&$checked": {
-      transform: "translateX(16px)",
-      color: "#30C474",
-      "& + $track": {
-        borderRadius: "2vh",
-        border: "0.25vh solid #001C58",
-        height: "2.3vh",
-        backgroundColor: "#30C474",
-        opacity: 1,
-      },
-    },
-    "&$focusVisible $thumb": {
-      color: "#52d869",
-      border: "6px solid #fff",
-    },
-  },
-  thumb: {
-    marginTop: "0.6vh",
-    marginLeft: "0.4vh",
-    marginRight: "0.4vh",
-    border: "0.2vh solid #001C58",
-    width: "0.9vh",
-    height: "0.9vh",
-  },
-  track: {
-    borderRadius: "2vh",
-    border: "0.25vh solid #001C58",
-    height: "2.3vh",
-    backgroundColor: grey[50],
-    opacity: 1,
-  },
-  checked: {},
-}))((props: any) => {
-  return <Switch disableRipple {...props} />;
-});
 
 class UsersListPage extends Component<UsersListProps, UsersListState> {
   constructor(props: UsersListProps) {
