c1b6d3050838a7fec27eb440dd14f52f043c898e
Farman Ahmed <farmanahmed2007@gmail.com>
2020-09-03 18:49:18 +0500

    fixed issues in #344

--- src/components/userManagement/CustomToggle.tsx
+++ src/components/userManagement/CustomToggle.tsx
@@ -9,7 +9,7 @@ interface ToggleProps {
 const CustomToggle: React.FC<ToggleProps> = (props) => {
   let className = "no-reverse"
   if (props.checked) {
-    className="reverse";
+    className = "reverse";
   }
 
   return (
@@ -17,8 +17,11 @@ const CustomToggle: React.FC<ToggleProps> = (props) => {
       <input type="checkbox" name="round" />
       <label onClick={props.onClick} className={className}>
         <svg viewBox="0 0 179.333 61.333">
-          <path
-            className="box"
+          <path className="base-box"
+            d="M153.142,30.81 c0,13.807-11.192,25-25,25H31c-13.807,0-25-11.193-25-25l0,0c0-13.807,11.193-25,25-25h97.143
+               C141.95,5.81,153.142,17.002,153.142,30.81L153.142,30.81z"
+          />
+          <path className="box"
             d="M153.142,30.81 c0,13.807-11.192,25-25,25H31c-13.807,0-25-11.193-25-25l0,0c0-13.807,11.193-25,25-25h97.143
                C141.95,5.81,153.142,17.002,153.142,30.81L153.142,30.81z"
           />
