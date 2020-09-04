1b28488e6c607a78930e245cf8fff2267681345a
ivan <=>
2020-09-04 15:58:29 +0300

    #1604 remove emails from manage classroom

--- src/components/teach/manageClassrooms/components/StudentTable.tsx
+++ src/components/teach/manageClassrooms/components/StudentTable.tsx
@@ -70,7 +70,6 @@ const StudentTable: React.FC<StudentTableProps> = props => {
             {renderSortArrow(UserSortBy.Name)}
           </Grid>
         </th>
-        <th className="email-column" style={{ width: '27%' }}>EMAIL</th>
         <th style={{ width: '29%' }}>
           <Grid container>
             CLASSES
@@ -108,7 +107,6 @@ const StudentTable: React.FC<StudentTableProps> = props => {
                   <span className="user-first-name">{user.firstName} </span>
                   <span className="user-last-name">{user.lastName}</span>
                 </td>
-                <td>{user.email}</td>
                 <td>
                   <div className="classroom-names">
                     {user.studyClassrooms ? user.studyClassrooms.map((classroom, i) =>
