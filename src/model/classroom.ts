81e69d273f05639dfa3e0070522835f6a46256ba
ivan <=>
2020-09-11 15:28:58 +0300

    #1482 show students

--- src/model/classroom.ts
+++ src/model/classroom.ts
@@ -2,40 +2,42 @@ import { UserBase } from "./user";
 import { Brick } from "./brick";
 
 export enum ClassroomStatus {
-    Disabled,
-    Active,
-    Deleted
+  Disabled,
+  Active,
+  Deleted
 }
 
 export interface StudentStatus {
-    studentId: number;
-    status: number;
+  studentId: number;
+  status: number;
+  numberOfAttempts: number;
+  avgScore: number;
 }
 
 export interface Assignment {
-    id: number;
-    brick: Brick;
-    assignedDate: string;
+  id: number;
+  brick: Brick;
+  assignedDate: string;
 
-    studentStatus: StudentStatus[];
-    byStatus: any;
-    studentStatusCount: {
-      0: number;
-      1: number;
-      2: number;
-    }
+  studentStatus: StudentStatus[];
+  byStatus: any;
+  studentStatusCount: {
+    0: number;
+    1: number;
+    2: number;
+  }
 }
 
 export interface Classroom {
-    id: number;
-    name: string;
-    created: Date;
-    updated: Date;
-    status: ClassroomStatus;
-    students: UserBase[];
-    teachers: UserBase[];
-    creator: UserBase;
-    assignments: Assignment[];
+  id: number;
+  name: string;
+  created: Date;
+  updated: Date;
+  status: ClassroomStatus;
+  students: UserBase[];
+  teachers: UserBase[];
+  creator: UserBase;
+  assignments: Assignment[];
 }
 
 export interface StudentStatus {
