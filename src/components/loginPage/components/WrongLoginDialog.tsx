4a3d07017611fa2971d800f0f3652eb6349a5dc2
ivan <=>
2020-09-25 14:32:43 +0300

    #1819 fix for logi popup

--- src/components/loginPage/components/WrongLoginDialog.tsx
+++ src/components/loginPage/components/WrongLoginDialog.tsx
@@ -11,14 +11,15 @@ const WrongLoginDialog: React.FC<WrongLoginDialogProps> = props => {
   return (
     <Dialog open={props.isOpen} onClose={props.close} className="dialog-box">
       <div className="dialog-header">
-      <div>We don't appear to have a record of you yet. Sign up</div>
+        <div>We don't appear to have a record of you yet</div>
+        <div>Sign up?</div>
       </div>
       <div className="dialog-footer">
         <button className="btn btn-md bg-theme-orange yes-button" onClick={props.submit}>
           <span>Yes</span>
         </button>
         <button className="btn btn-md bg-gray no-button" onClick={props.close}>
-          <span>Not yet!</span>
+          <span>No</span>
         </button>
       </div>
     </Dialog>
