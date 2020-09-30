654221208cf5a5d5d5037baf2d636bd49d01e410
ivan <=>
2020-09-30 12:54:21 +0300

    #1772 login page updated for mobile

--- src/components/loginPage/MobileLogin.tsx
+++ src/components/loginPage/MobileLogin.tsx
@@ -37,117 +37,98 @@ class MobileLoginPage extends React.Component<MobileLoginProps> {
     }, 300);
   }
 
+  renderPrivacyPolicy() {
+    return (
+      <div className="mobile-policy-text">
+        <span onClick={() => this.props.setPolicyDialog(true)}>
+          Privacy Policy
+        </span>
+      </div>
+    );
+  }
+
+  renderSignInPage() {
+    return (
+      <div className="first-col">
+        <div className="second-item">
+          <div className="logo-box">
+            <svg
+              className="svg active logo-image mobile"
+              onClick={() => this.props.history.push(map.Login)}
+            >
+              {/*eslint-disable-next-line*/}
+              <use href={sprite + "#login"} className="text-theme-orange" />
+            </svg>
+          </div>
+          <form
+            onSubmit={this.props.handleLoginSubmit}
+            className="mobile-button-box content-box"
+          >
+            <div className="input-block">
+              <input
+                type="email"
+                value={this.props.email}
+                onChange={(e) => this.props.setEmail(e.target.value)}
+                className="login-field"
+                required
+                placeholder="Email"
+              />
+            </div>
+            <div className="input-block">
+              <input
+                type={this.props.passwordHidden ? "password" : "text"}
+                value={this.props.password}
+                className="login-field password"
+                onChange={(e) => this.props.setPassword(e.target.value)}
+                required
+                placeholder="Password"
+              />
+              <div className="hide-password-icon-container">
+                <VisibilityIcon
+                  className="hide-password-icon"
+                  onClick={() =>
+                    this.props.setHidden(!this.props.passwordHidden)
+                  }
+                />
+              </div>
+            </div>
+            <div className="input-block button-box">
+              <Button
+                variant="contained"
+                color="primary"
+                className="sign-up-button"
+                type="button"
+                onClick={() =>
+                  this.props.register(this.props.email, this.props.password)
+                }
+              >
+                Sign up
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                className="sign-in-button"
+                type="submit"
+              >
+                Sign in
+              </Button>
+            </div>
+            {this.renderPrivacyPolicy()}
+          </form>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const { loginState } = this.props;
     return (
       <Hidden only={["sm", "md", "lg", "xl"]}>
         {loginState !== LoginState.ChooseLogin &&
         loginState !== LoginState.ChooseLoginAnimation ? (
-          <div style={{width: '100%'}}>
-            <div className="back-col">
-              <div className="back-box">
-                <svg
-                  className="svg active back-button"
-                  onClick={() => this.props.history.push(map.Login)}
-                >
-                  {/*eslint-disable-next-line*/}
-                  <use href={sprite + "#arrow-down"} className="theme-orange" />
-                </svg>
-              </div>
-            </div>
-            <div className="first-col">
-              <div className="second-item">
-                <div className="logo-box">
-                  <svg
-                    className="svg active logo-image mobile"
-                    onClick={() => this.props.history.push(map.Login)}
-                  >
-                    {/*eslint-disable-next-line*/}
-                    <use
-                      href={sprite + "#login"}
-                      className="text-theme-orange"
-                    />
-                  </svg>
-                </div>
-
-                <form
-                  onSubmit={this.props.handleLoginSubmit}
-                  className="mobile-button-box content-box"
-                >
-                  <div className="input-block">
-                    <input
-                      type="email"
-                      value={this.props.email}
-                      onChange={(e) => this.props.setEmail(e.target.value)}
-                      className="login-field"
-                      required
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="input-block">
-                    <input
-                      type={this.props.passwordHidden ? "password" : "text"}
-                      value={this.props.password}
-                      className="login-field password"
-                      onChange={(e) => this.props.setPassword(e.target.value)}
-                      required
-                      placeholder="Password"
-                    />
-                    <div className="hide-password-icon-container">
-                      <VisibilityIcon
-                        className="hide-password-icon"
-                        onClick={() =>
-                          this.props.setHidden(!this.props.passwordHidden)
-                        }
-                      />
-                    </div>
-                  </div>
-                  <div className="input-block button-box">
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      className="sign-up-button"
-                      type="button"
-                      onClick={() =>
-                        this.props.register(
-                          this.props.email,
-                          this.props.password
-                        )
-                      }
-                    >
-                      Sign up
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      className="sign-in-button"
-                      type="submit"
-                    >
-                      Sign in
-                    </Button>
-                  </div>
-                  <div className="mobile-policy-text">
-                    <span onClick={() => this.props.setPolicyDialog(true)}>
-                      Privacy Policy
-                    </span>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
+          this.renderSignInPage()
         ) : (
-          <div style={{width: '100%'}}>
-            <div className="back-col">
-              <div className="back-box">
-                <svg
-                  className="svg active back-button"
-                  onClick={this.props.moveToLogin}
-                >
-                  {/*eslint-disable-next-line*/}
-                  <use href={sprite + "#arrow-down"} />
-                </svg>
-              </div>
-            </div>
+          <div style={{ width: "100%" }}>
             <div className="first-col">
               <div className="second-item">
                 <div
@@ -155,26 +136,29 @@ class MobileLoginPage extends React.Component<MobileLoginProps> {
                     loginState === LoginState.ChooseLoginAnimation ? "big" : ""
                   }`}
                 >
-                  <svg
-                    className="svg active logo-image mobile"
-                    onClick={this.props.moveToLogin}
-                  >
-                    {/*eslint-disable-next-line*/}
-                    <use
-                      href={sprite + "#logo"}
-                      className="text-theme-orange"
+                  <div className="logo-box-inner">
+                    <svg
+                      className="svg active logo-image mobile"
+                      onClick={this.props.moveToLogin}
+                    >
+                      {/*eslint-disable-next-line*/}
+                      <use
+                        href={sprite + "#logo"}
+                        className="text-theme-orange"
+                      />
+                    </svg>
+                    <img
+                      className="logo-text-image"
+                      alt="text"
+                      src="/images/choose-user/brillder-white-text.svg"
                     />
-                  </svg>
+                  </div>
                 </div>
                 {loginState !== LoginState.ChooseLoginAnimation ? (
                   <div className="mobile-button-box button-box">
                     <RegisterButton onClick={this.props.moveToLogin} />
                     <GoogleButton />
-                    <div className="mobile-policy-text">
-                      <span onClick={() => this.props.setPolicyDialog(true)}>
-                        Privacy Policy
-                      </span>
-                    </div>
+                    {this.renderPrivacyPolicy()}
                   </div>
                 ) : (
                   ""
