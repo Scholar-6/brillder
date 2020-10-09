8713899e7277815dd620afc60e6f2e83c5949005
ivan <=>
2020-10-09 12:15:24 +0300

    #2007 italic

--- src/components/backToWorkPage/components/build/BuildBricks.tsx
+++ src/components/backToWorkPage/components/build/BuildBricks.tsx
@@ -148,7 +148,7 @@ class BuildBricks extends Component<BuildBricksProps> {
             <div className="circle b-yellow"></div>
           </div>
           <div className="bold empty-title">Bricks in this column are with editors.</div>
-          <div>
+          <div className="italic">
             They will appear here once you have played a
             preview of your brick and invited an editor to
             suggest changes to it.
@@ -168,10 +168,10 @@ class BuildBricks extends Component<BuildBricksProps> {
             </div>
           </div>
           <div className="bold empty-title">Bricks in this column are with publishers.</div>
-          <div className="smaller">
+          <div className="italic">
             They will appear here once your editor(s) approve(s) your brick and sends it to the Publisher.
           </div>
-          <div className="last-text">
+          <div className="last-text italic">
             You will receive a notification if your brick is published, and it will appear in the Public Library.
           </div>
         </div>
