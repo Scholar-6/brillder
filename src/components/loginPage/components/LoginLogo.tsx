cf777b25c4bcabbc278293e561f466f370b9fb50
Lindsay MacVean <lindsaymacvean@gmail.com>
2020-10-13 22:20:06 +0100

    added first bit component

--- src/components/loginPage/components/LoginLogo.tsx
+++ src/components/loginPage/components/LoginLogo.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-
+import LogoImg from "public/images/choose-login/logo.png";
+import WhiteText from "public/images/choose-user/brillder-white-text.svg";
 
 const LoginLogo: React.FC<any> = () => {
   return (
     <Grid container style={{ height: "100%" }} justify="center" alignItems="center">
       <div>
         <img
-          alt="Logo" src="/images/choose-login/logo.png"
+          alt="Logo" src={LogoImg}
           className="logo-image"
         />
         <Grid container justify="center">
           <img
-            alt="Logo" src="/images/choose-user/brillder-white-text.svg"
+            alt="Logo" src={WhiteText}
             className="logo-text-image"
           />
         </Grid>
