58870f3511b28ca936041c9f7e3374f22820a2e9
ivan <=>
2020-10-16 19:20:52 +0300

    fix text

--- src/components/proposal/questionnaire/brickTitle/brickTitle.tsx
+++ src/components/proposal/questionnaire/brickTitle/brickTitle.tsx
@@ -135,7 +135,7 @@ class BrickTitle extends Component<BrickTitleProps, BrickTitleState> {
               <img alt="titles" src="/images/new-brick/titles.png" />
             </div>
             <h1 style={{position: 'relative'}}>
-             {subjectName && <div className="subject-text">This wil be a {subjectName} brick</div>}
+             {subjectName && <div className="subject-text">This will be a {subjectName} brick</div>}
               What is it about?
             </h1>
             <form>
