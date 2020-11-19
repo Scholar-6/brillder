55ba4a757651bf0b6654bdbe4714858bc2f28d11
ivan <=>
2020-11-19 11:59:12 +0200

    #2392 new way

--- src/components/build/baseComponents/PublishButton.tsx
+++ src/components/build/baseComponents/PublishButton.tsx
@@ -34,7 +34,8 @@ const PublishButton: React.FC<ButtonProps> = props => {
   }
 
   return (
-    <div>
+    <div className="build-publish-button-container">
+      <div className={`hover-container ${hovered ? 'hovered' : ''}`}></div>
       <div className={className} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}
         onClick={() => {
           if (!props.disabled) {
