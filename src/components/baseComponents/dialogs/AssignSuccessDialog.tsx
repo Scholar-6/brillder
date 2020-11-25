474c7079be2a710b9723752b8209dfcd9b575e9a
ivan <=>
2020-11-25 18:51:56 +0200

    #2438

--- src/components/baseComponents/dialogs/AssignSuccessDialog.tsx
+++ src/components/baseComponents/dialogs/AssignSuccessDialog.tsx
@@ -15,6 +15,36 @@ interface DialogProps {
 }
 
 const AssignSuccessDialog: React.FC<DialogProps> = props => {
+  let itemsText = '';
+
+  const capitalize = (n: string) => {
+    if (n && n.length > 1) {
+      return n[0].toLocaleUpperCase() + n.substring(1);
+    }
+    return '';
+  }
+
+  let index = 0;
+  for (let item of props.selectedItems) {
+    if (item.isStudent) {
+      itemsText += capitalize(item.firstName) + ' ' + capitalize(item.lastName);
+    } else {
+      itemsText += capitalize(item.name);
+    }
+    index+= 1;
+    if (index === props.selectedItems.length - 1) {
+      itemsText += ' and ';
+    } else if (index < props.selectedItems.length - 1) {
+      itemsText += ', ';
+    }
+  }
+
+  if (props.selectedItems.length > 1) {
+    itemsText += ' have ';
+  } else {
+    itemsText += ' has ';
+  }
+
   return (
     <Dialog
       open={props.isOpen} onClick={props.close} onClose={props.close}
@@ -23,7 +53,7 @@ const AssignSuccessDialog: React.FC<DialogProps> = props => {
       <div className="dialog-header">
         <ListItem>
           <ListItemText
-            primary={`Classes or Students have been assigned to ${props.brickTitle}`}
+            primary={ `${itemsText} been assigned to ${props.brickTitle}`}
             className="bold" style={{ minWidth: '30vw' }}
           />
           <ListItemAvatar>
