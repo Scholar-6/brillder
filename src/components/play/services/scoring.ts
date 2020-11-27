6ba60cf56f037a199a227597118714b3749c0f6b
ivanromakh <ivanrouman@gmail.com>
2020-11-27 16:34:35 +0200

    #2478 if null then false

--- src/components/play/services/scoring.ts
+++ src/components/play/services/scoring.ts
@@ -62,7 +62,10 @@ export const calcBrickLiveAttempt = (brick: Brick, answers: ComponentAttempt<any
 export const calcBrickReviewAttempt = (brick: Brick, answers: ComponentAttempt<any>[], brickAttempt: BrickAttempt) => {
   let score = getScore(answers);
   let maxScore = getMaxScore(answers);
-  const reviewAnswers = answers.map((attempt, index) => ({ ...attempt, liveCorrect: brickAttempt.liveAnswers![index].correct }));
+  const reviewAnswers = answers.map((attempt, index) => ({
+    ...attempt,
+    liveCorrect: brickAttempt.liveAnswers![index] ? brickAttempt.liveAnswers![index].correct : false
+  }));
   return {
     brick, score, oldScore: brickAttempt.score, maxScore, student: null, answers: reviewAnswers, liveAnswers: brickAttempt.liveAnswers
   } as BrickAttempt;
