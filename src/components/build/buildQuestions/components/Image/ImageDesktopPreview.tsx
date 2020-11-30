af815b695fa2873fccb5ab1b69310811f00a680f
ivanromakh <ivanrouman@gmail.com>
2020-11-30 18:32:56 +0200

    #2427

--- src/components/build/buildQuestions/components/Image/ImageDesktopPreview.tsx
+++ src/components/build/buildQuestions/components/Image/ImageDesktopPreview.tsx
@@ -51,7 +51,8 @@ const ImageDesktopPreview: React.FC<Props> = props => {
             <Grid item xs={9} className="brick-row-container">
               <Grid container direction="row" className="h100">
                 <Grid item xs={8} className={className}>
-                  <img alt="preview" src={finalSrc} style={{height: props.height + 'vh'}} />
+                  <div style={{backgroundImage: "url('" + finalSrc + "')" , height: props.height + 'vh'}} />
+                  {/*<img alt="preview" src={finalSrc} style={{height: props.height + 'vh'}} />*/}
                 </Grid>
                 <Grid item xs={4} className="introduction-info"/>
               </Grid>
