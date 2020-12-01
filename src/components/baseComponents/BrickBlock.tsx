5cfaf20495dd1918e49e43082a590148ba9c6182
ivanromakh <ivanrouman@gmail.com>
2020-12-01 15:15:14 +0200

    copy icon

--- src/components/baseComponents/BrickBlock.tsx
+++ src/components/baseComponents/BrickBlock.tsx
@@ -29,6 +29,7 @@ interface BrickBlockProps {
 }
 
 const BrickBlockComponent: React.FC<BrickBlockProps> = ({ brick, index, row = 0, ...props }) => {
+  let circleIcon = props.circleIcon;
   let color = "";
   if (brick.status === BrickStatus.Draft) {
     color = "color1";
@@ -50,6 +51,10 @@ const BrickBlockComponent: React.FC<BrickBlockProps> = ({ brick, index, row = 0,
     } else {
       color = brick.subject.color;
     }
+  }
+
+  if (brick.adaptedFrom) {
+    circleIcon = 'copy';
   }
 
   const move = () => {
@@ -80,7 +85,7 @@ const BrickBlockComponent: React.FC<BrickBlockProps> = ({ brick, index, row = 0,
             <ShortBrickDescription
               user={props.user}
               searchString={props.searchString}
-              circleIcon={props.circleIcon}
+              circleIcon={circleIcon}
               iconColor={props.iconColor}
               onMouseEnter={props.handleMouseHover}
               handleDeleteOpen={props.handleDeleteOpen}
