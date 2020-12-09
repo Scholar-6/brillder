4e45571c877c9876a053fedbc1b743c188df58f8
ivan <=>
2020-12-09 16:47:15 +0200

    #2545

--- src/components/backToWorkPage/components/build/Tab.tsx
+++ src/components/backToWorkPage/components/build/Tab.tsx
@@ -1,49 +1,54 @@
 import React from "react";
 import SpriteIcon from "components/baseComponents/SpriteIcon";
 
-
 interface TabProps {
   draft: number;
   selfPublish: number;
-
-  isTeach: boolean;
   onCoreSwitch(): void;
 }
 
-const TabComponent: React.FC<TabProps> = ({ draft, selfPublish, onCoreSwitch }) => {
+const TabComponent: React.FC<TabProps> = (props) => {
+  const [publicHovered, publicHover] = React.useState(false);
+  const [personalHovered, personalHover] = React.useState(false);
+
   const getPublicTab = () => {
     return (
-      <div key={2} className="active" onClick={onCoreSwitch}>
-        <div style={{display: 'flex'}}>
+      <div key={2} className="active">
+        <div style={{ display: "flex" }}>
           <span>Public</span>
-          <SpriteIcon name="globe" className="active" />
+          <div className="tab-icon-container public" onMouseEnter={() => publicHover(true)} onMouseLeave={() => publicHover(false)}>
+            <SpriteIcon name="globe" className="active" />
+            {publicHovered && <div className="custom-tooltip">Educate the World</div>}
+          </div>
         </div>
       </div>
     );
-  }
+  };
 
   const getPersonalTab = () => {
     return (
-      <div key={3} className="no-active" onClick={onCoreSwitch}>
-        <div style={{display: 'flex'}}>
+      <div key={3} className="no-active" onClick={props.onCoreSwitch}>
+        <div style={{ display: "flex" }}>
           <span>Personal</span>
-          <SpriteIcon name="key" className="no-active" />
+          <div className="tab-icon-container personal" onMouseEnter={() => personalHover(true)} onMouseLeave={() => personalHover(false)}>
+            <SpriteIcon name="key" className="no-active" />
+            {personalHovered && <div className="custom-tooltip">Keep Control</div>}
+          </div>
         </div>
         <div className="round-button-container">
-          <div className="round-button draft">{draft}</div>
-          <div className="round-button self-publish">{selfPublish}</div>
+          <div className="round-button draft">{props.draft}</div>
+          <div className="round-button self-publish">{props.selfPublish}</div>
         </div>
       </div>
     );
-  }
+  };
 
   return (
     <div className="tab-container">
       {getPublicTab()}
       {getPersonalTab()}
     </div>
-  )
-}
-
+  );
+};
 
 export default TabComponent;
