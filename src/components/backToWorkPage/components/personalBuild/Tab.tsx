6d82da996f34251b1eb93b465931a18e1145da75
ivan <=>
2020-12-10 14:55:58 +0200

    #2545 fix personal tabs

--- src/components/backToWorkPage/components/personalBuild/Tab.tsx
+++ src/components/backToWorkPage/components/personalBuild/Tab.tsx
@@ -10,12 +10,18 @@ interface TabProps {
 }
 
 const TabComponent: React.FC<TabProps> = (props) => {
+  const [publicHovered, publicHover] = React.useState(false);
+  const [personalHovered, personalHover] = React.useState(false);
+
   const getPublicTab = () => {
     return (
       <div key={2} className="no-active" onClick={props.onCoreSwitch}>
         <div style={{ display: "flex", position: "relative" }}>
           <span>Public</span>
-          <SpriteIcon name="globe" className="no-active" />
+          <div className="tab-icon-container public" onMouseEnter={() => publicHover(true)} onMouseLeave={() => publicHover(false)}>
+            <SpriteIcon name="globe" className="no-active" />
+            {publicHovered && <div className="custom-tooltip">Educate the World</div>}
+          </div>
           <div className="round-button-container">
             <div className="round-button draft">{props.draft}</div>
             <div className="round-button build">{props.build}</div>
@@ -32,7 +38,10 @@ const TabComponent: React.FC<TabProps> = (props) => {
       <div key={3} className="active">
         <div style={{ display: "flex" }}>
           <span>Personal</span>
-          <SpriteIcon name="key" className="active" />
+          <div className="tab-icon-container personal" onMouseEnter={() => personalHover(true)} onMouseLeave={() => personalHover(false)}>
+            <SpriteIcon name="key" className="active" />
+            {personalHovered && <div className="custom-tooltip">Keep Control</div>}
+          </div>
         </div>
       </div>
     );
