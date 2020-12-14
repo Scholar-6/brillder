2062d1836298509001dc86be418728158f66edb6
ivan <=>
2020-12-14 16:15:44 +0200

    #2523

--- src/components/mainPage/LibraryButton.tsx
+++ src/components/mainPage/LibraryButton.tsx
@@ -1,7 +1,6 @@
-import map from "components/map";
 import React from "react";
 import { isMobile } from "react-device-detect";
-import './TeachButton.scss';
+import './LibraryButton.scss';
 
 interface ButtonProps {
   history: any;
@@ -17,30 +16,30 @@ const LibraryButton: React.FC<ButtonProps> = props => {
 
   const renderLibraryIcon = () => {
     return (
-      <svg viewBox="0 0 163.991 148.79" stroke="currentColor">
+      <svg viewBox="0 0 163.991 148.79" className="book-svg-icon" stroke="currentColor">
         <g transform="translate(-878.008 -684)">
           <path d="M3,4.5H48.6a30.4,30.4,0,0,1,30.4,30.4V141.29a22.8,22.8,0,0,0-22.8-22.8H3Z" transform="translate(881.008 685.5)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"/>
           <path d="M93.995,4.5H48.4A30.4,30.4,0,0,0,18,34.9V141.29a22.8,22.8,0,0,1,22.8-22.8h53.2Z" transform="translate(942.004 685.5)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"/>
-          <path d="M27,41.335V15" transform="translate(986.602 724.134)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/>
-          <path d="M18,48.135V6" transform="translate(979.801 717.333)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/>
-          <path d="M9,36.8V21" transform="translate(973 728.668)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/>
+          <path className="third-line" d="M27,41.335V15" transform="translate(986.602 724.134)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/>
+          <path className="second-line" d="M18,48.135V6" transform="translate(979.801 717.333)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/>
+          <path className="first-line" d="M9 36 l 0 -42" transform="translate(973 728.668)" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/>
         </g>
       </svg>
     );
   }
 
   const onClick = () => {
-      if (isMobile) {
-        if (!props.isSwiping) {
-          props.onMobileClick();
-        }
-        return;
+    if (isMobile) {
+      if (!props.isSwiping) {
+        props.onMobileClick();
       }
-      if (isActive) { 
-        props.history.push('/my-library');
-      } else {
-        props.onClick();
-      }
+      return;
+    }
+    if (isActive) { 
+      props.history.push('/my-library');
+    } else {
+      props.onClick();
+    }
   }
 
   return (
