715185412051013d42e72ef5b2f2fdfef5dd8a60
ivanromakh <ivanrouman@gmail.com>
2020-12-24 12:58:59 +0200

    fix for admin with student role preference

--- src/components/userProfilePage/UserProfile.tsx
+++ src/components/userProfilePage/UserProfile.tsx
@@ -120,12 +120,20 @@ class UserProfilePage extends Component<UserProfileProps, UserProfileState> {
     let isBuilder = canBuild(user);
     let isEditor = canEdit(user);
     let isStudent = isBuilder;
+    let isInstitute = isInstitution(user);
 
     let isOnlyStudent = user.roles.length === 1 && user.roles[0].roleId === UserType.Student;
     if (this.props.user.rolePreference && this.props.user.rolePreference.roleId === UserType.Student) {
       isBuilder = false;
       isEditor = false;
       isStudent = true;
+    }
+
+    if (isAdmin) {
+      isBuilder = true;
+      isEditor = true;
+      isStudent = true;
+      isInstitute = true;
     }
 
     return {
@@ -152,7 +160,7 @@ class UserProfilePage extends Component<UserProfileProps, UserProfileState> {
         { roleId: UserType.Teacher, name: "Teacher", disabled: !isAdmin },
         { roleId: UserType.Builder, name: "Builder", disabled: !isBuilder },
         { roleId: UserType.Publisher, name: "Publisher", disabled: !isEditor },
-        { roleId: UserType.Institution, name: "Institution", disabled: !isInstitution(user) },
+        { roleId: UserType.Institution, name: "Institution", disabled: !isInstitute },
         { roleId: UserType.Admin, name: "Admin", disabled: !isAdmin },
       ],
       noSubjectDialogOpen: false,
@@ -313,7 +321,7 @@ class UserProfilePage extends Component<UserProfileProps, UserProfileState> {
   }
 
   checkUserRole(roleId: number) {
-    if (this.props.user.rolePreference && this.props.user.rolePreference.roleId === UserType.Student) {
+    if (!this.state.isAdmin && this.props.user.rolePreference && this.props.user.rolePreference.roleId === UserType.Student) {
       if (roleId !== UserType.Student) {
         return false;
       }
