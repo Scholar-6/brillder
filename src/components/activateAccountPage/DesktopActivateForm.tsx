aa71c3d2b4a1aaf03149afb250856006ad4c9c75
William Gooch <william.gooch.wg@gmail.com>
2021-01-08 11:53:37 +0000

    Change account activation page.

---
+++ src/components/activateAccountPage/DesktopActivateForm.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+
+import TypingInput from "components/loginPage/components/TypingInput";
+import { enterPressed } from "components/services/key";
+
+interface LoginsState {
+  handleSubmit(e: any): void;
+}
+
+interface ActivateFormProps {
+  email: string;
+  password: string;
+  setPassword(password: string): void;
+  passwordHidden: boolean;
+
+  handleSubmit(e: any): void;
+  setHidden(passwordHidden: boolean): void;
+}
+
+class DesktopActivateForm extends React.Component<ActivateFormProps, LoginsState> {
+  constructor(props: ActivateFormProps) {
+    super(props);
+
+    this.state = {
+      handleSubmit: this.onKeyPressed.bind(this)
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.state.handleSubmit, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.state.handleSubmit, false);
+  }
+
+  onKeyPressed(e: any) {
+    if (enterPressed(e)) {
+      this.props.handleSubmit(e);
+    }
+  }
+
+  render() {
+    let className = 'content-box expanded';
+    return (
+      <div className={className}>
+        <div className="input-block">
+          <TypingInput
+            type="email"
+            className="login-field"
+            placeholder="Email"
+            value={this.props.email}
+            disabled={true}
+          />
+        </div>
+        <div className="input-block">
+          <TypingInput
+            required
+            type={this.props.passwordHidden ? "password" : "text"}
+            className="login-field password"
+            value={this.props.password}
+            placeholder="Password"
+            onChange={this.props.setPassword}
+          />
+          <div className="hide-password-icon-container">
+            <VisibilityIcon
+              className="hide-password-icon"
+              onClick={() => this.props.setHidden(!this.props.passwordHidden)}
+            />
+          </div>
+        </div>
+        <div className="input-block">
+          <div className="button-box">
+            <button onClick={this.props.handleSubmit} className="sign-in-button">Set Password</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default DesktopActivateForm;
---
+++ src/components/activateAccountPage/activateAccountPage.tsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { Grid, Hidden } from "@material-ui/core";
+import { connect } from "react-redux";
+import { History } from "history";
+import axios from 'axios';
+
+import "./activateAccountPage.scss";
+import actions from "redux/actions/auth";
+import LoginLogo from 'components/loginPage/components/LoginLogo';
+import GoogleButton from "components/loginPage/components/GoogleButton";
+import PolicyDialog from 'components/baseComponents/policyDialog/PolicyDialog';
+import RegisterButton from "components/loginPage/components/RegisterButton";
+import map from "components/map";
+import { Redirect, useLocation } from "react-router-dom";
+import PageLoader from "components/baseComponents/loaders/pageLoader";
+
+const mapDispatch = (dispatch: any) => ({
+  loginSuccess: () => dispatch(actions.loginSuccess()),
+});
+
+const connector = connect(null, mapDispatch);
+
+export enum LoginState {
+  ChooseLoginAnimation,
+  ChooseLogin,
+  ButtonsAnimation,
+  Login
+}
+
+interface ActivateAccountProps {
+  history: History;
+  match: any;
+}
+
+const ActivateAccountPage: React.FC<ActivateAccountProps> = (props) => {
+  let initPolicyOpen = false;
+  if (props.match.params.privacy && props.match.params.privacy === "privacy-policy") {
+    initPolicyOpen = true;
+  }
+  const [isPolicyOpen, setPolicyDialog] = useState(initPolicyOpen);
+  const [loginState, setLoginState] = useState(LoginState.ChooseLoginAnimation);
+  const [valid, setValid] = React.useState<boolean>();
+
+  const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  const token = params.get("token");
+
+  const verifyToken = async () => {
+    try {
+      await axios.get(`${process.env.REACT_APP_BACKEND_HOST}/auth/verifyResetPassword/${token}`);
+      setValid(true);
+    } catch(e) {
+      console.log(e.response.statusCode);
+      setValid(false);
+    }
+  }
+
+  React.useEffect(() => {
+    verifyToken();
+  }, [token, setValid]);
+
+  const moveToEmailPage = () => {
+    props.history.push(map.ActivateAccount + '/email?token=' + token);
+  }
+
+  const movingToLogin = () => {
+    setLoginState(LoginState.ButtonsAnimation);
+    setTimeout(() => {
+      moveToEmailPage();
+    }, 450);
+  }
+
+  if(!valid && valid !== false) {
+    return <PageLoader content="Checking token..." />
+  }
+
+  const renderButtons = () => {
+    let className = 'button-box f-column';
+    if (loginState === LoginState.ButtonsAnimation) {
+      className += ' expanding';
+    }
+    return (
+      <div className={className}>
+        <RegisterButton onClick={movingToLogin} />
+        <GoogleButton />
+      </div>
+    );
+  }
+
+  return (
+    <Grid
+      className="auth-page login-page"
+      container
+      item
+      justify="center"
+      alignItems="center"
+    >
+      {valid ? <>
+        <Hidden only={["xs"]}>
+          <div className="choose-login-desktop">
+            <Grid container direction="row" className="first-row">
+              <div className="first-col"></div>
+              <div className="second-col"></div>
+              <div className="third-col"></div>
+            </Grid>
+            <Grid container direction="row" className="second-row">
+              <div className="first-col">
+                <LoginLogo />
+              </div>
+              <div className="second-col">
+                {renderButtons()}
+              </div>
+            </Grid>
+            <Grid container direction="row" className="third-row">
+              <div className="first-col"></div>
+              <div className="second-col">
+                <span className="policy-text" onClick={() => setPolicyDialog(true)}>Privacy Policy</span>
+              </div>
+              <div className="third-col"></div>
+            </Grid>
+          </div>
+        </Hidden>
+        <PolicyDialog isOpen={isPolicyOpen} close={() => setPolicyDialog(false)} />
+      </>
+      : <Redirect to={map.Login} />}
+    </Grid>
+  );
+};
+
+export default connector(ActivateAccountPage);
