ff274ee857b1322c175ee27ff5949cf74c0f6fa9
William Gooch <william.gooch.wg@gmail.com>
2021-01-14 10:45:03 +0000

    Add subject field to create class popup.

--- src/components/teach/manageClassrooms/components/CreateClassDialog.tsx
+++ src/components/teach/manageClassrooms/components/CreateClassDialog.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
+import { Subject } from 'model/brick';
+import { InputBase, ListItemIcon, ListItemText, MenuItem, Select, SvgIcon } from '@material-ui/core';
+import { loadSubjects } from 'components/services/subject';
+import SpriteIcon from 'components/baseComponents/SpriteIcon';
+import { User, UserType } from 'model/user';
+import { ReduxCombinedState } from 'redux/reducers';
+import { connect } from 'react-redux';
 
 interface AssignClassProps {
   isOpen: boolean;
   submit(value: string): void;
   close(): void;
+
+  user: User;
 }
 
 const CreateClassDialog: React.FC<AssignClassProps> = (props) => {
   const [value, setValue] = React.useState("");
+  const [subjectIndex, setSubjectIndex] = React.useState<number>();
+
+  const [subjects, setSubjects] = React.useState<Subject[]>();
+  React.useEffect(() => {
+    const initAllSubjects = async () => {
+      const subs = await loadSubjects();
+      if(subs) {
+        setSubjects(subs);
+      }
+    }
+
+    if(props.user.roles.some(role => role.roleId === UserType.Admin)) {
+      initAllSubjects();
+    } else {
+      setSubjects(props.user.subjects);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if(subjects && subjects.length > 0) {
+      setSubjectIndex(subjects.findIndex(s => s.name === "General") ?? 0);
+    }
+  }, [subjects])
 
   return (
     <Dialog
@@ -19,6 +51,30 @@ const CreateClassDialog: React.FC<AssignClassProps> = (props) => {
       <div className="dialog-header" style={{marginBottom: '2vh'}}>
         <div className="title">What is the name of class?</div>
         <input value={value} onChange={e => setValue(e.target.value)} />
+      </div>
+      <div className="dialog-header" style={{marginBottom: '2vh'}}>
+        <div className="title">What subject is the class for?</div>
+        <Select
+          MenuProps={{ style: { zIndex: 1000000 } } /* Dialog box is always z-index 999999 */}
+          value={subjectIndex}
+          onChange={(evt) => setSubjectIndex(evt.target.value as number)}
+          input={<InputBase />}
+        >
+          {subjects?.map((s, i) => (
+            <MenuItem value={i} key={i}>
+              <ListItemIcon>
+                <SvgIcon>
+                  <SpriteIcon
+                    name="circle-filled"
+                    className="w100 h100 active"
+                    style={{ color: s.color }}
+                  />
+                </SvgIcon>
+              </ListItemIcon>
+              <ListItemText>{s.name}</ListItemText>
+            </MenuItem>
+          ))}
+        </Select>
       </div>
       <div className="dialog-footer">
         <button className="btn btn-md bg-theme-orange yes-button"
@@ -34,4 +90,7 @@ const CreateClassDialog: React.FC<AssignClassProps> = (props) => {
   );
 }
 
-export default CreateClassDialog;
+const mapState = (state: ReduxCombinedState) => ({ user: state.user.user });
+const connector = connect(mapState);
+
+export default connector(CreateClassDialog);
