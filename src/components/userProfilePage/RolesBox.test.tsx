a5839bbd31cde944c97ae92508ea0c49ec79596f
ivan <=>
2021-01-18 15:13:56 +0200

    #2107 enzyme tests

--- src/components/userProfilePage/RolesBox.test.tsx
+++ src/components/userProfilePage/RolesBox.test.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import {shallow} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 import RolesBox from "./RolesBox";
 import { UserRole, UserType } from "model/user";
 
+Enzyme.configure({ adapter: new Adapter() })
 
 describe("Roles Box", () => {
-  const roles: [
-    { roleId: UserType.Publisher, name: "Publisher", disabled: !isEditor },
-    { roleId: UserType.Institution, name: "Institution", disabled: !isInstitute },
-    { roleId: UserType.Admin, name: "Admin", disabled: !isAdmin },
-  ];
-
+  
   it("should create Roles Box", () => {
-    render(
+    const roles = [
+      { roleId: UserType.Publisher, name: "Publisher", disabled: false },
+      { roleId: UserType.Institution, name: "Institution", disabled: false },
+      { roleId: UserType.Admin, name: "Admin", disabled: false },
+    ];
+
+    const component = shallow(
       <RolesBox
         roles={roles}
         userRoles={[]}
@@ -21,5 +26,8 @@ describe("Roles Box", () => {
         toggleRole={() => {}}
       />
     );
+    console.log(component.props());
+    expect(component).toHaveLength(1);
+    expect(component.props().children).toHaveLength(6);
   });
 });
