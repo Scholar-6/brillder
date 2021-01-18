6d07537ca4eba2b31115b37a8ab1921b8ebe6c92
ivan <=>
2021-01-18 15:46:42 +0200

    #2107 enzyme tests

--- src/components/userProfilePage/RolesBox.test.tsx
+++ src/components/userProfilePage/RolesBox.test.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import {shallow} from "enzyme";
+import { shallow } from "enzyme";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import RolesBox from "./RolesBox";
 import { UserRole, UserType } from "model/user";
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
+
+const roles = [
+  { roleId: UserType.Publisher, name: "Publisher", disabled: false },
+  { roleId: UserType.Institution, name: "Institution", disabled: false },
+  { roleId: UserType.Admin, name: "Admin", disabled: false },
+];
 
 describe("Roles Box", () => {
-  
-  it("should create Roles Box", () => {
-    const roles = [
-      { roleId: UserType.Publisher, name: "Publisher", disabled: false },
-      { roleId: UserType.Institution, name: "Institution", disabled: false },
-      { roleId: UserType.Admin, name: "Admin", disabled: false },
-    ];
-
+  it("check student preference", () => {
     const component = shallow(
       <RolesBox
         roles={roles}
@@ -26,8 +25,60 @@ describe("Roles Box", () => {
         toggleRole={() => {}}
       />
     );
-    console.log(component.props());
     expect(component).toHaveLength(1);
     expect(component.props().children).toHaveLength(6);
+    expect(component.props().children[0].props.children.props.checked).toBe(true);
+    expect(component.props().children[1].props.children.props.checked).toBe(false);
+    expect(component.props().children[2].props.children.props.checked).toBe(false);
+  });
+
+  it("check teacher preference", () => {
+    const component = shallow(
+      <RolesBox
+        roles={roles}
+        userRoles={[]}
+        rolePreference={UserType.Teacher}
+        toggleRole={() => {}}
+      />
+    );
+    expect(component).toHaveLength(1);
+    expect(component.props().children).toHaveLength(6);
+    expect(component.props().children[0].props.children.props.checked).toBe(false);
+    expect(component.props().children[1].props.children.props.checked).toBe(true);
+    expect(component.props().children[2].props.children.props.checked).toBe(false);
+  });
+
+  it("check builder preference", () => {
+    const component = shallow(
+      <RolesBox
+        roles={roles}
+        userRoles={[]}
+        rolePreference={UserType.Builder}
+        toggleRole={() => {}}
+      />
+    );
+    expect(component).toHaveLength(1);
+    expect(component.props().children).toHaveLength(6);
+    expect(component.props().children[0].props.children.props.checked).toBe(false);
+    expect(component.props().children[1].props.children.props.checked).toBe(false);
+    expect(component.props().children[2].props.children.props.checked).toBe(true);
+  });
+
+  it("check admin role", () => {
+    const component = shallow(
+      <RolesBox
+        roles={roles}
+        userRoles={[UserType.Admin]}
+        rolePreference={UserType.Builder}
+        toggleRole={() => {}}
+      />
+    );
+    expect(component).toHaveLength(1);
+    expect(component.props().children).toHaveLength(6);
+    expect(component.props().children[0].props.children.props.checked).toBe(false);
+    expect(component.props().children[1].props.children.props.checked).toBe(false);
+    expect(component.props().children[2].props.children.props.checked).toBe(true);
+
+    expect(component.props().children[5].props.children.props.checked).toBe(true);
   });
 });
