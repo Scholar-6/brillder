79ecf7a2ca8f0f9696c8d2c33df32a710b17b44f
aromakh <ivanrouman@gmail.com>
2021-01-20 16:24:06 +0200

    #2614 text smaller and typing

--- src/components/viewAllPage/AllSubjectsSidebar.tsx
+++ src/components/viewAllPage/AllSubjectsSidebar.tsx
@@ -1,16 +1,31 @@
 
 import React, { Component } from "react";
 import { Grid } from "@material-ui/core";
+import TypingLabel from "components/baseComponents/TypingLabel";
 
+interface State {
+  finished: boolean;
+}
 
-interface FilterProps { }
+class AllSubjectsSidebar extends Component<any, State> {
+  constructor(props: any) {
+    super(props);
 
-class AllSubjectsSidebar extends Component<FilterProps> {
+    this.state = {
+      finished: false
+    }
+  }
+
   render() {
     return (
       <Grid container item xs={3} className="sort-and-filter-container">
-        <div className="unauthorized-sidebar">
-          <div className="bold title">Select a Subject or View All</div>
+        <div className="unauthorized-sidebar" >
+          <div>
+            <TypingLabel className="bold title f-1-9vw" label="Click to select a Subject" onEnd={() => this.setState({finished: true})} />
+          </div>
+          <div>
+            { this.state.finished && <TypingLabel className="bold title f-1-9vw" label=" or View All bricks." onEnd={() => {}} /> }
+          </div>
         </div>
       </Grid>
     );
