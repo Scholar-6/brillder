dd9e7a0be75585276ce9914faa44935a783b496a
ivan <=>
2021-01-29 16:55:21 +0200

    #2682 self publish hover

--- src/components/playPreview/finalStep/SelfPublishColumn.tsx
+++ src/components/playPreview/finalStep/SelfPublishColumn.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
-import sprite from "assets/img/icons-sprite.svg";
+import SpriteIcon from "components/baseComponents/SpriteIcon";
 
 interface PublishProps {
   onClick(): void;
@@ -11,11 +11,8 @@ const SelfPublishColumn: React.FC<PublishProps> = props => {
   return (
     <Grid container item xs={3} justify="center">
       <div>
-        <div className="button-container" onClick={props.onClick}>
-          <svg className="svg active">
-            {/*eslint-disable-next-line*/}
-            <use href={sprite + "#award"} style={{strokeWidth: 1.5}} />
-          </svg>
+        <div className="button-container self-publish-button" onClick={props.onClick}>
+          <SpriteIcon name="award" />
         </div>
         <div className="link-text">Self Publish</div>
       </div>
