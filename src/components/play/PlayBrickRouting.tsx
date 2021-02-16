9649156f2838f73c9e06d4dd7aa32afa5fd21383
William Gooch <william.gooch.wg@gmail.com>
2021-02-16 10:37:09 +0000

    Create account for unauthorized user.

--- src/components/play/PlayBrickRouting.tsx
+++ src/components/play/PlayBrickRouting.tsx
@@ -18,7 +18,7 @@ import PageHeadWithMenu, {
   PageEnum,
 } from "components/baseComponents/pageHeader/PageHeadWithMenu";
 
-import { Brick } from "model/brick";
+import { Brick, isAuthenticated } from "model/brick";
 import { PlayStatus, BrickAttempt } from "./model";
 import {
   Question,
@@ -60,7 +60,9 @@ interface BrickRoutingProps {
   // redux
   brick: Brick;
   user: User;
+  isAuthenticated: isAuthenticated;
   getUser(): Promise<any>;
+  setUser(user: User): void;
 }
 
 const MobileTheme = React.lazy(() => import('./themes/BrickPageMobileTheme'));
@@ -86,6 +88,7 @@ const BrickRouting: React.FC<BrickRoutingProps> = (props) => {
   const [sidebarRolledUp, toggleSideBar] = React.useState(false);
   const [searchString, setSearchString] = React.useState("");
   const [attemptId, setAttemptId] = React.useState<string>();
+  const [userToken, setUserToken] = React.useState<string>(); // used for unauthenticated user.
 
   setBrillderTitle(brick.title);
 
@@ -139,10 +142,10 @@ const BrickRouting: React.FC<BrickRoutingProps> = (props) => {
     }
     return axios.post(
       process.env.REACT_APP_BACKEND_HOST + "/play/attempt",
-      brickAttempt,
+      { brickAttempt, userId: props.user.id },
       { withCredentials: true }
     ).then(async (response) => {
-      if (!props.user.hasPlayedBrick) {
+      if (!props.user.hasPlayedBrick && props.isAuthenticated === isAuthenticated.True) {
         await props.getUser();
       }
       setAttemptId(response.data);
@@ -166,10 +169,10 @@ const BrickRouting: React.FC<BrickRoutingProps> = (props) => {
     }
     return axios.put(
       process.env.REACT_APP_BACKEND_HOST + "/play/attempt",
-      { id: attemptId, body: brickAttempt },
+      { id: attemptId, userId: props.user.id, body: brickAttempt },
       { withCredentials: true }
     ).then(async (response) => {
-      if (!props.user.hasPlayedBrick) {
+      if (!props.user.hasPlayedBrick && props.isAuthenticated === isAuthenticated.True) {
         await props.getUser();
       }
       setAttemptId(response.data);
@@ -223,6 +226,29 @@ const BrickRouting: React.FC<BrickRoutingProps> = (props) => {
     } else {
       // unauthorized users finish it here. show popup
       setUnauthorized(true);
+    }
+  }
+
+  const moveToPostPlay = () => {
+    if(props.isAuthenticated === isAuthenticated.True) {
+      props.history.push(map.postPlay(brick.id, props.user.id));
+    } else if (userToken) {
+      props.history.push(map.ActivateAccount + "?token=" + userToken);
+    }
+  }
+
+  const createInactiveAccount = async (email: string) => {
+    if (!props.user) {
+      // create a new account for an unauthorized user.
+      try {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/auth/createUser`, { email }); // should be done without credentials!
+        const { user, token } = response.data;
+        props.setUser(user);
+        setUnauthorized(false);
+        setUserToken(token);
+      } catch (e) {
+        console.log("couldn't create new account");
+      }
     }
   }
 
@@ -346,6 +372,7 @@ const BrickRouting: React.FC<BrickRoutingProps> = (props) => {
             user={props.user}
             brick={brick}
             history={props.history}
+            moveNext={moveToPostPlay}
           />
         </Route>
         <ValidationFailedDialog
@@ -383,7 +410,7 @@ const BrickRouting: React.FC<BrickRoutingProps> = (props) => {
         </div>
         <UnauthorizedUserDialog
           isOpen={unauthorizedOpen}
-          login={() => props.history.push('/')}
+          login={(email) => createInactiveAccount(email)}
           again={again}
           close={() => setUnauthorized(false)}
         />
@@ -477,11 +504,13 @@ const parseAndShuffleQuestions = (brick: Brick): Brick => {
 
 const mapState = (state: ReduxCombinedState) => ({
   user: state.user.user,
-  brick: state.brick.brick
+  brick: state.brick.brick,
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 const mapDispatch = (dispatch: any) => ({
   getUser: () => dispatch(userActions.getUser()),
+  setUser: (user: User) => dispatch(userActions.setUser(user)),
 });
 
 const connector = connect(mapState, mapDispatch);
