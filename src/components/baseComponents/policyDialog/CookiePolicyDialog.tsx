330ef025b96a91710ed7789bd19a24c7218da0e5
aromakh <ivanrouman@gmail.com>
2021-02-23 18:13:58 +0200

    #2821 popup text updated with email

--- src/components/baseComponents/policyDialog/CookiePolicyDialog.tsx
+++ src/components/baseComponents/policyDialog/CookiePolicyDialog.tsx
@@ -14,7 +14,7 @@ const CookiePolicyDialog: React.FC<PolicyDialogProps> = (props) => {
       <div className="private-policy-content">
         <h1 className="private-policy-title">We Use Cookies</h1>
         <p>
-        If we had a market stall we would ask you where you are from and where you are going. We don't have a market stall, so we are asking if we can put some cookies in your browser that track how you found us and if you decide to come back. If you accept, then change your mind, you can push the button in the bottom right to 'stop tracking'. If you want all records of your visit to be deleted you can send us a message using the help button in the bottom left.
+          If we had a market stall we would ask you where you are from and where you are going. We don't have a market stall, so we are asking if we can put some cookies in your browser that track how you found us and if you decide to come back. If you accept, then change your mind, you can push the button in the bottom right to 'stop tracking'. If you want all records of your visit to be deleted, you can send us a message at <a href = "mailto: support@scholar6.org">support@scholar6.org</a>.
         </p>
       </div>
       <button className="btn btn-md bg-theme-orange yes-button" onClick={props.close}>
