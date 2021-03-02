0f03c3079ec0fe4633889e108efe552e00d90b94
Ivan Romakh <ivanrouman@gmail.com>
2021-03-02 15:02:26 +0200

    #2864 synthesis progress bar in mobile

--- src/components/play/baseComponents/timeProgressbar/TimeProgressbarV2.tsx
+++ src/components/play/baseComponents/timeProgressbar/TimeProgressbarV2.tsx
@@ -2,38 +2,19 @@ import React from "react";
 
 import ProgressbarCountdown from "./ProgressbarCountdown";
 import { BrickLengthEnum } from "model/brick";
+import { getSynthesisTime } from 'components/play/services/playTimes';
 const moment = require("moment");
 // TODO: try combining this into import { Moment }, * as moment from 'moment';
 
 interface CounterProps {
-  isLive?: boolean;
   isIntro?: boolean;
+  isSynthesis?: boolean;
   brickLength: BrickLengthEnum;
   startTime: any;
   onEnd(): void;
 }
 
 const TimeProgressbar: React.FC<CounterProps> = (props) => {
-  const getLiveDuration = () => {
-    let durationMins = 8;
-    if (props.brickLength === BrickLengthEnum.S40min) {
-      durationMins = 16;
-    } else if (props.brickLength === BrickLengthEnum.S60min) {
-      durationMins = 24;
-    }
-    return moment.duration(durationMins, "minutes");
-  };
-
-  const getReviewDuration = () => {
-    let durationMins = 3;
-    if (props.brickLength === BrickLengthEnum.S40min) {
-      durationMins = 6;
-    } else if (props.brickLength === BrickLengthEnum.S60min) {
-      durationMins = 9;
-    }
-    return moment.duration(durationMins, "minutes");
-  }
-
   const getIntroDuration = () => {
     let durationMins = 5;
     if (props.brickLength === BrickLengthEnum.S40min) {
@@ -44,14 +25,17 @@ const TimeProgressbar: React.FC<CounterProps> = (props) => {
     return moment.duration(durationMins, "minutes");
   }
 
+  const getSynthesisDuration = () => {
+    const timeMinutes = getSynthesisTime(props.brickLength);
+    return moment.duration(timeMinutes, "minutes");
+  }
+
   const getDuration = () => {
     if (props.isIntro) {
       return getIntroDuration();
     }
-    if (props.isLive) {
-      return getLiveDuration();
-    } else {
-      return getReviewDuration();
+    if (props.isSynthesis) {
+      return getSynthesisDuration();
     }
   }
 
