64d2cf09a1385b8a1b68b9709ef53862f4791c93
Ivan Romakh <ivanrouman@gmail.com>
2021-03-22 14:28:34 +0200

    move to new prep

--- src/components/play/PlayLeftSidebar.tsx
+++ src/components/play/PlayLeftSidebar.tsx
@@ -26,6 +26,7 @@ import ShareButton from "./baseComponents/sidebarButtons/ShareButton";
 import AssignButton from "./baseComponents/sidebarButtons/AssignButton";
 import AdaptButton from "./baseComponents/sidebarButtons/AdaptButton";
 import AssignFailedDialog from "components/baseComponents/dialogs/AssignFailedDialog";
+import { playNewPrep } from "./routes";
 
 
 interface SidebarProps {
@@ -196,7 +197,7 @@ class PlayLeftSidebarComponent extends Component<SidebarProps, SidebarState> {
     if (isLive && this.props.sidebarRolledUp) {
       return (
         <div>
-          <div className="prep-button" onClick={() => this.props.history.push(map.playIntro(this.props.brick.id))}>
+          <div className="prep-button" onClick={() => this.props.history.push(playNewPrep(this.props.brick.id))}>
             <SpriteIcon name="file-text" />
             <div>Prep</div>
           </div>
