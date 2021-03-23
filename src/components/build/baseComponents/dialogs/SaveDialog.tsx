a104ed4d4e2a024204a0d31b2650d1d918a680d4
Ivan Romakh <ivanrouman@gmail.com>
2021-03-23 16:28:03 +0200

    save and exit dialog styled

--- src/components/build/baseComponents/dialogs/SaveDialog.tsx
+++ src/components/build/baseComponents/dialogs/SaveDialog.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import BaseDialogWrapper from "components/baseComponents/dialogs/BaseDialogWrapper";
 
+import './SaveDialog.scss';
 
 interface SkipTutorialDialogProps {
   open: boolean;
@@ -12,15 +13,15 @@ const SaveDialog: React.FC<SkipTutorialDialogProps> = ({
   open, save, close
 }) => {
   return (
-    <BaseDialogWrapper open={open} close={close} submit={save}>
-      <div className="dialog-header">
+    <BaseDialogWrapper open={open} className="save-brick-dialog" close={close} submit={save}>
+      <div className="dialog-header bold">
         <div>Stop building for now?</div>
       </div>
       <div className="dialog-footer">
-        <button className="btn btn-md bg-theme-orange yes-button" onClick={save}>
+        <button className="btn btn-md bg-theme-orange bold yes-button" onClick={save}>
           <span>Yes</span>
         </button>
-        <button className="btn btn-md bg-gray no-button" onClick={close}>
+        <button className="btn btn-md bg-gray bold no-button" onClick={close}>
           <span>No</span>
         </button>
       </div>
