335e2080fb10a13b274edd0dad3d03d842931c18
Ivan Romakh <ivanrouman@gmail.com>
2021-03-26 16:56:49 +0200

    fixes

--- src/components/play/brief/Brief.tsx
+++ src/components/play/brief/Brief.tsx
@@ -78,22 +78,24 @@ const BriefPage: React.FC<Props> = ({ brick, ...props }) => {
       </div>
       <div className="brick-container play-preview-panel live-page play-brief-page">
         <div className="introduction-page">
-          <div className="open-question">
-            <MathInHtml value={brick.openQuestion} />
-          </div>
-          <div className="introduction-content">
-            {renderBriefTitle()}
-            {renderBriefExpandText()}
-          </div>
-          <div className="new-layout-footer" style={{ display: 'none' }}>
-            <div className="title-column">
-              Now you’re ready for preparatory tasks
+          <div className="scrollable">
+            <div className="open-question">
+              <MathInHtml value={brick.openQuestion} />
             </div>
-            <img alt="" className="footer-arrow" src="/images/play-arrows/BriefArrow.svg" />
-            <div className="new-navigation-buttons">
-              <div className="n-btn next" onClick={props.moveNext}>
-                Next
+            <div className="introduction-content">
+              {renderBriefTitle()}
+              {renderBriefExpandText()}
+            </div>
+            <div className="new-layout-footer" style={{ display: 'none' }}>
+              <div className="title-column">
+                Now you’re ready for preparatory tasks
+            </div>
+              <img alt="" className="footer-arrow" src="/images/play-arrows/BriefArrow.svg" />
+              <div className="new-navigation-buttons">
+                <div className="n-btn next" onClick={props.moveNext}>
+                  Next
                 <SpriteIcon name="arrow-right" />
+                </div>
               </div>
             </div>
           </div>
