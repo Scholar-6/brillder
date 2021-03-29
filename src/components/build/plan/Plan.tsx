037728f9870096fbe6f8f50a6a654d2eb962f003
ivan <=>
2021-03-29 18:44:02 +0300

    working on plan workarea

--- src/components/build/plan/Plan.tsx
+++ src/components/build/plan/Plan.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import * as Y from "yjs";
 import { connect } from "react-redux";
-import { Grid, InputBase, ListItemIcon, ListItemText, MenuItem, Select, SvgIcon } from '@material-ui/core';
+import {
+  Grid,
+  InputBase,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  Select,
+  SvgIcon,
+} from "@material-ui/core";
 
 import CommentPanel from "components/baseComponents/comments/CommentPanel";
 import { CommentLocation } from "model/comments";
 import { ReduxCombinedState } from "redux/reducers";
 
-import { Brick } from "model/brick";
+import { AcademicLevel, Brick, BrickLengthEnum } from "model/brick";
 import SpriteIcon from "components/baseComponents/SpriteIcon";
 import CommentButton from "../baseComponents/commentButton/CommentButton";
 import UndoRedoService from "components/services/UndoRedoService";
@@ -18,6 +26,8 @@ import { QuillEditorContext } from "components/baseComponents/quill/QuillEditorC
 import QuillGlobalToolbar from "components/baseComponents/quill/QuillGlobalToolbar";
 import KeyWordsComponent from "../proposal/questionnaire/brickTitle/KeyWords";
 import { User } from "model/user";
+import DifficultySelect from "../proposal/questionnaire/brickTitle/DifficultySelect";
+import BrickLength from "../proposal/questionnaire/brickLength/brickLength";
 
 export interface PlanProps {
   currentBrick: Brick;
@@ -32,17 +42,17 @@ export interface PlanProps {
 }
 
 const PlanPage: React.FC<PlanProps> = (props) => {
-  const {subjects} = props.user;
-  const { ybrick } = props;
+  const { subjects } = props.user;
+  const { ybrick, locked } = props;
   const editorIdState = React.useState("");
   const [subjectIndex, setSubjectIndex] = React.useState<number>();
 
   React.useEffect(() => {
     const subjectId = ybrick.get("subjectId");
-    if(subjects) {
-      setSubjectIndex(subjects.findIndex(s => s.id === subjectId) ?? 0);
+    if (subjects) {
+      setSubjectIndex(subjects.findIndex((s) => s.id === subjectId) ?? 0);
     }
-  }, [subjects])
+  }, [subjects]);
 
   console.log(subjectIndex);
 
@@ -69,7 +79,7 @@ const PlanPage: React.FC<PlanProps> = (props) => {
             <QuillEditorContext.Provider value={editorIdState}>
               <div className="p-edit-container">
                 <QuillGlobalToolbar
-                  disabled={props.locked}
+                  disabled={locked}
                   availableOptions={[
                     "bold",
                     "italic",
@@ -89,41 +99,104 @@ const PlanPage: React.FC<PlanProps> = (props) => {
                     <input
                       value={ybrick.get("title")}
                       placeholder="Title"
-                      disabled={props.locked}
+                      disabled={locked}
                       onChange={(e) => ybrick.set("title", e.target.value)}
                     />
                   </div>
                   <KeyWordsComponent
-                    disabled={props.locked}
+                    disabled={locked}
                     keyWords={ybrick.get("keywords")}
                   />
-                  <div className="subject-select-container">    
-                  {subjectIndex !== undefined &&
-                    <Select
-                      value={subjectIndex}
-                      onChange={(evt) =>
-                        setSubjectIndex(evt.target.value as number)
-                      }
-                      input={<InputBase />}
-                    >
-                      {subjects?.map((s, i) => (
-                        <MenuItem value={i} key={i}>
-                          <ListItemIcon>
-                            <SvgIcon>
-                              <SpriteIcon
-                                name="circle-filled"
-                                className="w100 h100 active"
-                                style={{ color: s.color }}
-                              />
-                            </SvgIcon>
-                          </ListItemIcon>
-                          <ListItemText>{s.name}</ListItemText>
-                        </MenuItem>
-                      ))}
-                    </Select>}
+                  <div className="subject-select-container">
+                    {subjectIndex !== undefined && (
+                      <Select
+                        value={subjectIndex}
+                        onChange={(evt) =>
+                          setSubjectIndex(evt.target.value as number)
+                        }
+                        input={<InputBase />}
+                      >
+                        {subjects?.map((s, i) => (
+                          <MenuItem value={i} key={i}>
+                            <ListItemIcon>
+                              <SvgIcon>
+                                <SpriteIcon
+                                  name="circle-filled"
+                                  className="w100 h100 active"
+                                  style={{ color: s.color }}
+                                />
+                              </SvgIcon>
+                            </ListItemIcon>
+                            <ListItemText>{s.name}</ListItemText>
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    )}
                   </div>
                   <div className="level-and-length-container">
-                    
+                    <DifficultySelect
+                      disabled={locked}
+                      level={ybrick.get("academicLevel")}
+                      onChange={(academicLevel) =>
+                        ybrick.set("academicLevel", academicLevel)
+                      }
+                    />
+                    <Select
+                      className="brick-length"
+                      value={ybrick.get("brickLength")}
+                    >
+                      <MenuItem value={BrickLengthEnum.S20min}>
+                        20 mins
+                      </MenuItem>
+                      <MenuItem value={BrickLengthEnum.S40min}>
+                        40 mins
+                      </MenuItem>
+                      <MenuItem value={BrickLengthEnum.S60min}>
+                        60 mins
+                      </MenuItem>
+                    </Select>
+                  </div>
+                  <div className="open-question-container">
+                    <QuillEditor
+                      disabled={locked}
+                      data={ybrick.get("openQuestion")}
+                      toolbar={["bold", "italic", "latex"]}
+                      imageDialog={true}
+                    />
+                  </div>
+                  <div className="brief-container">
+                    <div className="header">Brief</div>
+                    <QuillEditor
+                      disabled={locked}
+                      data={ybrick.get("brief")}
+                      toolbar={[
+                        "bold",
+                        "italic",
+                        "fontColor",
+                        "latex",
+                        "bulletedList",
+                        "numberedList",
+                      ]}
+                      imageDialog={true}
+                    />
+                  </div>
+                  <div className="prep-container">
+                    <div className="header">Prep</div>
+                    <QuillEditor
+                      disabled={locked}
+                      data={ybrick.get("prep")}
+                      toolbar={[
+                        "bold",
+                        "italic",
+                        "fontColor",
+                        "latex",
+                        "bulletedList",
+                        "numberedList",
+                        "blockQuote",
+                        "image",
+                      ]}
+                      imageDialog={true}
+                    />
                   </div>
                 </Grid>
               </div>
@@ -187,22 +260,16 @@ const PlanPage: React.FC<PlanProps> = (props) => {
             </Grid>*/}
         </Grid>
       </div>
-      {/*
-        <div className="bottom-scroll-area">
-          <div className="bottom-button-container">
-            <button
-              className="btn btn-transparent svgOnHover"
-              onClick={() => {}}
-            >
-              <SpriteIcon
-                name="arrow-down"
-                className={`active text-theme-orange ${
-                  !canScroll && "disabled"
-                }`}
-              />
-            </button>
-            </div>
-        </div>*/}
+      <div className="bottom-scroll-area">
+        <div className="bottom-button-container">
+          <button className="btn btn-transparent svgOnHover" onClick={() => {}}>
+            <SpriteIcon
+              name="arrow-down"
+              className={`active text-theme-orange`}
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
