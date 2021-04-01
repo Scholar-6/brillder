e1de6ed8aa2fd8a115b61035a4f8b7f9d1311a2b
Ivan Romakh <ivanrouman@gmail.com>
2021-04-01 15:08:09 +0300

    #3041 improving

--- src/components/baseComponents/timeDropdowns/TimeDropdowns.tsx
+++ src/components/baseComponents/timeDropdowns/TimeDropdowns.tsx
@@ -12,47 +12,64 @@ interface State {
   months: number[];
   years: number[];
 
+  startDate: Date;
+
   year: number;
   month: number;
   day: number;
 }
 
-class TimeDropdowns extends React.Component<any, State> {
+class TimeDropdowns extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
 
-    let start = 2021;
-    let end = 2025;
+    const startDate = new Date(2021, 3, 8);
 
-    let years = [];
+    const start = startDate.getFullYear();
+    const end = start + 4; 
+
+    const startMonth = startDate.getMonth();
+    const startDay = startDate.getDate();
+
+    const years = [];
     for (let i = start; i <= end; i++) {
       years.push(i);
     }
 
-    let months = [];
-    for (let i = 1; i <= 12; i++) {
-      months.push(i);
-    }
+    const months = this.getMonths(startMonth);
 
-    let days = this.getDays(3, start);
+    let days = this.getDays(3, start, startDay);
 
     this.state = {
       days,
       months,
       years,
+      startDate,
       year: start,
-      month: 1,
-      day: 1
+      month: months[0],
+      day: days[0]
     }
 
-    props.onChange(new Date(1, 1, start));
+    props.onChange(new Date(days.length + 1, months.length + 1, start));
   }
 
-  getDays(month: number, year: number) {
+  getMonths(startMonth: number) {
+    const months = [];
+    for (let i = 1; i <= 12; i++) {
+      if (i > startMonth) {
+        months.push(i);
+      }
+    }
+    return months;
+  }
+
+  getDays(month: number, year: number, startDay: number) {
     let days = [];
     let last = new Date(year, month, 0).getDate();
     for (let i = 1; i <= last; i++) {
-      days.push(i);
+      if (i > startDay) {
+        days.push(i);
+      }
     }
     return days;
   }
@@ -67,14 +84,26 @@ class TimeDropdowns extends React.Component<any, State> {
   }
 
   setMonth(newMonth: number) {
-    const days = this.getDays(newMonth, this.state.year);
+    let startDay = 0;
+    if (this.state.year === this.state.years[0]) {
+      startDay = this.state.startDate.getDate();
+    }
+    const days = this.getDays(newMonth, this.state.year, startDay);
     this.setState({days, month: newMonth});
     this.onChange(this.state.year, newMonth, this.state.day);
   }
 
   setYear(newYear: number) {
-    const days = this.getDays(this.state.month, newYear);
-    this.setState({days, year: newYear});
+    let startDay = 0;
+    if (newYear === this.state.years[0]) {
+      startDay = this.state.startDate.getDate();
+    }
+    const days = this.getDays(this.state.month, newYear, startDay);
+    let months = this.getMonths(0);
+    if (newYear === this.state.years[0]) {
+      months = this.getMonths(this.state.startDate.getMonth());
+    }
+    this.setState({days, year: newYear, months});
     this.onChange(newYear, this.state.month, this.state.day);
   }
 
