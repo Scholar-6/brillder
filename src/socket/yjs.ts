ab20ed0b614815c403a09e23e587e3b4f49fe939
William Gooch <william.gooch.wg@gmail.com>
2021-04-12 11:39:37 +0100

    Change yjs delay back to 100.

--- src/socket/yjs.ts
+++ src/socket/yjs.ts
@@ -58,8 +58,7 @@ export const getYDoc = (history: History, brickId: number, firstName: string, la
                 } catch (e) {
                     console.log("couldn't access ws", e);
                 }
-            }, 1000);
-            // TODO: figure out why this needs to be so long on production??
+            }, 100);
         }
     });
     
