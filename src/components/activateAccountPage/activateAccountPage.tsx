b4f24050cf3ea5b62faba8c867bafb0a454a279d
ivan <=>
2021-04-27 17:45:11 +0300

    #3273 activate account page

--- src/components/activateAccountPage/activateAccountPage.tsx
+++ src/components/activateAccountPage/activateAccountPage.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
-import { Grid, Hidden } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { History } from "history";
 import axios from 'axios';
 
 import "./activateAccountPage.scss";
-import LoginLogo from 'components/loginPage/components/LoginLogo';
-import GoogleButton from "components/loginPage/components/GoogleButton";
 import PolicyDialog from 'components/baseComponents/policyDialog/PolicyDialog';
-import RegisterButton from "components/loginPage/components/RegisterButton";
 import map from "components/map";
 import { Redirect, useLocation } from "react-router-dom";
 import PageLoader from "components/baseComponents/loaders/pageLoader";
-import TermsLink from "components/baseComponents/TermsLink";
 import { isPhone } from "services/phone";
 import DesktopActivateAccountPage from "./DesktopActivateAccountPage";
 import { connect } from "react-redux";
 import auth from "redux/actions/auth";
 import { UserType } from "model/user";
+import MobileEmailActivate from "./MobileEmailActivate";
 
 export enum LoginState {
   ChooseLoginAnimation,
@@ -39,7 +36,6 @@ const ActivateAccountPage: React.FC<ActivateAccountProps> = (props) => {
 
   const [email, setEmail] = useState('');
   const [isPolicyOpen, setPolicyDialog] = useState(initPolicyOpen);
-  const [loginState, setLoginState] = useState(LoginState.ChooseLoginAnimation);
   const [valid, setValid] = React.useState<boolean>();
 
   const { search } = useLocation();
@@ -63,32 +59,8 @@ const ActivateAccountPage: React.FC<ActivateAccountProps> = (props) => {
     verifyToken();
   }, [token, props.history, setValid]);
 
-  const moveToEmailPage = () => {
-    props.history.push(map.ActivateAccount + '/email?token=' + token);
-  }
-
-  const movingToLogin = () => {
-    setLoginState(LoginState.ButtonsAnimation);
-    setTimeout(() => {
-      moveToEmailPage();
-    }, 450);
-  }
-
   if(!valid && valid !== false) {
     return <PageLoader content="Checking token..." />
-  }
-
-  const renderButtons = () => {
-    let className = 'button-box f-column';
-    if (loginState === LoginState.ButtonsAnimation) {
-      className += ' expanding';
-    }
-    return (
-      <div className={className}>
-        <RegisterButton onClick={movingToLogin} />
-        <GoogleButton />
-      </div>
-    );
   }
 
   if (!email) {
@@ -107,31 +79,13 @@ const ActivateAccountPage: React.FC<ActivateAccountProps> = (props) => {
       justify="center"
       alignItems="center"
     >
-      {valid ? <>
-        <Hidden>
-          <div className="choose-login-desktop">
-            <Grid container direction="row" className="first-row">
-              <div className="first-col"></div>
-              <div className="second-col"></div>
-              <div className="third-col"></div>
-            </Grid>
-            <Grid container direction="row" className="second-row">
-              <div className="first-col">
-                <LoginLogo />
-              </div>
-              <div className="second-col">
-                {renderButtons()}
-              </div>
-            </Grid>
-            <Grid container direction="row" className="third-row">
-              <div className="first-col"></div>
-              <div className="second-col">
-                <TermsLink history={props.history}/>
-              </div>
-              <div className="third-col"></div>
-            </Grid>
-          </div>
-        </Hidden>
+      {valid && token ? <>
+        <MobileEmailActivate
+          token={token}
+          history={props.history}
+          email={email}
+          setPolicyDialog={setPolicyDialog}
+        />
         <PolicyDialog isOpen={isPolicyOpen} close={() => setPolicyDialog(false)} />
       </>
       : <Redirect to={map.Login} />}
