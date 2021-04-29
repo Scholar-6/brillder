01a341c56077095e4e31db5a43c1dd811f2a2339
William Gooch <william.gooch.wg@gmail.com>
2021-04-29 11:42:29 +0100

    Fix bold latex not rendering.

--- src/components/baseComponents/quill/QuillLatex.tsx
+++ src/components/baseComponents/quill/QuillLatex.tsx
@@ -1,10 +1,19 @@
 import { Quill } from "react-quill";
 
 const Inline = Quill.import("blots/inline");
+const Text = Quill.import("blots/text");
+const Cursor = Quill.import("blots/cursor");
 
 class LatexBlot extends Inline {
     static formats(): boolean {
         return true;
+    }
+
+    formatAt(index: number, length: number, name: string, value: any): void {
+        console.log(index, length, name, value);
+        if(value === false) {
+            return super.formatAt(index, length, name, value);
+        }
     }
 }
 LatexBlot.blotName = "latex";
