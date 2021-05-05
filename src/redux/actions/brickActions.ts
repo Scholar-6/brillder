c79ff1514064741600bca2c306a987a365138baa
Ivan Romakh <ivanrouman@gmail.com>
2021-05-05 13:58:56 +0300

    #479 if 403 redirect to home page

--- src/redux/actions/brickActions.ts
+++ src/redux/actions/brickActions.ts
@@ -24,13 +24,15 @@ const fetchBrick = (id: number) => {
   return function (dispatch: any) {
     return axios.get(process.env.REACT_APP_BACKEND_HOST + '/brick/' + id, {withCredentials: true})
       .then(res => {
-        let brick = res.data as Brick;
+        const brick = res.data as Brick;
         brick.questions.sort((q1, q2) => q1.order - q2.order);
         dispatch(fetchBrickSuccess(brick));
         dispatch(comments.getComments(brick.id));
+        return { status: res.status };
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(fetchBrickFailure(error.message));
+        return { status: error.request.status }
       });
   }
 }
