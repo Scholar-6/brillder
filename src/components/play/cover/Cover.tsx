1a08d464abb3c8442a9e23318ab91dc69408f997
Ivan Romakh <ivanrouman@gmail.com>
2021-05-20 16:48:19 +0300

    cover page

--- src/components/play/cover/Cover.tsx
+++ src/components/play/cover/Cover.tsx
@@ -33,6 +33,9 @@ const DesktopTheme = React.lazy(() => import('./themes/CoverDesktopTheme'));
 const CoverPage: React.FC<IntroductionProps> = ({ brick, ...props }) => {
   const [bioOpen, setBio] = React.useState(false);
 
+  const [firstPhonePopup, setFirstPhonePopup] = React.useState(false);
+  const [secondPhonePopup, setSecondPhonePopup] = React.useState(false);
+
   useEffect(() => {
     function handleMove(e: any) {
       if (rightKeyPressed(e)) {
@@ -56,7 +59,7 @@ const CoverPage: React.FC<IntroductionProps> = ({ brick, ...props }) => {
       <div>
         <div>Are you ready to learn?</div>
         <div className="c-next-btn-container">
-          <button type="button" className="bolder" onClick={startBrick}>
+          <button type="button" onClick={startBrick}>
             Play Now
           </button>
         </div>
@@ -70,47 +73,50 @@ const CoverPage: React.FC<IntroductionProps> = ({ brick, ...props }) => {
         <div className="brick-id-container">Brick #{brick.id}</div>
         <div className="question">What is a brick?</div>
         <div className="hover-area">
-          <SpriteIcon name="help-circle-custom" />
-          <div className="hover-content">
-            <div>A brick is a learning unit that should take either 20, 40, or 60 minutes to complete.</div>
-            <div>Bricks follow a cognitively optimised sequence:</div>
-            <div className="container">
-              <div className="white-circle">1</div>
-              <div className="l-text">
-                Preparation: <span className="regular">stimulus content gets you in the zone.</span>
+          <SpriteIcon name="help-circle-custom" onClick={() => setFirstPhonePopup(true)} />
+          {!isPhone() &&
+            <div className="hover-content">
+              <div>A brick is a learning unit that should take either 20, 40, or 60 minutes to complete.</div>
+              <br />
+              <div>Bricks follow a cognitively optimised sequence:</div>
+              <div className="container">
+                <div className="white-circle">1</div>
+                <div className="l-text">
+                  Preparation: <span className="regular">stimulus content gets you in the zone.</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="white-circle">2</div>
+                <div className="l-text">
+                  Investigation: <span className="regular">challenging interactive questions make you think.</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="white-circle">3</div>
+                <div className="l-text">
+                  <span className="regular">A preliminary score</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="white-circle">4</div>
+                <div className="l-text">
+                  Synthesis: <span className="regular">explanation.</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="white-circle">5</div>
+                <div className="l-text">
+                  Review: <span className="regular">hints help you correct your answers.</span>
+                </div>
+              </div>
+              <div className="container">
+                <div className="white-circle">6</div>
+                <div className="l-text">
+                  <span className="regular">A final score</span>
+                </div>
               </div>
             </div>
-            <div className="container">
-              <div className="white-circle">2</div>
-              <div className="l-text">
-                Investigation: <span className="regular">challenging interactive questions make you think.</span>
-              </div>
-            </div>
-            <div className="container">
-              <div className="white-circle">3</div>
-              <div className="l-text">
-                <span className="regular">A preliminary score</span>
-              </div>
-            </div>
-            <div className="container">
-              <div className="white-circle">4</div>
-              <div className="l-text">
-                Synthesis: <span className="regular">explanation.</span>
-              </div>
-            </div>
-            <div className="container">
-              <div className="white-circle">5</div>
-              <div className="l-text">
-                Review: <span className="regular">hints help you correct your answers.</span>
-              </div>
-            </div>
-            <div className="container">
-              <div className="white-circle">6</div>
-              <div className="l-text">
-              <span className="regular">A final score</span>
-              </div>
-            </div>
-          </div>
+          }
         </div>
       </div>
     );
@@ -149,7 +155,82 @@ const CoverPage: React.FC<IntroductionProps> = ({ brick, ...props }) => {
             <div className="cover-info-row">
               {renderBrickCircle()}
               {brick.subject?.name}, Level {brick.academicLevel && AcademicLevelLabels[brick.academicLevel]}
-              <SpriteIcon name="help-circle-custom" />
+              <SpriteIcon name="help-circle-custom" onClick={() => setSecondPhonePopup(true)} />
+              {firstPhonePopup &&
+                <div className="mobile-help-container" onClick={() => setFirstPhonePopup(false)}>
+                  <div>A brick is a learning unit that should take either 20, 40, or 60 minutes to complete.</div>
+                  <br />
+                  <div>Bricks follow a cognitively optimised sequence:</div>
+                  <div className="container">
+                    <div className="white-circle">1</div>
+                    <div className="l-text">
+                      Preparation: <span className="regular">stimulus content gets you in the zone.</span>
+                    </div>
+                  </div>
+                  <div className="container">
+                    <div className="white-circle">2</div>
+                    <div className="l-text">
+                      Investigation: <span className="regular">challenging interactive questions make you think.</span>
+                    </div>
+                  </div>
+                  <div className="container">
+                    <div className="white-circle">3</div>
+                    <div className="l-text">
+                      <span className="regular">A preliminary score</span>
+                    </div>
+                  </div>
+                  <div className="container">
+                    <div className="white-circle">4</div>
+                    <div className="l-text">
+                      Synthesis: <span className="regular">explanation.</span>
+                    </div>
+                  </div>
+                  <div className="container">
+                    <div className="white-circle">5</div>
+                    <div className="l-text">
+                      Review: <span className="regular">hints help you correct your answers.</span>
+                    </div>
+                  </div>
+                  <div className="container">
+                    <div className="white-circle">6</div>
+                    <div className="l-text">
+                      <span className="regular">A final score</span>
+                    </div>
+                  </div>
+                </div>}
+              {secondPhonePopup &&
+                <div className="mobile-help-container second" onClick={() => setSecondPhonePopup(false)}>
+                  <div className="flex-content">
+                    <div>Brillder focusses on universal concepts and topics, not specific exam courses.</div>
+                    <br />
+                    <div>LEVELS:</div>
+                    <div className="container">
+                      <div className="white-circle">I</div>
+                      <div className="l-text">
+                        <div>Foundation</div>
+                        <div className="regular">For 15-16 yr-olds, equivalent to GCSE / IB Middle Years / High School Diploma</div>
+                      </div>
+                    </div>
+                    <br />
+                    <div className="container">
+                      <div className="white-circle">II</div>
+                      <div className="and-sign">&</div>
+                      <div className="white-circle">III</div>
+                      <div className="l-text smaller">
+                        <div>Core</div>
+                        <div className="regular">For 17-18 yr-olds, equivalent to A-level / IB / High School Honors</div>
+                      </div>
+                    </div>
+                    <br />
+                    <div className="container">
+                      <div className="white-circle">IV</div>
+                      <div className="l-text">
+                        <div>Extension</div>
+                        <div className="regular">College / Undergraduate level, to challenge Oxbridge (UK) or Advanced Placement (US) students</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>}
             </div>
           </div>
           <div className="introduction-info">
