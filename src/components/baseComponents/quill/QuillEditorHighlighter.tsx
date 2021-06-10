f3172e3c8a90ad8bc8c6e1a1516f59d891158d1f
Ivan Romakh <ivanrouman@gmail.com>
2021-06-10 13:21:44 +0300

    #3430 quill highlighter

--- src/components/baseComponents/quill/QuillEditorHighlighter.tsx
+++ src/components/baseComponents/quill/QuillEditorHighlighter.tsx
@@ -20,117 +20,139 @@ import ImageUpload from "./QuillImageUpload";
 import QuillCustomClipboard from "./QuillCustomClipboard";
 import QuillDesmos from "./QuillDesmos";
 
+let Inline = ReactQuill.Quill.import('blots/inline');
+
 function randomEditorId() {
-    return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10);
 }
 
+class SpanBlock extends Inline {    
+
+  static create(value: any){
+      let node = super.create();
+      node.setAttribute('class','q-highlighter');
+      return node;    
+  } 
+}
+
+SpanBlock.blotName = 'spanblock';
+SpanBlock.tagName = 'div';
+ReactQuill.Quill.register(SpanBlock);
+
 interface QuillEditorProps {
-    data?: string;
-    allowLinks?: boolean;
-    allowMediaEmbed?: boolean;
-    allowTables?: boolean;
-    allowDesmos?: boolean;
-    onChange?(data: string): void;
-    onBlur?(): void;
+  data?: string;
+  allowLinks?: boolean;
+  allowMediaEmbed?: boolean;
+  allowTables?: boolean;
+  allowDesmos?: boolean;
+  onChange?(data: string): void;
+  onBlur?(): void;
 }
 
 const QuillEditorHighlighter = React.forwardRef<HTMLDivElement, QuillEditorProps>((props, forwardRef) => {
-    /*eslint-disable-next-line*/
-    const [currentQuillId, setCurrentQuillId] = React.useContext(QuillEditorContext);
+  /*eslint-disable-next-line*/
+  const [currentQuillId, setCurrentQuillId] = React.useContext(QuillEditorContext);
 
-    const callOnChange = React.useCallback(
-        _.debounce((content: string) => {
-            if (props.onChange) {
-                props.onChange(content);
-            }
-        }, 500),
-        [props.onChange]
-    );
+  const callOnChange = React.useCallback(
+    _.debounce((content: string) => {
+      if (props.onChange) {
+        props.onChange(content);
+      }
+    }, 500),
+    [props.onChange]
+  );
 
-    const onChange = (content: string) => {
-        setData(content);
-        callOnChange(content);
+  const onChange = (content: string) => {
+    setData(content);
+    callOnChange(content);
+  }
+
+  const [uniqueId] = React.useState(randomEditorId());
+  const [data, setData] = React.useState(props.data);
+  const [quill, setQuill] = React.useState<Quill | null>(null);
+
+  const [imageModule, setImageModule] = React.useState<ImageUpload>();
+  const [desmosModule, setDesmosModule] = React.useState<QuillDesmos>();
+
+  const [clipboardModule, setClipboardModule] = React.useState<QuillCustomClipboard>();
+  React.useEffect(() => {
+    if (imageModule && clipboardModule) {
+      clipboardModule.onPasteImage = imageModule.onImagePaste.bind(imageModule);
     }
+  }, [imageModule, clipboardModule]);
 
-    const [uniqueId] = React.useState(randomEditorId());
-    const [data, setData] = React.useState(props.data);
-    const [quill, setQuill] = React.useState<Quill | null>(null);
+  const onFocus = React.useCallback(() => {
+    setCurrentQuillId(uniqueId);
+  }, [setCurrentQuillId, uniqueId]);
 
-    const [imageModule, setImageModule] = React.useState<ImageUpload>();
-    const [desmosModule, setDesmosModule] = React.useState<QuillDesmos>();
+  const onBlur = React.useCallback(() => {
+    if (currentQuillId === uniqueId) {
+      setCurrentQuillId(undefined);
+    }
+    props?.onBlur?.();
+  }, [currentQuillId, setCurrentQuillId, uniqueId, props.onBlur])
 
-    const [clipboardModule, setClipboardModule] = React.useState<QuillCustomClipboard>();
-    React.useEffect(() => {
-        if (imageModule && clipboardModule) {
-            clipboardModule.onPasteImage = imageModule.onImagePaste.bind(imageModule);
+  const modules = React.useMemo(() => ({
+    toolbar: false,
+    autolink: props.allowLinks,
+    mediaembed: props.allowMediaEmbed,
+    imageupload: false,
+    clipboard: true,
+    keyboard: {
+      bindings: QuillBetterTable.keyboardBindings,
+    },
+    table: false,
+    'better-table': props.allowTables ? {
+      operationMenu: {
+        items: {},
+      },
+    } : false,
+    desmos: props.allowDesmos,
+  }), [uniqueId, props.allowLinks, props.allowMediaEmbed, props.allowDesmos]);
+
+  const ref = React.useCallback((node: ReactQuill) => {
+    if (node) {
+      const editor = node.getEditor();
+      setImageModule(editor.getModule("imageupload") as ImageUpload);
+      setDesmosModule(editor.getModule("desmos") as QuillDesmos);
+      setClipboardModule(editor.getModule("clipboard") as QuillCustomClipboard);
+      editor.on("editor-change", () => {
+        const clipboard = editor.getModule("clipboard");
+        if (clipboardModule !== clipboard) {
+          setClipboardModule(clipboard);
         }
-    }, [imageModule, clipboardModule]);
-
-    const onFocus = React.useCallback(() => {
-        setCurrentQuillId(uniqueId);
-    }, [setCurrentQuillId, uniqueId]);
-
-    const onBlur = React.useCallback(() => {
-        if (currentQuillId === uniqueId) {
-            setCurrentQuillId(undefined);
+      });
+      editor.on('selection-change', (range, range2) => {
+        const selection = editor.getSelection();
+        if (selection) {
+          console.log(range, range2, selection);
+          editor.formatText(selection,'spanblock',true);
         }
-        props?.onBlur?.();
-    }, [currentQuillId, setCurrentQuillId, uniqueId, props.onBlur])
+      })
+      if (quill !== editor) {
+        setQuill(editor);
+      }
+    }
+    /*eslint-disable-next-line*/
+  }, []);
 
-    const modules = React.useMemo(() => ({
-        toolbar: false,
-        autolink: props.allowLinks,
-        mediaembed: props.allowMediaEmbed,
-        imageupload: false,
-        clipboard: true,
-        keyboard: {
-            bindings: QuillBetterTable.keyboardBindings,
-        },
-        table: false,
-        'better-table': props.allowTables ? {
-            operationMenu: {
-                items: {},
-            },
-        } : false,
-        desmos: props.allowDesmos,
-    }), [uniqueId, props.allowLinks, props.allowMediaEmbed, props.allowDesmos]);
-
-    const ref = React.useCallback((node: ReactQuill) => {
-        if (node) {
-            const editor = node.getEditor();
-            setImageModule(editor.getModule("imageupload") as ImageUpload);
-            setDesmosModule(editor.getModule("desmos") as QuillDesmos);
-            setClipboardModule(editor.getModule("clipboard") as QuillCustomClipboard);
-            editor.on("editor-change", () => {
-                const clipboard = editor.getModule("clipboard");
-                if (clipboardModule !== clipboard) {
-                    setClipboardModule(clipboard);
-                }
-            });
-            if (quill !== editor) {
-                setQuill(editor);
-            }
-        }
-        /*eslint-disable-next-line*/
-    }, []);
-
-    return (
-        <div
-            className="quill-document-editor"
-            data-toolbar={[]}
-            ref={forwardRef}
-        >
-            <ReactQuill
-                theme="snow"
-                value={data || ""}
-                onChange={onChange}
-                onBlur={onBlur}
-                onFocus={onFocus}
-                modules={modules}
-                ref={ref}
-            />
-        </div>
-    );
+  return (
+    <div
+      className="quill-document-editor"
+      data-toolbar={[]}
+      ref={forwardRef}
+    >
+      <ReactQuill
+        theme="snow"
+        value={data || ""}
+        onChange={onChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
+        modules={modules}
+        ref={ref}
+      />
+    </div>
+  );
 });
 
 export default QuillEditorHighlighter;
