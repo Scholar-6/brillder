b684d174186cd497e4f8ca48ea45171e26ac545d
Ivan Romakh <ivanrouman@gmail.com>
2021-06-10 17:02:55 +0300

    #3482 only show tooltip for current state of brick

--- src/components/build/baseComponents/statusCircle/StatusCircle.tsx
+++ src/components/build/baseComponents/statusCircle/StatusCircle.tsx
@@ -49,7 +49,9 @@ const StatusCircle: React.FC<Props> = ({isCore, status}) => {
       return (
         <div className="b-c-container">
           <SpriteIcon name="circle-filled" className={className} />
-          <div className="css-custom-tooltip">{getToolipText(staticStatus)} </div>
+          {staticStatus === status &&
+            <div className="css-custom-tooltip">{getToolipText(staticStatus)} </div>
+          }
         </div>
       );
     }
