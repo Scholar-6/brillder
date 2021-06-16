c19caeb975e6d2959dd05c62ead3ecaafa3762c7
Toby Peres <tobyperes@Tobys-MacBook-Pro-3.local>
2021-06-16 16:50:06 +0100

    pair match scoring

--- src/components/play/services/scoring/PairMatchScoring.ts
+++ src/components/play/services/scoring/PairMatchScoring.ts
@@ -2,7 +2,7 @@ import { ComponentAttempt } from "components/play/model";
 import { PairMatchComponent } from "components/play/questionTypes/pairMatch/interface";
 
 const mark = (component: PairMatchComponent, attempt: ComponentAttempt<any>) => {
-    let markIncrement = 1;
+    let markIncrement = 2;
     attempt.correct = true;
     attempt.marks = 0;
     attempt.maxMarks = markIncrement * component.list.length;
@@ -19,3 +19,12 @@ const mark = (component: PairMatchComponent, attempt: ComponentAttempt<any>) => 
 }
 
 export default mark;
+
+/* i think we can't necessarily only give points
+if the user has interacted - for one the random 
+output of options maybe be correct, but also 
+the user may agree with whatever the system 
+outputs initially - unless we explicitly 
+tell the user that they must interact with the 
+options to score any points, but i don't like
+that*/
