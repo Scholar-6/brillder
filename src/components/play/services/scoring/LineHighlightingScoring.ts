a7077a40a52e05df627a9a19a7d66c990f108913
Toby Peres <tobyperes@Tobys-MacBook-Pro-3.local>
2021-06-24 13:38:17 +0100

    revert lineHighLighting

--- src/components/play/services/scoring/LineHighlightingScoring.ts
+++ src/components/play/services/scoring/LineHighlightingScoring.ts
@@ -2,34 +2,27 @@ import { ComponentAttempt } from "components/play/model";
 
 const mark = (component: any, attempt: ComponentAttempt<any>) => {
     attempt.correct = true;
-    attempt.maxMarks = 0;
-    attempt.marks = 0;
+    attempt.maxMarks = 6;
 
     component.lines.forEach((line: any, index: number) => {
         const isChecked = attempt.answer.indexOf(index) !== -1;
         const shouldBeChecked = line.checked === true;
-        if(shouldBeChecked){attempt.maxMarks += 3}
-        if(isChecked){
-        if(shouldBeChecked === false) {
+        if(isChecked !== shouldBeChecked) {
             attempt.correct = false;
-            attempt.marks -= 2;
         }
-        else {
-            attempt.marks += 3;
-        }}
     });
 
-    
-    if(attempt.marks<0){
-        attempt.marks=0;
+    if (attempt.correct) {
+        attempt.marks = attempt.maxMarks;
+    } else {
+        if(attempt.answer.length === 0) {
+            attempt.marks = 0;
+        } else {
+            attempt.marks = 2;
+        }
     }
 
     return attempt;
 };
 
 export default mark;
-
-/* this does what Joe wants but doesn't factor in the
-added difficulty of lots of options - maxMarks is 
-proportional to the number of correct answers rather 
-than to number of options */
