a38e4d8e8b843876d1b358991d3e1d94b745621b
Toby Peres <tobyperes@Tobys-MacBook-Pro-3.local>
2021-06-29 13:21:17 +0100

    shortAnswerScoring update

--- src/components/play/services/scoring/ShortAnswerScoring.ts
+++ src/components/play/services/scoring/ShortAnswerScoring.ts
@@ -2,28 +2,60 @@ import { ShortAnswerData } from "components/build/buildQuestions/questionTypes/s
 import { ShortAnswerAnswer } from "components/play/questionTypes/shortAnswer/ShortAnswer";
 import { stripHtmlExceptSubAndSup as stripHtml } from "components/build/questionService/ConvertService";
 import { ComponentAttempt } from "components/play/model";
+import { endianness } from "os";
 
 const mark = (component: ShortAnswerData, attempt: ComponentAttempt<ShortAnswerAnswer>) => {
-    attempt.maxMarks = 6;
-
-    // The maximum number of marks is divided between all answers.
-    let markIncrement = attempt.maxMarks / component.list.length;
+    attempt.maxMarks = 0;
     attempt.correct = true;
     attempt.marks = 0;
-
+    
     component.list.forEach((answer, index) => {
+        const correctAnswer = stripHtml(answer.value);
+        if(correctAnswer.toLowerCase().length<4){
+            //2 marks for each of the first 3 symbols
+            attempt.maxMarks += 2*correctAnswer.toLowerCase().length;
+        }
+        else{
+            //2 marks for each of the first 3 symbols and 1 for each after
+            attempt.maxMarks += 6 + (correctAnswer.toLowerCase().length - 3);
+        }
+
         if (attempt.answer[index]) {
-            const correctAnswer = stripHtml(answer.value);
             const givenAnswer = stripHtml(attempt.answer[index]);
+            let end = 0;
             console.log(`${givenAnswer.toLowerCase()} ${correctAnswer.toLowerCase()}`)
-            if (givenAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-                // add the correct amount of marks
-                attempt.marks += markIncrement;
-            } else {
-                // the answer is not correct.
+
+            //check how far through letters to check
+            if(givenAnswer.toLowerCase().length!==correctAnswer.toLowerCase().length){
                 attempt.correct = false;
+                if(givenAnswer.toLowerCase().length<correctAnswer.toLowerCase().length) {
+                    end = givenAnswer.toLowerCase().length
+                }
+                else {
+                    end = correctAnswer.toLowerCase().length
+                }
             }
-        } else {
+            else {
+                end=givenAnswer.toLowerCase().length;
+            }
+            
+            //check each symbol and compare to correct answer. 2 marks for each of first 3, 
+            //1 for each of remaining
+            for(let i=0; i<end; i++){
+                if(givenAnswer.toLowerCase()[i]===correctAnswer.toLowerCase()[i]){
+                    if(i<3){
+                        attempt.marks += 2;
+                    }
+                    else{
+                        attempt.marks += 1;
+                    }
+                }
+                else{
+                    attempt.correct = false;
+                }
+            }
+        } 
+        else {
             // the answer is not filled in.
             attempt.correct = false;
         } 
