5ae0c3d137b32251e1b97a2d40c8a7088ef92d38
Ivan Romakh <ivanrouman@gmail.com>
2021-07-05 16:33:16 +0300

    #3544 levels help

--- src/components/viewAllPage/MobileCategory.tsx
+++ src/components/viewAllPage/MobileCategory.tsx
@@ -22,6 +22,7 @@ import routes from "components/play/routes";
 import PhoneTopBrick16x9 from "components/baseComponents/PhoneTopBrick16x9";
 import { getSubjects } from "services/axios/subject";
 import map from "components/map";
+import MobileHelp from "components/baseComponents/hoverHelp/MobileHelp";
 
 const MobileTheme = React.lazy(() => import("./themes/ViewAllPageMobileTheme"));
 
@@ -496,6 +497,52 @@ class MobileCategoryPage extends Component<BricksListProps, BricksListState> {
               {this.renderAcademicLevel(AcademicLevel.Second)}
               {this.renderAcademicLevel(AcademicLevel.Third)}
               {this.renderAcademicLevel(AcademicLevel.Fourth)}
+              <div className="va-difficult-help">
+                <MobileHelp>
+                  <div className="flex-content">
+                    <div>
+                      Brillder focusses on universal concepts and topics, not
+                      specific exam courses.
+                    </div>
+                    <br />
+                    <div>LEVELS:</div>
+                    <div className="container">
+                      <div className="white-circle">I</div>
+                      <div className="l-text">
+                        <div>Foundation</div>
+                        <div className="regular">
+                          For 15-16 yr-olds, equivalent to GCSE / IB Middle
+                          Years / High School Diploma
+                        </div>
+                      </div>
+                    </div>
+                    <br />
+                    <div className="container">
+                      <div className="white-circle">II</div>
+                      <div className="and-sign">&</div>
+                      <div className="white-circle">III</div>
+                      <div className="l-text smaller">
+                        <div>Core</div>
+                        <div className="regular">
+                          For 17-18 yr-olds, equivalent to A-level / IB / High
+                          School Honors
+                        </div>
+                      </div>
+                    </div>
+                    <br />
+                    <div className="container">
+                      <div className="white-circle">IV</div>
+                      <div className="l-text">
+                        <div>Extension</div>
+                        <div className="regular">
+                          College / Undergraduate level, to challenge Oxbridge
+                          (UK) or Advanced Placement (US) students
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </MobileHelp>
+              </div>
             </div>
             {this.renderSubjects(subjects)}
           </div>
