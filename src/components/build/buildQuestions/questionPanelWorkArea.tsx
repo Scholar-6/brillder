5a1ab22548a280d4aaf34c9d670f7fd1dc877e09
ivan <=>
2021-07-08 14:51:47 +0300

    remove image

--- src/components/build/buildQuestions/questionPanelWorkArea.tsx
+++ src/components/build/buildQuestions/questionPanelWorkArea.tsx
@@ -68,7 +68,7 @@ const QuestionPanelWorkArea: React.FC<QuestionProps> = ({
   const [componentTypes, setComponentType] = React.useState([
     { id: 1, type: QuestionComponentTypeEnum.Text },
     { id: 2, type: QuestionComponentTypeEnum.Quote },
-    { id: 3, type: QuestionComponentTypeEnum.Image },
+    //{ id: 3, type: QuestionComponentTypeEnum.Image },
     { id: 4, type: QuestionComponentTypeEnum.Sound },
     { id: 5, type: QuestionComponentTypeEnum.Graph }
   ]);
@@ -147,13 +147,13 @@ const QuestionPanelWorkArea: React.FC<QuestionProps> = ({
                   hoverMarginTop="-1.5vw"
                   value={QuestionComponentTypeEnum.Quote}
                 />
-                {
+                {/*
                 <DragBox
                   locked={locked}
                   name="jpg"
                   label="IMAGE"
                   value={QuestionComponentTypeEnum.Image}
-                />}
+                />*/}
                 <DragBox
                   locked={locked}
                   isImage={true} src="/images/soundicon.png"
