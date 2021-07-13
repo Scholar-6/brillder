a611e96617e10ea6cad0572b525ad8de536f09b4
Ivan Romakh <ivanrouman@gmail.com>
2021-07-13 14:03:52 +0300

    #3648 android hide login bottom

--- src/components/loginPage/phone/MobileEmailLogin.tsx
+++ src/components/loginPage/phone/MobileEmailLogin.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Hidden } from "@material-ui/core";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import Button from "@material-ui/core/Button";
 import { History } from "history";
@@ -8,6 +7,7 @@ import TermsLink from "components/baseComponents/TermsLink";
 import SpriteIcon from "components/baseComponents/SpriteIcon";
 import map from "components/map";
 import axios from "axios";
+import { hideZendesk, showZendesk } from "services/zendesk";
 
 interface MobileLoginProps {
   email: string;
@@ -25,94 +25,120 @@ interface MobileLoginProps {
   setPolicyDialog(isOpen: boolean): void;
 }
 
-class MobileEmailLoginPage extends React.Component<MobileLoginProps> {
+interface State {
+  originalHeight: number;
+  keyboardShown: boolean;
+  resizeHandler(): void;
+}
+class MobileEmailLoginPage extends React.Component<MobileLoginProps, State> {
+  constructor(props: MobileLoginProps) {
+    super(props);
 
-  renderSignInPage() {
-    return (
-      <div className="second-item">
-        <div className="logo-box">
-          <div className="logo-box-inner">
-            <div className="logo-image mobile">
-              <SpriteIcon name="logo" className="active text-theme-orange" onClick={() => this.props.history.push(map.Login)} />
-            </div>
-          </div>
-          <div className="h-arrow-down">
-            <SpriteIcon name="arrow-down" />
-          </div>
-        </div>
-        <form
-          onSubmit={this.props.handleLoginSubmit}
-          className="mobile-button-box content-box"
-        >
-          <div className="input-block">
-            <input
-              type="email"
-              value={this.props.email}
-              onChange={(e) => this.props.setEmail(e.target.value)}
-              className="login-field"
-              required
-              placeholder="Email"
-            />
-          </div>
-          <div className="phone-reset-link-container">
-            <div className="reset-password-link" onClick={async () => {
-              try {
-                if (this.props.email) {
-                  try {
-                    await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/auth/resetPassword/${this.props.email}`, {}, { withCredentials: true });
-                  } catch {}
-                  this.props.setEmailSended(true);
-                } else {
-                  this.props.setEmptyEmail(true);
-                }
-              } catch {
-                // failed
-              }
-            }}>Forgot password?</div>
-          </div>
-          <div className="input-block">
-            <input
-              type={this.props.passwordHidden ? "password" : "text"}
-              value={this.props.password}
-              className="login-field password"
-              onChange={(e) => this.props.setPassword(e.target.value)}
-              required
-              placeholder="Password"
-            />
-            <div className="hide-password-icon-container">
-              <VisibilityIcon
-                className="hide-password-icon"
-                onClick={() =>
-                  this.props.setHidden(!this.props.passwordHidden)
-                }
-              />
-            </div>
-          </div>
-          <div className="input-block button-box">
-            <Button
-              variant="contained"
-              color="primary"
-              className="sign-in-button orange-button bold"
-              type="submit"
-            >
-              Sign in
-              </Button>
-          </div>
-          <div className="mobile-policy-text">
-            <TermsLink history={this.props.history}/>
-          </div>
-        </form>
-      </div>
-    );
+    this.state = {
+      originalHeight: window.innerHeight,
+      keyboardShown: false,
+      resizeHandler: this.resizeHandler.bind(this)
+    }
+  }
+
+  resizeHandler() {
+    if (this.state.originalHeight > window.innerHeight + 60) {
+      hideZendesk();
+      this.setState({ keyboardShown: true })
+    } else {
+      this.setState({ keyboardShown: false })
+      showZendesk();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.state.resizeHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.state.resizeHandler);
   }
 
   render() {
     return (
-      <Hidden only={["sm", "md", "lg", "xl"]}>
-        <div className="first-col">
-          {this.renderSignInPage()}
+      <div className="first-col">
+        <div className="second-item">
+          <div className="logo-box">
+            <div className="logo-box-inner">
+              <div className="logo-image mobile">
+                <SpriteIcon name="logo" className="active text-theme-orange" onClick={() => this.props.history.push(map.Login)} />
+              </div>
+            </div>
+            <div className="h-arrow-down">
+              <SpriteIcon name="arrow-down" />
+            </div>
+          </div>
+          <form
+            onSubmit={this.props.handleLoginSubmit}
+            className="mobile-button-box content-box"
+          >
+            <div className="input-block">
+              <input
+                type="email"
+                value={this.props.email}
+                onChange={(e) => this.props.setEmail(e.target.value)}
+                className="login-field"
+                required
+                placeholder="Email"
+              />
+            </div>
+            {!this.state.keyboardShown &&
+            <div className="phone-reset-link-container">
+              <div className="reset-password-link" onClick={async () => {
+                try {
+                  if (this.props.email) {
+                    try {
+                      await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/auth/resetPassword/${this.props.email}`, {}, { withCredentials: true });
+                    } catch { }
+                    this.props.setEmailSended(true);
+                  } else {
+                    this.props.setEmptyEmail(true);
+                  }
+                } catch {
+                  // failed
+                }
+              }}>Forgot password?</div>
+            </div>}
+            <div className="input-block">
+              <input
+                type={this.props.passwordHidden ? "password" : "text"}
+                value={this.props.password}
+                className="login-field password"
+                onChange={(e) => this.props.setPassword(e.target.value)}
+                required
+                placeholder="Password"
+              />
+              <div className="hide-password-icon-container">
+                <VisibilityIcon
+                  className="hide-password-icon"
+                  onClick={() =>
+                    this.props.setHidden(!this.props.passwordHidden)
+                  }
+                />
+              </div>
+            </div>
+            <div className="input-block button-box">
+              <Button
+                variant="contained"
+                color="primary"
+                className="sign-in-button orange-button bold"
+                type="submit"
+              >
+                Sign in
+              </Button>
+            </div>
+            {!this.state.keyboardShown &&
+            <div className="mobile-policy-text">
+              <TermsLink history={this.props.history} />
+            </div>}
+          </form>
         </div>
-      </Hidden>
+      </div>
     );
   }
 }
