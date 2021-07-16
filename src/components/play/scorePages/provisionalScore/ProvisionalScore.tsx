76280b9ba5aed58e70ce185112a7291348379691
Ivan Romakh <ivanrouman@gmail.com>
2021-07-16 13:18:16 +0300

    #3656 mobile

--- src/components/play/scorePages/provisionalScore/ProvisionalScore.tsx
+++ src/components/play/scorePages/provisionalScore/ProvisionalScore.tsx
@@ -165,6 +165,9 @@ class ProvisionalScore extends React.Component<
           </div>
           <div className="content">
             <div className="title">Provisional Score</div>
+            {this.state.score < this.state.maxScore &&
+              <div className="hr-sub-title">You can improve this when reviewing your answers</div>
+            }
             <div className="pr-progress-center">
               <div className="pr-progress-container">
                 <CircularProgressbar
