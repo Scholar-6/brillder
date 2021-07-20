6ca65926c322f2d7be46e1c898405e0361b1dc67
Ivan Romakh <ivanrouman@gmail.com>
2021-07-20 14:57:07 +0300

    #2592

--- src/components/resetPasswordPage/ResetPasswordPage.tsx
+++ src/components/resetPasswordPage/ResetPasswordPage.tsx
@@ -1,15 +1,19 @@
-import { Grid } from '@material-ui/core';
 import React, { useEffect } from 'react';
-import { Redirect, useLocation } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
+import Button from "@material-ui/core/Button";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 import axios from 'axios';
+import { Redirect, useLocation } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import PageLoader from 'components/baseComponents/loaders/pageLoader';
 import map from 'components/map';
 import TypingInput from 'components/loginPage/components/TypingInput';
-import VisibilityIcon from "@material-ui/icons/Visibility";
 import LoginLogo from 'components/loginPage/components/LoginLogo';
 import TextDialog from 'components/baseComponents/dialogs/TextDialog';
 import authActions from "redux/actions/auth";
-import { connect } from 'react-redux';
+import { isPhone } from 'services/phone';
+import SpriteIcon from 'components/baseComponents/SpriteIcon';
 
 interface ResetPasswordPageProps {
   history: any;
@@ -32,7 +36,7 @@ const ResetPasswordPage: React.FC<ResetPasswordPageProps> = props => {
       try {
         await axios.get(`${process.env.REACT_APP_BACKEND_HOST}/auth/verifyResetPassword/${token}`);
         setValid(true);
-      } catch(e) {
+      } catch (e) {
         setValid(false);
       }
     }
@@ -42,18 +46,92 @@ const ResetPasswordPage: React.FC<ResetPasswordPageProps> = props => {
 
   const resetPassword = async () => {
     try {
-      if(password === confirmPassword) {
+      if (password === confirmPassword) {
         await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/auth/changePassword/${token}`, { password: password }, { withCredentials: true });
         props.checkAuth();
         setSuccess(true);
       }
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
   }
 
-  if(!valid && valid !== false) {
+  if (!valid && valid !== false) {
     return <PageLoader content="Checking token..." />
+  }
+
+  if (isPhone()) {
+    return (
+      <Grid
+        className="auth-page login-page"
+        container
+        item
+        justify="center"
+        alignItems="center"
+      >
+        {token ? <>
+          <div className="first-col">
+            <div className="second-item">
+              <div className="logo-box">
+                <div className="logo-box-inner">
+                  <div className="logo-image mobile">
+                    <SpriteIcon name="login" className="active text-theme-orange" />
+                  </div>
+                </div>
+              </div>
+              <form className="mobile-button-box content-box">
+                <div className="input-block">
+                  <input
+                    type={passwordHidden ? "password" : "text"}
+                    value={password}
+                    className="login-field password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    placeholder="New Password"
+                  />
+                  <div className="hide-password-icon-container">
+                    <VisibilityIcon
+                      className="hide-password-icon"
+                      onClick={() => setPasswordHidden(p => !p)}
+                    />
+                  </div>
+                </div>
+                <div className="input-block">
+                  <input
+                    type={passwordHidden ? "password" : "text"}
+                    value={confirmPassword}
+                    className="login-field password"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                    placeholder="Confirm Password"
+                  />
+                  <div className="hide-password-icon-container">
+                    <VisibilityIcon
+                      className="hide-password-icon"
+                      onClick={() => setPasswordHidden(p => !p)}
+                    />
+                  </div>
+                </div>
+                <div className="input-block button-box">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    className="sign-up-button"
+                    type="button"
+                    onClick={resetPassword}
+                  >
+                    Reset Password
+                  </Button>
+                </div>
+                <div className="mobile-policy-text">
+                </div>
+              </form>
+            </div>
+          </div>
+        </>
+          : <Redirect to={map.Login} />}
+      </Grid>
+    );
   }
 
   return (
@@ -131,6 +209,6 @@ const mapDispatch = (dispatch: any) => ({
   checkAuth: () => dispatch(authActions.isAuthorized()),
 });
 
-const connector = connect(() => {}, mapDispatch);
+const connector = connect(() => { }, mapDispatch);
 
 export default connector(ResetPasswordPage);
