a7ffe3a13c9eb869bb94efdcfd25f1912cffb511
Ivan Romakh <ivanrouman@gmail.com>
2021-07-21 15:35:10 +0300

    mobile fixes

--- src/components/baseComponents/pageHeader/MenuDropdown.tsx
+++ src/components/baseComponents/pageHeader/MenuDropdown.tsx
@@ -32,6 +32,7 @@ interface MenuDropdownProps {
   user: User;
   page: PageEnum;
   history: any;
+  className?: string;
   onLogout(): void;
   forgetBrick(): void;
 }
@@ -254,7 +255,7 @@ const MenuDropdown: React.FC<MenuDropdownProps> = (props) => {
 
   return (
     <Menu
-      className="menu-dropdown"
+      className={"menu-dropdown " + props.className}
       open={props.dropdownShown}
       onClose={props.hideDropdown}
     >
