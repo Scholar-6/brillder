23c86e4a2dcac80462053a2a6a167b934ef89ead
Ivan Romakh <ivanrouman@gmail.com>
2021-07-27 11:08:35 +0300

    #3693

--- src/components/postPlay/desktop/PostDesktopPlay.tsx
+++ src/components/postPlay/desktop/PostDesktopPlay.tsx
@@ -26,15 +26,10 @@ import BookSidebar from "./BookSidebar";
 import QuestionPage from "./QuestionPage";
 import PageHeadWithMenu, { PageEnum } from "components/baseComponents/pageHeader/PageHeadWithMenu";
 import SpriteIcon from "components/baseComponents/SpriteIcon";
-import FrontPage from "../FrontPage";
-import { Grid } from "@material-ui/core";
-import map from "components/map";
+import BookPages from "./BookPages";
 
 const TabletTheme = React.lazy(() => import('../themes/PageTabletTheme'));
 const DesktopTheme = React.lazy(() => import('../themes/PageDesktopTheme'));
-
-const DesktopBookTheme = React.lazy(() => import('../themes/PageBookDesktopTheme'));
-
 
 export enum BookState {
   Front,
@@ -146,6 +141,13 @@ class PostDesktopPlay extends React.Component<ProposalProps, ProposalState> {
     }
   }
 
+  setAttempt(attempt: PlayAttempt) {
+    try {
+      this.prepareAttempt(attempt);
+      this.setState({ attempt, bookState: BookState.Brief });
+    } catch { }
+  }
+
   render() {
     if (!this.state.attempt) {
       return <PageLoader content="...Getting Attempt..." />;
@@ -156,70 +158,27 @@ class PostDesktopPlay extends React.Component<ProposalProps, ProposalState> {
       return <Redirect to="/home" />;
     }
 
-    const { brick, student } = this.state.attempt;
+    const { brick } = this.state.attempt;
 
     let questions: Question[] = [];
     for (let question of brick.questions) {
       parseQuestion(question as ApiQuestion, questions);
     }
 
-    let color = "#B0B0AD";
-    if (brick.subjectId) {
-      const subject = this.state.subjects.find(s => s.id === brick.subjectId);
-      if (subject) {
-        color = subject.color;
-      }
-    }
-
     if (this.state.bookState === BookState.Front) {
-      let bookClass = "book-main-container";
-      bookClass += ' closed';
 
-      return (
-        <React.Suspense fallback={<></>}>
-          <DesktopTheme />
-          <DesktopBookTheme />
-          <div className="post-play-page">
-            <PageHeadWithMenu
-              page={PageEnum.Book}
-              user={this.props.user}
-              placeholder="Search Ongoing Projects & Published Bricks…"
-              history={this.props.history}
-              search={() => { }}
-              searching={(v: string) => { }}
-            />
-            <Helmet>
-              <title>{getBrillderTitle(brick.title)}</title>
-            </Helmet>
-            <Grid
-              container
-              direction="row"
-              style={{ height: "100% !important" }}
-              justify="center"
-            >
-              <Grid className="main-text-container">
-                <h1>This book is yours.</h1>
-                <h2>Hover your mouse over the cover to</h2>
-                <h2>see a summary of your results.</h2>
-                <button onClick={() => this.props.history.push(map.MyLibrary + '?subjectId=' + brick.subjectId)}>
-                  View it in my library
-                </button>
-              </Grid>
-              <div className={bookClass}>
-                <div className="book-container" onMouseOut={() => { }}>
-                  <div className="book" onMouseOver={() => { }}>
-                    <div className="back"></div>
-                    <div className="page6"></div>
-                    <div className="page5"></div>
-                    <div className="front-cover"></div>
-                    <FrontPage brick={brick} student={student} color={color} onClick={() => this.setState({bookState: BookState.Brief})} />
-                  </div>
-                </div>
-              </div>
-            </Grid>
-          </div>
-        </React.Suspense>
-      )
+      let color = "#B0B0AD";
+      if (brick.subjectId) {
+        const subject = this.state.subjects.find(s => s.id === brick.subjectId);
+        if (subject) {
+          color = subject.color;
+        }
+      }
+
+      return <BookPages
+        history={this.props.history} color={color} user={this.props.user} attempt={this.state.attempt} attempts={this.state.attempts}
+        setAttempt={this.setAttempt.bind(this)}
+      />
     }
 
     return (
