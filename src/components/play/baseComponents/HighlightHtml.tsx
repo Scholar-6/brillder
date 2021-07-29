bb629b6e7ee1aca0db1ea26e4ab4452e5dd6ec2e
William Gooch <william.gooch.wg@gmail.com>
2021-07-29 10:22:24 +0100

    Overhaul highlighter feature

--- src/components/play/baseComponents/HighlightHtml.tsx
+++ src/components/play/baseComponents/HighlightHtml.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 import { PlayMode } from "../model";
 
+//@ts-ignore
+import rangy from "rangy";
+import "rangy/lib/rangy-classapplier";
+
 import YoutubeAndMathInHtml from "./YoutubeAndMathQuote";
 
 import "./HighlightHtml.scss";
+
+console.log(rangy);
+rangy.init();
+const classApplier = rangy.createClassApplier("hi");
 
 interface SelectableProps {
   value: string;
@@ -16,29 +24,57 @@ interface SelectableProps {
 const HighlightHtml: React.FC<SelectableProps> = (props) => {
   const [textBox, setTextBox] = React.useState<HTMLDivElement>();
   const shouldHighlight = props.mode === PlayMode.Highlighting && props.onHighlight;
-  const shouldUnHighlight = props.mode === PlayMode.UnHighlighting && props.onHighlight;
+
+  // 29/07/21 - document.execCommand is deprecated
+  // const onMouseUp = React.useCallback(() => {
+  //   if(!textBox) return;
+  //   const selection = window.getSelection();
+  //   if(selection && !selection.isCollapsed && selection.anchorNode && selection.focusNode) {
+  //     textBox.contentEditable = "true";
+  //     if(shouldHighlight) {
+  //       const position = selection.anchorNode.compareDocumentPosition(selection.focusNode);
+  //       let backwards = false;
+  //       if ((!position && selection.anchorOffset > selection.focusOffset) || position === Node.DOCUMENT_POSITION_PRECEDING) {
+  //         backwards = true;
+  //       }
+  //       console.log(selection.anchorOffset, selection.focusOffset);
+  //       if(!backwards) {
+  //         if (!document.execCommand("HiliteColor", false, "var(--highlight-yellow)")) {
+  //           document.execCommand("BackColor", false, "var(--highlight-yellow)");
+  //         }
+  //       } else {
+  //         document.execCommand("RemoveFormat", false, "foreColor");
+  //       }
+  //     }
+  //     textBox.contentEditable = "false";
+  //     props.onHighlight(textBox?.innerHTML);
+  //     selection.removeAllRanges();
+  //   }
+  // }, [shouldHighlight]);
 
   const onMouseUp = React.useCallback(() => {
     if(!textBox) return;
     const selection = window.getSelection();
-    if(selection && !selection.isCollapsed) {
-      const range = selection?.getRangeAt(0);
+    if(selection && !selection.isCollapsed && selection.anchorNode && selection.focusNode) {
       textBox.contentEditable = "true";
-      if (range) {
-        selection.removeAllRanges();
-        selection.addRange(range);
-      }
       if(shouldHighlight) {
-        if (!document.execCommand("HiliteColor", false, "var(--highlight-yellow)")) {
-          document.execCommand("BackColor", false, "var(--highlight-yellow)");
+        const position = selection.anchorNode.compareDocumentPosition(selection.focusNode);
+        let backwards = false;
+        if ((!position && selection.anchorOffset > selection.focusOffset) || position === Node.DOCUMENT_POSITION_PRECEDING) {
+          backwards = true;
         }
-      } else if (shouldUnHighlight) {
-        document.execCommand("RemoveFormat", false);
+        console.log(selection.anchorOffset, selection.focusOffset);
+        if(!backwards) {
+          classApplier.applyToSelection();
+        } else {
+          classApplier.undoToSelection();
+        }
       }
       textBox.contentEditable = "false";
       props.onHighlight(textBox?.innerHTML);
+      selection.removeAllRanges();
     }
-  }, [shouldUnHighlight, shouldHighlight]);
+  }, [shouldHighlight]);
 
   const textRef = React.useCallback((div: HTMLDivElement) => {
     if(textBox) {
