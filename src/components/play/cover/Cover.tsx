c66efe8478e224a03aa3e12e8823db87c2fae634
Ivan Romakh <ivanrouman@gmail.com>
2021-08-02 13:10:07 +0300

    #3535

--- src/components/play/cover/Cover.tsx
+++ src/components/play/cover/Cover.tsx
@@ -23,6 +23,7 @@ import UnauthorizedUserDialogV2 from "components/baseComponents/dialogs/Unauthor
 import TextDialog from "components/baseComponents/dialogs/TextDialog";
 
 import { CreateByEmailRes, createUserByEmail } from "services/axios/user";
+import HoveredImage from "../baseComponents/HoveredImage";
 
 
 interface Props {
@@ -165,6 +166,7 @@ const CoverPage: React.FC<Props> = ({ brick, ...props }) => {
   if (isPhone()) {
     return (
       <React.Suspense fallback={<></>}>
+        <HoveredImage />
         <MobileTheme />
         <div className="cover-page">
           {renderFirstRow()}
@@ -277,6 +279,7 @@ const CoverPage: React.FC<Props> = ({ brick, ...props }) => {
 
   return (
     <React.Suspense fallback={<></>}>
+      <HoveredImage />
       {isMobile ? <TabletTheme /> : <DesktopTheme />}
       <div className="brick-row-container cover-page">
         <div className="brick-container">
