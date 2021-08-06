bd67ebb8494f4db931591f376a1defe0695764bd
Ivan Romakh <ivanrouman@gmail.com>
2021-08-06 11:15:20 +0300

    #3780

--- src/components/build/baseComponents/phonePreview/questionPreview/PairMatchPreview.tsx
+++ src/components/build/baseComponents/phonePreview/questionPreview/PairMatchPreview.tsx
@@ -15,13 +15,25 @@ const PairMatchPreview: React.FC<any> = () => {
   return (
     <div className="phone-preview-component pairmatch-preview">
       <Grid container justify="center" className="small-text">
-        A small number of words in the 
+        A small number of words in the
         scene are no longer in the current
         use. See if you can match each to
         its correct definition.
       </Grid>
-      <Grid container justify="center" className={isGreen ? 'inside-green' : ''}>
-      </Grid>
+      <div className="jj-pair-content">
+        <div className="jj-pair-option-col">
+          <div>festinade (adj.)</div>
+          <div>questrist (adj.)</div>
+          <div>corky (adj.)</div>
+          <div>pinion (adj.)</div>
+        </div>
+        <div className="jj-pair-answer-col">
+          <div className="jj-fourth"><span>bind the arms so they cannot move</span></div>
+          <div className="jj-third"><span>old and spoilt, gone off</span></div>
+          <div className="jj-first"><span>speedy</span></div>
+          <div className="jj-second"><span>seeker or pursuer</span></div>
+        </div>
+      </div>
     </div>
   )
 }
