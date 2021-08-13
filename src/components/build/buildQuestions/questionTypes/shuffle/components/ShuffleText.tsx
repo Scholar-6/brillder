4d50226734726c3b7f48bf7fb6c6d4a0b94bdfdf
Ivan Romakh <ivanrouman@gmail.com>
2021-08-13 17:45:52 +0300

    lover case

--- src/components/build/buildQuestions/questionTypes/shuffle/components/ShuffleText.tsx
+++ src/components/build/buildQuestions/questionTypes/shuffle/components/ShuffleText.tsx
@@ -5,7 +5,7 @@ const ShuffleText = () => {
   return (
     <div className="flex-center">
       <SpriteIcon name="feather-shuffle" />
-      <div>These will be randomised in the play Interface.</div>
+      <div>These will be randomised in the play interface.</div>
     </div>
   );
 }
