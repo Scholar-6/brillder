7f02c425f7584f396a5d55cf48a4cd3bd17e5f0a
Ivan Romakh <ivanrouman@gmail.com>
2021-08-13 11:39:12 +0300

    #3801

--- src/components/baseComponents/loaders/pageLoader.tsx
+++ src/components/baseComponents/loaders/pageLoader.tsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './pageLoader.scss';
-import LoaderLogo from 'assets/img/dot-loader.svg';
+import SpriteIcon from '../SpriteIcon';
 
-class PageLoader extends Component<pageLoaderProps> {
-  render() {
-    return (
-      <div className="page-loader">
-        <img src={LoaderLogo} alt="Loader logo" />
-      </div>
-    );
-  }
-}
-interface pageLoaderProps {
+interface PageLoaderProps {
   content: string;
 }
+
+const PageLoader: React.FC<PageLoaderProps> = () => {
+  return (
+    <div className="page-loader">
+      <SpriteIcon name="f-loader" className="spinning" />
+    </div>
+  );
+}
+
 export default PageLoader;
