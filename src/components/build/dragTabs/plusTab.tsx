fdefbc11df3c0d91394ba7e737922005550212f6
Ivan Romakh <ivanrouman@gmail.com>
2021-08-13 15:01:15 +0300

    #3788 tabs

--- src/components/build/dragTabs/plusTab.tsx
+++ src/components/build/dragTabs/plusTab.tsx
@@ -8,15 +8,16 @@ export interface PlusTabProps {
 }
 
 const PlusTab: React.FC<PlusTabProps> = ({ tutorialStep }) => {
-  let className = "add-tab last-tab";
+  let className = "";
   if (tutorialStep === TutorialStep.Investigation) {
-    className += " editor-border svgOnHover border-animation";
+    className += " editor-border border-animation";
   }
   return (
     <Grid className={"drag-tile"} container alignContent="center" justify="center">
-      <div className={`svgOnHover ${className}`}>
+      <div className={`svgOnHover add-tab last-tab ${className}`}>
         {tutorialStep === TutorialStep.Investigation && <SpriteIcon name="dashed-circle" className="circle-border" />}
         <SpriteIcon name="plus-line-custom" className="w100 h100 active text-theme-dark-blue"/>
+        <div className="css-custom-tooltip">Add a new question panel</div>
       </div>
     </Grid>
   );
