d969641348d0a336ca87a45c1fb74363068e0527
William Gooch <william.gooch.wg@gmail.com>
2021-08-27 10:12:27 +0100

    Fix highlighting using requestAnimationFrame for View All page.

--- src/components/baseComponents/BrickTitle.tsx
+++ src/components/baseComponents/BrickTitle.tsx
@@ -10,23 +10,44 @@ const classApplier = rangy.createClassApplier("search-highlight");
 
 interface BrickTitleProps {
   title: string;
+  className?: string;
   searchString?: string;
 }
 
-const BrickTitle:React.FC<BrickTitleProps> = ({title, searchString}) => {
-  const textRef = React.useCallback((el: HTMLElement) => {
-    if(!searchString || !el) return;
+const BrickTitle:React.FC<BrickTitleProps> = ({title, className = "brick-inline", searchString}) => {
+  const [textRef, setTextRef] = React.useState<HTMLSpanElement>();
+  const requestRef = React.useRef<number>();
+  
+  const updateHighlighting = React.useCallback(() => {
+    if(!searchString || !textRef) return;
+    if(textRef.innerText === "") {
+      // This is required to wait for the text of the title to be visible, since it starts off hidden and animates.
+      // A bit hacky, but Rangy only finds text that's *visible* on the page (innerText) rather than all text (textContent).
+      // So we have to wait for the animation to start playing and *then* sort out highlighting.
+      requestRef.current = requestAnimationFrame(updateHighlighting);
+    }
 
     const parentRange = rangy.createRange();
-    parentRange.selectNode(el);
+    parentRange.selectNode(textRef);
     const range = rangy.createRange();
     range.findText(searchString, {
       withinRange: parentRange,
     });
     classApplier.applyToRange(range);
-  }, [searchString])
+  }, [textRef, title, searchString]);
 
-  return <span ref={textRef} className="brick-inline" dangerouslySetInnerHTML={{__html: title}} />
+  React.useLayoutEffect(() => {
+    requestRef.current = requestAnimationFrame(updateHighlighting);
+    return () => {
+      requestRef.current && cancelAnimationFrame(requestRef.current);
+    }
+  }, [updateHighlighting]);
+
+  const textRefCallback = React.useCallback((el: HTMLElement) => {
+    setTextRef(el);
+  }, [])
+
+  return <span ref={textRefCallback} className={className} dangerouslySetInnerHTML={{__html: title}} />
 }
 
 export default BrickTitle;
