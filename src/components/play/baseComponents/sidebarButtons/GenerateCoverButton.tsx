a88a9a79acd649b623c892c2a2c1613303522cd3
William Gooch <william.gooch.wg@gmail.com>
2021-09-02 10:08:08 +0100

    Change name of downloaded PDF

--- src/components/play/baseComponents/sidebarButtons/GenerateCoverButton.tsx
+++ src/components/play/baseComponents/sidebarButtons/GenerateCoverButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import axios from 'axios';
 import SpriteIcon from 'components/baseComponents/SpriteIcon';
+import { Brick } from 'model/brick';
+import { stripHtml } from 'components/build/questionService/ConvertService';
 
 interface GenerateCoverButtonProps {
-    brickId: number;
+    brick: Brick;
     sidebarRolledUp: boolean;
 }
 
@@ -13,20 +15,20 @@ const GenerateCoverButton: React.FC<GenerateCoverButtonProps> = props => {
     const [numberOfScans, setNumberOfScans] = React.useState(0);
     React.useEffect(() => {
         (async () => {
-            const { data: { count } } = await axios.get(`${process.env.REACT_APP_BACKEND_HOST}/scans/${props.brickId}`, { withCredentials: true });
+            const { data: { count } } = await axios.get(`${process.env.REACT_APP_BACKEND_HOST}/scans/${props.brick.id}`, { withCredentials: true });
             setNumberOfScans(count);
         })();
-    }, [props.brickId]);
+    }, [props.brick]);
 
     const generateCover = React.useCallback(async () => {
-        const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_HOST}/brick/${props.brickId}/pdf/cover`, { withCredentials: true, responseType: "blob" });
+        const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_HOST}/brick/${props.brick.id}/pdf/cover`, { withCredentials: true, responseType: "blob" });
         const url = window.URL.createObjectURL(new Blob([data]));
         const link = document.createElement("a");
         link.href = url;
-        link.setAttribute("download", `cover-${props.brickId}.pdf`);
+        link.setAttribute("download", `${stripHtml(props.brick.title)}.pdf`);
         document.body.appendChild(link);
         link.click();
-    }, [props.brickId]);
+    }, [props.brick]);
 
     if (props.sidebarRolledUp) {
         return (
