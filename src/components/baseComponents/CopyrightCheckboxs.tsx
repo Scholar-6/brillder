ed713aa5cc6148fad67a56c8be18adf667a2a5b3
Ivan Romakh <ivanrouman@gmail.com>
2021-09-21 15:10:09 +0300

    build-image-style

--- src/components/baseComponents/CopyrightCheckboxs.tsx
+++ src/components/baseComponents/CopyrightCheckboxs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Checkbox from "@material-ui/core/Checkbox";
+import SpriteIcon from './SpriteIcon';
 
 interface Props {
   isSound?: boolean;
@@ -14,20 +15,24 @@ const CopyrightCheckboxes: React.FC<Props> = (props) => {
   return (
     <div className="light-font permision-buttons">
       <div onClick={() => props.setPermision(permision ? permision === true ? 1 : true : true)}>
-        <Checkbox
-          checked={permision === true}
-          className={validationRequired ? "required" : ""}
-        />
+        <div>
+          <SpriteIcon name="globe" />
+          <Checkbox
+            checked={permision === true}
+            className={validationRequired ? "required" : ""}
+          />
+        </div>
           I am aware of licence restrictions around the publication of {props.isSound ? 'sounds' : 'images'} online. I have checked this {props.isSound ? 'sound' : 'image'} is available for use without such restriction or within Creative Commons criteria which allow commercial use.
-          <span className="text-theme-orange">*</span>
       </div>
       <div onClick={() => props.setPermision(permision ? permision === true ? 1 : true : 1)}>
-        <Checkbox
-          checked={permision === 1}
-          className={validationRequired ? "required" : ""}
-        />
+        <div>
+        <SpriteIcon name="key" />
+          <Checkbox
+            checked={permision === 1}
+            className={validationRequired ? "required" : ""}
+          />
+        </div>
           I am creating a brick for educational use with my own students and can therefore use licensed {props.isSound ? 'sounds' : 'images'} consistent with the exemptions for education in the Copyright Designs and Patents Act, 1988.
-          <span className="text-theme-orange">*</span>
       </div>
     </div>
   )
