56d7213a62dfcc86f0448cdb30c2f172729909bb
Ivan Romakh <ivanrouman@gmail.com>
2021-10-08 11:53:01 +0300

    Stripepage page

---
+++ src/components/stripePage/StripePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './StripePage.scss';
+
+const StripePage: React.FC<any> = (props) => {
+  return <div className="StripePayPage">
+    Stripe Pay
+  </div>
+}
+
+export default StripePage;
