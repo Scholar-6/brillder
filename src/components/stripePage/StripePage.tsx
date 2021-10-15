feaf864afb95ddb46cf210750fabba83fc81d7c0
aromakh <andriyromakh@gmail.com>
2021-10-15 16:51:47 +0300

    stripe basic changes.

--- src/components/stripePage/StripePage.tsx
+++ src/components/stripePage/StripePage.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-
 import './StripePage.scss';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import StripePageCreditCard from 'components/stripePageCreditCard/StripePageCreditCard';
+import { User } from 'model/user';
+import { ReduxCombinedState } from 'redux/reducers';
+import { connect } from 'react-redux';
+import userActions from "../../redux/actions/user";
 
-const StripePage: React.FC<any> = (props) => {
-  return <div className="StripePayPage">
-    Stripe Pay
-  </div>
+interface StripePageProps {
+  user: User;
 }
 
-export default StripePage;
+const StripePage: React.FC<any> = (props: any) => {
+  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK as string);
+  debugger
+
+  var user = props.user;
+  return (<div className="StripePayPage">
+    <Elements stripe={stripePromise}>
+      <StripePageCreditCard user={user}></StripePageCreditCard>
+    </Elements>
+  </div>)
+}
+
+const mapState = (state: ReduxCombinedState) => ({
+  user: state.user.user,
+});
+
+const mapDispatch = (dispatch: any) => ({
+  getUser: () => dispatch(userActions.getUser()),
+});
+
+export default connect(mapState, mapDispatch)(StripePage);
