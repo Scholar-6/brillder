13348fe54f93ba819b6ab9b6e256b2255ff4bb8b
Ivan Romakh <ivanrouman@gmail.com>
2022-01-19 14:40:28 +0200

    #4230

--- src/components/userManagement/UsersList.tsx
+++ src/components/userManagement/UsersList.tsx
@@ -498,7 +498,7 @@ class UsersListPage extends Component<UsersListProps, UsersListState> {
                   <td>{user.email}</td>
                   <td>{this.renderUserType(user)}</td>
                   <td className="activate-button-container">
-                    <CustomToggle checked={user.status === UserStatus.Active} onClick={() => this.toggleUser(user)} />
+                    <CustomToggle checked={user.status === UserStatus.Active} name={user.firstName} onClick={() => this.toggleUser(user)} />
                   </td>
                   <UserActionsCell
                     userId={user.id}
