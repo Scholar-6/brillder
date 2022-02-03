141b6a6d28e0081881ce7a13fd6dfa3a352656cf
Ivan Romakh <ivanrouman@gmail.com>
2022-02-03 16:45:35 +0200

    #4305

--- src/components/teach/manageClassrooms/studentTable/StudentTable.tsx
+++ src/components/teach/manageClassrooms/studentTable/StudentTable.tsx
@@ -123,7 +123,7 @@ const StudentTable: React.FC<StudentTableProps> = props => {
               <Checkbox
                 checked={user.selected}
                 onMouseOver={() => onHover(user)} onMouseLeave={() => onBlur(user)} />
-              {renderLibraryLink(user)}
+              {!user.hasInvitation && renderLibraryLink(user)}
               {user.selectHovered && <div className="custom-tooltip">Select</div>}
             </div>
             <div className="student-name">
