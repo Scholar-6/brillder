4604eb6489fdb8febea70d55a368304688189430
Ivan Romakh <ivanrouman@gmail.com>
2022-04-21 10:45:54 +0300

    #4485 library in profile update

--- src/components/userProfilePage/RealLibraryCoonect.tsx
+++ src/components/userProfilePage/RealLibraryCoonect.tsx
@@ -86,10 +86,10 @@ const RealLibraryConnect: React.FC<Props> = () => {
     <div className="customer-real-library">
       <div className="sub-title-d43"> Get a premium account for free through your UK library</div>
       <div className="flex-center">
-        <div>
+        <div className="relative">
+          {(libraryId === -1 || libraryId === null) && <div className="absolute-placeholder unselectable" onClick={e => e.preventDefault()}>Library Authority</div>}
           <Select
             className="select-existed-class"
-            placeholder="Library Name"
             value={libraryId}
             onChange={e => setLibrary(e.target.value as any)}
             MenuProps={{ classes: { paper: 'select-classes-list' } }}
@@ -105,7 +105,7 @@ const RealLibraryConnect: React.FC<Props> = () => {
           <ProfileInput value={libraryCardNumber} validationRequired={false} className="" type="text" onChange={e => setCardNumber(e.target.value)} placeholder="Library Card Number" />
         </div>
         <div>
-          <ProfileInput value={pin} validationRequired={false} className="" type="password" onChange={e => setPin(e.target.value)} placeholder="Pin" />
+          <ProfileInput value={pin} validationRequired={false} className="" type="password" onChange={e => setPin(e.target.value)} placeholder="Library Pin" />
         </div>
       </div>
       <div className="flex-center btn-container">
