52b6df723b1f3fe7c5a30a2cfcd6b6bcc85ca1ac
Ivan Romakh <ivanrouman@gmail.com>
2022-04-27 15:20:18 +0300

    #4510 popup updated

--- src/components/baseComponents/dialogs/CompetitionLibraryDialog.tsx
+++ src/components/baseComponents/dialogs/CompetitionLibraryDialog.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Dialog from "@material-ui/core/Dialog";
+import SpriteIcon from "../SpriteIcon";
+import { isPhone } from "services/phone";
 
 interface DialogProps {
   isOpen: boolean;
@@ -8,9 +10,14 @@ interface DialogProps {
 
 const CompetitionLibraryDialog: React.FC<DialogProps> = ({ isOpen, close }) => {
   return (
-    <Dialog open={isOpen} onClose={close} className="dialog-box">
+    <Dialog open={isOpen} onClose={close} className="dialog-box custom-popup-d35">
+      <div className="flex-center m-b-10">
+        <div className="red-circle-icon-d35">
+          <SpriteIcon name="alert-triangle" />
+        </div>
+      </div>
       <div className="dialog-header">
-        <div className="bold" style={{textAlign: 'center'}}>There is a competition running for this brick. You cannot view your answers until it is over</div>
+        <div className="bold" style={{ textAlign: 'center' }}>There is a competition running for this brick. {isPhone() ? '' : <br />} You cannot view your answers until it is over.</div>
       </div>
       <div className="dialog-footer">
         <button className="btn btn-md bg-theme-green yes-button" onClick={close}>
