43004d4cc07a895fecb1c2834300c19e13136ae1
Ivan Romakh <ivanrouman@gmail.com>
2022-04-29 16:33:28 +0300

    #4510

--- src/components/baseComponents/dialogs/CompetitionLibraryDialog.tsx
+++ src/components/baseComponents/dialogs/CompetitionLibraryDialog.tsx
@@ -11,6 +11,9 @@ interface DialogProps {
 const CompetitionLibraryDialog: React.FC<DialogProps> = ({ isOpen, close }) => {
   return (
     <Dialog open={isOpen} onClose={close} className="dialog-box custom-popup-d35">
+      <div className="close-button svgOnHover" onClick={close}>
+        <SpriteIcon name="cancel" className="w100 h100 active" />
+      </div>
       <div className="flex-center m-b-10">
         <div className="red-circle-icon-d35">
           <SpriteIcon name="alert-triangle" />
@@ -21,7 +24,7 @@ const CompetitionLibraryDialog: React.FC<DialogProps> = ({ isOpen, close }) => {
       </div>
       <div className="dialog-footer">
         <button className="btn btn-md bg-theme-green yes-button" onClick={close}>
-          <span>Ok</span>
+          <span>Enter Competition</span>
         </button>
       </div>
     </Dialog>
