33d75204ef4f040d968c93b6bcfa1e6c9a545185
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-05-20 21:36:27 +0100

    refactored the cover to more clearly display the play button depending on the user

--- src/components/play/cover/components/coverAuthorRow/CoverCreditsPlay.tsx
+++ src/components/play/cover/components/coverAuthorRow/CoverCreditsPlay.tsx
@@ -16,33 +16,43 @@ interface Props {
 
 const CoverCreditsPlay: React.FC<Props> = ({ user, isAssignment, isCompetition, isLibraryUser, isAuthor, isPublisher, isPaidEducator, onClick }) => {
   const renderDynamicPart = () => {
-    if (!user) {
-      return <div />;
-    }
-    if (isPaidEducator || isLibraryUser || isAssignment) {
-      if (isCompetition) {
-        return <div className="absolute-credits">{2}</div>;
-      }
-      return <div />;
-    }
-    if (!isAuthor && !isPublisher) {
+    // All of this is just presentation on the frontend, the actual charging is sorted out on backend
+    // play is free if: anonymous user, premium educator, library user, 
+    // author of the brick, publisher, or the Brick has been assigned to the user 
+    if (!user || isPaidEducator || isLibraryUser || isAuthor || isPublisher || isAssignment) {
       return (
-        <div className="absolute-credits">
-          {isCompetition ? 2 : 1 }
-        </div>
+        <>
+          Play Free
+        </>
       );
     }
-    return <div />;
+    // Always charge 2 credits for a competition
+    if (isCompetition) {
+      return (
+        <>
+          <SpriteIcon name="circle-lines" />
+          <div className="absolute-credits">2</div>
+          <SpriteIcon className={`hover-icon ${isAuthor ? 'always-visible' : ''}`} name="feather-play-circle" />
+          Play Now
+        </>
+      );
+    }
+    // All other cases are charged 1 credit
+    return (
+      <>
+        <SpriteIcon name="circle-lines" />
+        <div className="absolute-credits">1</div>
+        <SpriteIcon className={`hover-icon ${isAuthor ? 'always-visible' : ''}`} name="feather-play-circle" />
+        Play Now
+      </>
+    );
   }
 
   return (
     <div className="cover-play-button cover-credits-play">
       <div className="c-next-btn-container">
         <button type="button" onClick={onClick}>
-          <SpriteIcon name="circle-lines" />
           {renderDynamicPart()}
-          <SpriteIcon className={`hover-icon ${isAuthor ? 'always-visible' : ''}`} name="feather-play-circle" />
-          Play Now
         </button>
       </div>
     </div>
