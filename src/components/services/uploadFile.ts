cd8898308217aefdac179237c841b9e840032e37
Ivan Romakh <ivanrouman@gmail.com>
2022-11-15 15:41:41 +0200

    #4753 images from s3 bucket

--- src/components/services/uploadFile.ts
+++ src/components/services/uploadFile.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 export function fileUrl(fileName: string) {
   if (!fileName) { return '' }
-  return `${process.env.AWS_S3_IMAGES_BUCKET_NAME}/files/${fileName}`;
+  console.log(888, process.env);
+  return `${process.env.REACT_APP_AWS_S3_IMAGES_BUCKET_NAME}/files/${fileName}`;
 }
 
 export function uploadFile(inputFile: File, callback: Function, onError: Function) {
