ca5f88deb126b87ec90002a706b7b84fc9045ef3
Ivan Romakh <ivanrouman@gmail.com>
2023-01-18 16:03:31 +0200

    pointer events pair match

--- src/components/play/questionTypes/pairMatch/PairMatchImageContent.tsx
+++ src/components/play/questionTypes/pairMatch/PairMatchImageContent.tsx
@@ -42,7 +42,7 @@ const PairMatchImageContent: React.FC<AnswerProps> = ({ fileName, imageCaption, 
             }
           }}
         />
-        <img alt="" src={fileUrl(fileName)} width="100%" />
+        <img alt="" className="no-pointer-events" src={fileUrl(fileName)} width="100%" />
         {imageCaption && <div dangerouslySetInnerHTML={{ __html: imageCaption }} />}
       </div>
     );
