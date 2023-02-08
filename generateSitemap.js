a00d972e9fbdc127a8483c77c5f8c5341060d2c9
Ivan Romakh <ivanrouman@gmail.com>
2023-02-08 12:48:58 +0200

    #4806 vertical shuffle

--- generateSitemap.js
+++ generateSitemap.js
@@ -3,11 +3,9 @@ const axios = require("axios").default;
 const dotenv = require("dotenv");
 dotenv.config();
 
-/* gateway error
 axios.get("https://api.brillder.com/sitemap.xml?baseUrl=https://app.brillder.com").then(response => {
     let xml = response.data;
     xml = xml.replace(`xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`, `xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://app.brillder.com/play/dashboard/</loc></url><url><loc>https://app.brillder.com/login</loc></url><url><loc>https://app.brillder.com/home</loc></url>`)
 
     fs.writeFileSync("./public/sitemap.xml", xml);
 });
-*/
