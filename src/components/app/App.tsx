bf98c01689bee39ac86a044627c99d62bd5ac432
Ivan Romakh <ivanrouman@gmail.com>
2023-03-27 17:42:15 +0300

    redeploy

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -88,7 +88,6 @@ interface AppProps {
   setReferralId(referralId: string): void;
 }
 
-
 const App: React.FC<AppProps> = props => {
   const history = useHistory();
   const location = useLocation();
