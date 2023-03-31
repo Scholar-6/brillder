dc73aeb7ac6bc11d0cf91a5412cc3494aa988ffa
Ivan Romakh <ivanrouman@gmail.com>
2023-03-31 15:08:16 +0300

    fix for search in mobile catalogue

--- src/services/competition.ts
+++ src/services/competition.ts
@@ -6,8 +6,6 @@ export const checkCompetitionActive = (c: Competition) => {
   const endDate = new Date(c.endDate);
   const startDate = new Date(c.startDate);
 
-  console.log('Competition Dates: ', endDate, startDate, timeNow)
-
   if (endDate.getTime() > timeNow) {
     if (startDate.getTime() < timeNow) {
       return true;
