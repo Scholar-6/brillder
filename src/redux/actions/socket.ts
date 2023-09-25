0979812182f7b2d3b7d6113becf43ba5256b7aac
Ivan Romakh <ivanrouman@gmail.com>
2023-09-25 16:15:03 +0300

    fix sockets

--- src/redux/actions/socket.ts
+++ src/redux/actions/socket.ts
@@ -30,13 +30,6 @@ export const socketStopEditing = () => {
     } as Action;
 };
 
-export const socketNavigateToQuestion = (questionId?: number) => {
-    return {
-        type: "socket/NAVIGATE_TO_QUESTION",
-        questionId
-    } as Action;
-}
-
 export const socketUpdateBrick = (brick: Brick) => {
     return {
         type: "socket/UPDATE_BRICK",
